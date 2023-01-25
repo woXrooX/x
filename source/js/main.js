438bcad9f24412e53a6a618800d1b7e13406de71
woXrooX <woXrooX@gmail.com>
2023-01-25 17:59:14 +0500

    + Conf.. Bridge

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,19 @@
 "use strict";
+///////////////////////////// Brideg / Fetch
+import bridge from "./modules/bridge.js";
+
+///////////////////////////// GLOBAL DATA
+// conf, session, langCode, langDict, languages, currencies
+GlobalData: {
+  let response = await bridge("bridge", {for:"globalData"});
+  window.conf = response["conf"];
+  // window.session = response["session"];
+  window.langCode = response["langCode"];
+  window.langDict = response["langDict"];
+  // window.languages = response["languages"];
+  // window.currencies = response["currencies"];
+}
+
 
 ///////////////////////////// SPA / Core
 import Core from "./SPA/core.js";
@@ -7,21 +22,6 @@ import Core from "./SPA/core.js";
 ///////////////////////////// Tools
 // import Form from "./modules/tools.js";
 
-
-///////////////////////////// Brideg / Fetch
-// import bridge from "./modules/bridge.js";
-
-///////////////////////////// GLOBAL DATA
-// conf, session, langCode, langDict, languages, currencies
-// GlobalData: {
-//   let response = await bridge("bridge", {for:"globalData"});
-//   window.conf = response["conf"];
-//   window.session = response["session"];
-//   window.langCode = response["langCode"];
-//   window.langDict = response["langDict"];
-//   window.languages = response["languages"];
-//   window.currencies = response["currencies"];
-// }
 
 // prefers-color-scheme TMP
 // console.log(window.matchMedia("(prefers-color-scheme: dark)"));
