d09f557317ced7172fa732461eafb67746185d05
woXrooX <woXrooX@gmail.com>
2023-02-01 16:42:34 +0500

    under development

--- source/js/pages/home.js
+++ source/js/pages/home.js
@@ -19,12 +19,12 @@ export default function home(){
   let data2 = `
 <container>
   <row>
-    <column class="wd-25">C1</column>
-    <column class="wd-75">C2</column>
+    <column class="w-75">C1</column>
+    <column class="w-25">C2</column>
   </row>
   <row>
-    <column class="wd-100">C1</column>
-    <column class="wd-5">C2</column>
+    <column class="w-100">C1</column>
+    <column class="w-5">C2</column>
   </row>
 </container>
   `;
--- source/js/pages/logIn.js
+++ source/js/pages/logIn.js
@@ -1,37 +1,48 @@
 "use strict";
 
 export const TITLE = "Log In";
+
+export const CSS = ``;
+
 export default function logIn(){
 
   let dom = `
 <container>
-  <column class="w-50">
-    <row>
-      <h1>${langDict["logIn"][langCode]}</h1>
-    </row>
-    <form for='logIn' autocomplete='off'>
+  <row>
+    <column class="w-50 box p-5">
 
-    <label>
-        <p for='username'>${langDict["username"][langCode]}</p>
-        <input type='text' name='username' minlength='${conf["username_min_length"]}' maxlength='${conf["username_max_length"]}' autofocus>
-    </label>
+      <row>
+        <h1>${langDict["logIn"][langCode]}</h1>
+      </row>
 
-    <label>
-        <p for='password'>${langDict["password"][langCode]}</p>
-        <input type='password' name='password' minlength='${conf["password_min_length"]}' maxlength='${conf["password_max_length"]}' autocomplete='on'><br>
-    </label>
+      <row>
+        <form for='logIn' autocomplete='off'>
 
-    <label>
-        <p for='logIn'></p>
-        <input type='submit' name='logIn' value='${langDict["logIn"][langCode]}'>
-    </label>
+          <label>
+              <p for='username'>${langDict["username"][langCode]}</p>
+              <input type='text' name='username' minlength='${conf["username_min_length"]}' maxlength='${conf["username_max_length"]}' autofocus>
+          </label>
 
-    </form>
+          <label>
+              <p for='password'>${langDict["password"][langCode]}</p>
+              <input type='password' name='password' minlength='${conf["password_min_length"]}' maxlength='${conf["password_max_length"]}' autocomplete='on'><br>
+          </label>
 
-    <row>
-      <a href="{url_for('signUp')}">${langDict["dontHaveAccountGoToSignUp"][langCode]}</a>
-    </row>
-  </column>
+          <label>
+              <p for='logIn'></p>
+              <input type='submit' name='logIn' value='${langDict["logIn"][langCode]}'>
+          </label>
+
+        </form>
+      </row>
+
+      <row>
+        <a href="{url_for('signUp')}">${langDict["dontHaveAccountGoToSignUp"][langCode]}</a>
+      </row>
+
+    </column>
+
+  </row>
 </container>
   `;
 
