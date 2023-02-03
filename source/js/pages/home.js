2fcc3df23aa5ede176cd749ca762f79b3dceb3e6
woXrooX <woXrooX@gmail.com>
2023-02-03 19:52:48 +0500

    Clean Up

--- source/js/pages/home.js
+++ source/js/pages/home.js
@@ -2,24 +2,7 @@
 
 export const TITLE = "Home";
 export default function home(){
-
-
-
   let data = `<container>Home</container>`;
-
-  let data2 = `
-<container>
-  <row>
-    <column class="w-75">C1</column>
-    <column class="w-25">C2</column>
-  </row>
-  <row>
-    <column class="w-100">C1</column>
-    <column class="w-5">C2</column>
-  </row>
-</container>
-  `;
-
 
   return data;
 
