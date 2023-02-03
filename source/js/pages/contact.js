0347f6d800e245fcae993ab5c72ef16014e2eaff
woXrooX <woXrooX@gmail.com>
2023-02-03 19:23:47 +0500

    OG contact us page

--- source/js/pages/contact.js
+++ source/js/pages/contact.js
@@ -1,87 +1,8 @@
 "use strict";
 
+export const TITLE = "Contact Us";
 export default function content(){
-  let dom = `
-<section style="padding: 10px 150px;">
-  <form>
-
-  <label>
-      <p for='username'>Number</p>
-      <input type='number' name='number'>
-  </label>
-
-    <label>
-        <p for='username'>Username</p>
-        <input type='text' name='username'>
-    </label>
-
-    <label>
-        <p for='password'>Password</p>
-        <input type='password' name='password'>
-    </label>
-
-    <label>
-        <p for='password'>E-Mail</p>
-        <input type='eMail' name='email'>
-    </label>
-
-    <label>
-      <p for='logIn'>Checkbox</p>
-      <input type='checkbox' name='checkbox' checked>
-    </label>
-
-    <fieldset for='gender'>
-
-      <legend>Radio</legend>
-
-      <label>
-        <input type="radio" name="gender" value="male">
-        Male
-      </label>
-
-      <label>
-        <input type="radio" name="gender" value="female">
-        Female
-      </label>
-
-    </fieldset>
-
-    <label>
-        <p for='logIn'>Color</p>
-        <input type='color' name='color'>
-    </label>
-
-    <label>
-      <p for='language'>Select</p>
-      <select name="language">
-        <option selected disabled>Choose</option>
-        <option value="v1">V1</option>
-        <option value="v2">V2</option>
-        <option value="v3">V3</option>
-      </select>
-    </label>
-
-    <label>
-        <p for='logIn'>Textarea</p>
-        <textarea rows="10"></textarea>
-    </label>
-
-    <label>
-        <p for='submit'></p>
-        <input type='submit' name='submit' value='Submit'>
-    </label>
-
-    <label>
-        <p for='button'></p>
-        <button type='submit' name='button'>Button</button>
-    </label>
-
-  </form>
-
-</section>
-  `;
-
-
+  let dom = `<h1>Contact Us</h1>`;
   return dom;
 
 }
