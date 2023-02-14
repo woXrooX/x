34566e9a2dc2521b4853a64ea09c2a44753f076b
woXrooX <woXrooX@gmail.com>
2023-02-15 02:04:59 +0500

    Tools to tools file

--- source/main.py
+++ source/main.py
@@ -5,7 +5,6 @@ import pathlib # APP_RUNNING_FROM
 import os # For Creating Folders, Checking if project.json exists
 import sys # for eixt
 # import stripe
-from functools import wraps # For pageGuard() Wrapper
 
 #################################################### GLOBAL appRunningFrom
 APP_RUNNING_FROM = pathlib.Path(__file__).parent.absolute()
@@ -54,64 +53,9 @@ PUBLIC_CONF = {
 
 
 #################################################### GLOBAL tools
-from python.tools.MySQL import MySQL
 # import python.tools.tools as tools
-
-"""
-
-@wraps(func)
-
-The functools.wraps function is a decorator used to preserve metadata of a decorated function,
-such as the name, docstring, and argument signature, to the wrapped function.
-When you use the functools.wraps decorator,
-it takes the original function as an argument and returns a new function that has the same metadata as the original function.
-
-"""
-
-# Page Guard
-def pageGuard(page):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            # Check If Page Exists
-            if page not in CONF["pages"]: return redirect(url_for("home"))
-
-            # Is Page Enabled
-            if CONF["pages"][page]["enabled"] == False: return redirect(url_for("home"))
-
-            # Looping Through Page's Allowed List
-            for allowed in CONF["pages"][page]["allowed"]:
-                # Only Allowed "unauthenticated" Users
-                if allowed == "unauthenticated" and "user" in session: return redirect(url_for("home"))
-
-                # Only Allowed "unauthorized" Users
-                if allowed == "unauthorized" and "user" not in session: return redirect(url_for("home"))
-
-                # Only Allowed "authorized" Users
-                # Retrive authorized_type_id From Database
-                # authorized_type_id = 5
-                # if allowed == "authorized":
-                #     if "user" not in session or "user" in session and session["user"]["type"] != authorized_type_id:
-                #         return redirect(url_for("home"))
-
-            return func(*args, **kwargs)
-
-        return wrapper
-
-    return decorator
-
-def publicSessionUser():
-    # Check If User In Session
-    if "user" not in session: return None
-
-    publicData = {
-        "username": session["user"]["username"],
-        "firstname": session["user"]["firstname"],
-        "lastname": session["user"]["lastname"]
-
-    }
-
-    return publicData
+from python.tools.MySQL import MySQL
+from python.tools.tools import pageGuard, publicSessionUser
 
 
 #################################################### URL
