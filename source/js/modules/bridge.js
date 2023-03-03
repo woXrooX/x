f8ee45cc260d027357ef7fbed77f656fa8ddd7c8
woXrooX <woXrooX@gmail.com>
2023-03-03 18:22:31 +0500

    Tool / Bridge 1. Bridge works unique for content=type equals json and default w/o mod to other type 2. Must be truthy value on url argument 3. Now returns error message instead just logging them

--- source/js/modules/bridge.js
+++ source/js/modules/bridge.js
@@ -1,29 +1,45 @@
 "use strict";
 
 ///////////////////////////// FETCH - Bridge
-export default async function bridge(url='', bodyData={}){
+// export default async function bridge(url='', data={}, contentType = null){
+export default async function bridge(url='', data=null, contentType = null){
+  // Check If url Is Not Falsy
+  if(!!url == false) return;
+
   try{
-    const response = await fetch(URL+url, {
-      method: 'POST',                               // *GET, POST, PUT, DELETE, etc.
-      mode: 'same-origin',                          // no-cors, *cors, same-origin
-      cache: 'force-cache',                         // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'include',                       // include, *same-origin, omit
-      headers: {
-        'Content-Type': 'application/json',          // 'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      redirect: 'follow',                           // manual, *follow, error
-      referrerPolicy: 'no-referrer',                // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(bodyData)                // body data type must match "Content-Type" header
+    const response = await fetch(url, {
+      method: 'POST',
+      mode: 'same-origin',
+      cache: 'force-cache',
+      credentials: 'include',
+
+      // Check If contentType == "application/json"
+      ...(contentType == "application/json") && {headers: {"Content-Type": "application/json"}},
+
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+
+      // Check If contentType == "application/json" Then Pass JSON.stringify(data) Else Just Data
+      body: (contentType == "application/json") ? JSON.stringify(data) : data
+
     });
+
     if(response.ok){
       const data = await response.json();
       return data;
+
     }else{
       console.warn("[Bridge] Response Error - code: "+response.status);
+      return response.status;
+
     }
+
   }catch(error){
     console.warn("[Bridge] "+error);
+    return error;
+
   }
+
 }
 
 // Make bridge() Usable W/O Importing It
