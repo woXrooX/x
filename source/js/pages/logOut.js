fa1dd32d3dc101485112e68eec9d5288b9a59b0f
woXrooX <woXrooX@gmail.com>
2023-03-10 22:02:15 +0500

    cauttion

--- source/js/pages/logOut.js
+++ source/js/pages/logOut.js
@@ -6,6 +6,10 @@ export default function content(){
   const dom = `
 <container>
   <row>
+    <p for='logOut'>${window.Lang.use("ifLogOutWontBeAbleTo")}</p>
+  </row>
+
+  <row>
     <column class="bc-2 p-5 bs radius">
       <form action="logOut" for="logOut">
         <label>
