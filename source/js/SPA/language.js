86784a7bb1ee22af35474d0825e34ce6a76f9085
woXrooX <woXrooX@gmail.com>
2023-03-10 18:19:34 +0500

    time to swith to window.Lang.use("keyword")

--- source/js/SPA/language.js
+++ source/js/SPA/language.js
@@ -1,18 +1,30 @@
 "use strict";
 
 export default class Language{
+
+  // Can Be Used For Translations And Just For Normal Use Cases
   static translate(keyword, code = window.langCode){
     try{
       return window.langDict[keyword][code];
 
     }catch(error){
       return `<b>Language Error:</b> ${error}<br>
-              <b>On Keyword:</b> ${keyword}`;
+              <b>On Keyword:</b> ${keyword}<br>
+              <b>Using Language Code:</b> ${code}`;
 
     }finally{}
 
   }
+
+  // Just Returns Text For The "keyword"
+  static use(keyword){ return Language.translate(keyword);}
+
 }
 
-// Make Language Usable W/O Importing It
+
+// Lang Is Alias To Language
+class Lang extends Language{}
+
+// Make Language And Lang Usable W/O Importing It
 window.Language = Language;
+window.Lang = Lang;
