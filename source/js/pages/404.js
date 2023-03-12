0196b0787ffcf70a27ae6bb5d959e5a296d73bff
woXrooX <woXrooX@gmail.com>
2023-03-12 21:08:24 +0500

    404 beautyfied

--- source/js/pages/404.js
+++ source/js/pages/404.js
@@ -4,8 +4,14 @@ export const TITLE = "Page Not Found";
 export default function content(){
 
   const dom = `
-<h1>404</h1>
-<p>Page Not Found!</p>
+<container>
+  <row>
+    <column class="w-25 bc-3 radius p-5 bs">
+      <h1>404</h1>
+      <h3 style="color: var(--color-text-secondary);">Page Not Found!</h3>
+    </column>
+  </row>
+</container>
   `;
 
   return dom;
