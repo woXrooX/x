67a58e531c41621315cfd92d5262e2e3917f18dd
woXrooX <woXrooX@gmail.com>
2023-03-13 15:41:22 +0500

    openai update

--- source/python/tools/OpenAI.py
+++ source/python/tools/OpenAI.py
@@ -3,6 +3,7 @@
 if __name__ != "__main__":
 
     from __main__ import CONF
+    from python.tools.response import response
 
     import openai
     import json
@@ -10,46 +11,29 @@ if __name__ != "__main__":
     class MySQL:
 
         @staticmethod
-        def prompt(prompt, model="text-davinci-003", temperature=0.1):
+        def prompt(
+            model="text-davinci-003",
+            prompt,
+            max_tokens=1000,
+            temperature=0.1
+        ):
             # Check If Feature Is Enabled
-            if "OpenAI" not in CONF: return "OpenAI Is Not Enabled!"
+            if "OpenAI" not in CONF: response(type="info", message="OpenAI Is Not Enabled!")
 
             # Check If prompt Is Valid
-            if not prompt: return "Invalid Prompt!"
+            if not prompt: return response(type="error", message="Invalid Prompt!")
 
+            openai.api_key = CONF["OpenAI"]["api_key"]
+            response = openai.Completion.create(
+              model="text-davinci-003",
+              prompt=prompt,
+              max_tokens=max_tokens,
+              temperature=0.3,
+              top_p=1,
+              n=1,
+              stream=False,
+              logprobs=None
+              # stop="\n"
+            )
 
-                openai.api_key = CONF["OpenAI"]["api_key"]
-                response = openai.Completion.create(
-                  model="text-davinci-003",
-                  prompt=prompt,
-                  max_tokens=1000,
-                  temperature=0.3,
-                  top_p=1,
-                  n=1,
-                  stream=False,
-                  logprobs=None
-                  # stop="\n"
-                )
-
-
-
-
-req = f"""text"""
-
-openai.api_key = "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"
-response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt=req,
-  max_tokens=1000,
-  temperature=0.3,
-  top_p=1,
-  n=1,
-  stream=False,
-  logprobs=None
-  # stop="\n"
-)
-
-print(response)
-
-with open("responses.txt", "w") as file:
-    file.write(response)
+            return response(type="success", data={"OpenAI": response})
