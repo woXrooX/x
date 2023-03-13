e77269f7eb15209f6f9565a42bb7f7b8786f7427
woXrooX <woXrooX@gmail.com>
2023-03-13 15:46:43 +0500

    OpenAI + try catch

--- source/python/tools/OpenAI.py
+++ source/python/tools/OpenAI.py
@@ -23,17 +23,21 @@ if __name__ != "__main__":
             # Check If prompt Is Valid
             if not prompt: return response(type="error", message="Invalid Prompt!")
 
-            openai.api_key = CONF["OpenAI"]["api_key"]
-            response = openai.Completion.create(
-              model="text-davinci-003",
-              prompt=prompt,
-              max_tokens=max_tokens,
-              temperature=0.3,
-              top_p=1,
-              n=1,
-              stream=False,
-              logprobs=None
-              # stop="\n"
-            )
+            try:
+                openai.api_key = CONF["OpenAI"]["api_key"]
+                response = openai.Completion.create(
+                  model="text-davinci-003",
+                  prompt=prompt,
+                  max_tokens=max_tokens,
+                  temperature=0.3,
+                  top_p=1,
+                  n=1,
+                  stream=False,
+                  logprobs=None
+                  # stop="\n"
+                )
 
-            return response(type="success", data={"OpenAI": response})
+                return response(type="success", data={"OpenAI": response})
+
+            except:
+                return response(type="error", message="OpenAIError")
