e5a41e805284b1b67ebe3169a2cf3dba1f39d19d
woXrooX <woXrooX@gmail.com>
2023-03-17 16:31:05 +0500

    + m, + default box

--- source/js/pages/404.js
+++ source/js/pages/404.js
@@ -4,9 +4,9 @@ export const TITLE = "Page Not Found";
 export default function content(){
 
   const dom = `
-<container>
+<container class="m-t-5">
   <row>
-    <column class="w-25 bc-3 radius p-5 bs">
+    <column class="w-25 box-default p-5">
       <h1>404</h1>
       <h3 style="color: var(--color-text-secondary);">Page Not Found!</h3>
     </column>
