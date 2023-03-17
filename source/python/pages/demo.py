bad8b00285aa66a8fa0094567bf4d4a90091be65
woXrooX <woXrooX@gmail.com>
2023-03-17 13:36:26 +0500

    bug fix

--- source/python/pages/demo.py
+++ source/python/pages/demo.py
@@ -4,8 +4,10 @@ from __main__ import app, request, render_template
 # Home Made
 from __main__ import CONF, EXTERNALS
 
+from python.tools.tools import pageGuard
 
 #################################################### Demo
 @app.route("/demo", methods=["GET"])
+@pageGuard("demo")
 def demo():
     if request.method == "GET": return render_template("index.html", **globals())
