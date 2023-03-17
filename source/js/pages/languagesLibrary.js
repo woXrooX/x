f387f20c45503e2a998d4d522db1b49f87c88adf
woXrooX <woXrooX@gmail.com>
2023-03-17 14:03:53 +0500

    + counter

--- source/js/pages/languagesLibrary.js
+++ source/js/pages/languagesLibrary.js
@@ -2,8 +2,12 @@
 
 export function before(){
   window.pageData.dom = "";
+  window.pageData.count = 0;
 
   for(const keyword in window.langDict){
+    // Counter
+    window.pageData.count++;
+
     window.pageData.dom += `
 <tr>
   <td>${keyword}</td>
@@ -23,6 +27,7 @@ export default function content(){
   let dom = `
 <container class="p-5">
   <row><h1>${window.Lang.use("languagesLibrary")}</h1></row>
+  <row class="p-2"><p>This app supports ${window.pageData.count} keywords</p></row>
 
   <row>
     <column class="box-default left p-1">
