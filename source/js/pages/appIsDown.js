a2156c75c7a41453861c6a6fa33f850d7bb5f411
woXrooX <woXrooX@gmail.com>
2023-03-17 16:13:42 +0500

    appIsDown Front

--- source/js/pages/appIsDown.js
+++ source/js/pages/appIsDown.js
@@ -7,8 +7,8 @@ export default function content(){
 <container>
   <row>
     <column class="w-25 box-default p-5">
-      <h1>Reason</h1>
-      <h3 style="color: var(--color-text-secondary);">Explanation</h3>
+      <h1>${window.CONF["default"]["appIsDown"]["reason"]}</h1>
+      <h3 style="color: var(--color-text-secondary);">${window.CONF["default"]["appIsDown"]["explanation"]}</h3>
     </column>
   </row>
 </container>
