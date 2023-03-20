e2ed58e9620bb1a1bbee2d23207bb5bf796e7e5d
woXrooX <woXrooX@gmail.com>
2023-03-20 15:46:41 +0500

    MVP ready

--- source/js/SPA/elements/tooltip.js
+++ source/js/SPA/elements/tooltip.js
@@ -18,19 +18,72 @@ export default class Tooltip extends HTMLElement{
     this.shadow = this.attachShadow({mode: 'closed'});
 
     Type: {
-      this.typeName = "warning";
-      const attributeType = this.getAttribute("type").toLowerCase();
-      if(["info", "warning", "error"].includes(attributeType)) this.typeName = attributeType;
+      this.type = "warning";
+      let attributeType = this.type;
+      if(this.hasAttribute("type") === true) attributeType = this.getAttribute("type").toLowerCase();
+      if(["info", "warning", "error"].includes(attributeType)) this.type = attributeType;
       else this.textContent = this.getAttribute("type");
     }
 
+    console.log(this.type);
+    console.log(this.textContent);
+
     CSS: {
         const style = document.createElement('style');
-        
+
         style.textContent = `
-        tooltip{
-          background-color: red;
+        tooltip{}
+
+        tooltip > icon{
+          cursor: help;
+
+          background-color: var(--color-${this.type});
+          color: white;
+          text-align: center;
+          padding: 3px 10px;
+          border-radius: 100%;
         }
+
+        icon:hover + content{
+          opacity: 1;
+          transform: translate(-50%, -120%);
+        }
+
+        tooltip > content{
+          pointer-events: none;
+
+          background-color: var(--color-brand);
+          color: var(--color-text-primary);
+
+          opacity: 0;
+
+          padding: var(--padding);
+          border-radius: var(--radius);
+          box-shadow: var(--shadow-light);
+
+          max-width: 30vw;
+          height: auto;
+
+          position: absolute;
+          transform: translate(-50%, -100%);
+
+          transition: var(--transition-velocity) ease-in-out;
+          transition-property: opacity, transform;
+
+        }
+
+        tooltip > content::after{
+          content: "";
+          position: absolute;
+          top: 100%;
+          left: 50%;
+          margin-left: -17px;
+          border-width: 5px;
+          border-style: solid;
+          border-color: var(--color-brand) transparent transparent transparent;
+
+        }
+
         `;
 
         this.shadow.appendChild(style);
@@ -40,31 +93,19 @@ export default class Tooltip extends HTMLElement{
     // Clone And Append Template
     this.shadow.appendChild(Tooltip.#template.content.cloneNode(true));
 
-    // If typeName === TRUE Append Type Specific Icon Else Append "warning" Icon
-    this.shadow.querySelector("toast>main>icon").innerHTML = !!ICONS[this.typeName] ? ICONS[this.typeName] : ICONS["warning"];
-
-    // InnerHTML "typeName"
-    this.shadow.querySelector("toast>main>type").innerHTML = Language.translate(this.typeName);
+    // If type === TRUE Append Type Specific Icon Else Append "warning" Icon
+    this.shadow.querySelector("tooltip>icon").innerHTML = !!ICONS[this.type] ? ICONS[this.type] : ICONS["warning"];
 
     // InnerHTML "textContent"
-    this.shadow.querySelector("toast>main>content").innerHTML = Language.translate(this.textContent);
+    this.shadow.querySelector("tooltip>content").innerHTML = Language.translate(this.textContent);
 
-    // InnerHTML Close Button Icon
-    this.shadow.querySelector("toast>dismiss").innerHTML = ICONS["close"];
-
-    // Remove Tooltip On Click Dismiss
-    // dismiss.onclick = ()=> this.remove(); // Bug w/ N sec removal
-    this.shadow.querySelector("toast>dismiss").onclick = ()=> this.style.display = "none";
   }
 
-  static new(type, content){
-    if(!!type === false || !!content === false) return;
+  static new(type, content, parentSelector){
+    if(!!type === false || !!content === false || !!parentSelector === false) return;
 
 
-    document.querySelector(Tooltip.#selector).innerHTML += `<x-toast type="${type}">${content}</x-toast>`;
-
-    // Auto Remove After N Seconds
-    setTimeout(()=>{document.querySelector(Tooltip.#selector).firstChild?.remove();}, Tooltip.#autoDismissTimer);
+    document.querySelector(parentSelector).innerHTML += `<x-tooltip type="${type}">${content}</x-tooltip>`;
 
   }
 }
