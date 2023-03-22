50425f70a6afd435ca6e22a7945e6dbb8abe8626
woXrooX <woXrooX@gmail.com>
2023-03-22 15:18:40 +0500

    + start and end methods

--- source/js/SPA/loading.js
+++ source/js/SPA/loading.js
@@ -4,19 +4,37 @@ export default class Loading{
   static selector = "body > loading";
   static #elementLoading = null;
 
-  static #exists(){
+  static {
     Loading.#elementLoading = document.querySelector(Loading.selector);
-    return !!Loading.#elementLoading;
 
   }
 
-  static done(){
+  static start(){
     // Check if body > loading exists
-    if(Loading.#exists() === false) return;
+    if(!!Loading.#elementLoading === false) return;
+    console.log("start");
 
+    // Set z-index To --z-loading
+    Loading.#elementLoading.style.zIndex = getComputedStyle(document.body).getPropertyValue('--z-loading');
+
+    // Change The Opacity To 1
+    Loading.#elementLoading.style.opacity = 1;
+
+  }
+
+  static end(){
+    // Check if body > loading exists
+    if(!!Loading.#elementLoading === false) return;
+    console.log("end");
+
+    // Change The Opacity To 0
     Loading.#elementLoading.style.opacity = 0;
+
+    // Wait For The Duration Of --transition-velocity
     setTimeout(()=>{
+      // Set z-index To --z-loading
       Loading.#elementLoading.style.zIndex = getComputedStyle(document.body).getPropertyValue('--z-minus');
+
     }, parseInt(getComputedStyle(document.body).getPropertyValue('--transition-velocity')));
 
   }
