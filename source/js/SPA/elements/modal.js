bb207dcc430d93e21c423bb9f93519f6f4da90b1
woXrooX <woXrooX@gmail.com>
2023-03-28 13:02:46 +0500

    + modal v0.5

---
+++ source/js/SPA/elements/modal.js
@@ -0,0 +1,167 @@
+// <x-modal>yzoken</x-modal>
+
+"use strict";
+
+export default class Modal extends HTMLElement{
+  static #template = document.createElement("template");
+
+  static {
+    Modal.#template.innerHTML = `
+      <modal>
+        <header>
+          <x-icon>x</x-icon>
+        </header>
+        <main></main>
+      </modal>
+      <trigger></trigger>
+    `;
+  }
+
+  constructor(){
+    super();
+
+    // Closed
+    this.shadow = this.attachShadow({mode: 'closed'});
+
+    this.shown = false;
+
+    CSS: {
+      const style = document.createElement('style');
+      style.textContent = `
+        modal{
+          pointer-events: none;
+
+          background-color: var(--color-surface-4);
+          opacity: 0;
+
+          width: 200px;
+          height: 200px;
+
+          border-radius: var(--radius);
+          box-shadow: var(--shadow-light);
+
+          position: absolute;
+          z-index: var(--z-modal);
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%) scale(0.8);
+
+          transition: var(--transition-velocity) ease-in-out;
+          transition-property: transform, opacity;
+
+        }
+
+        modal.show{
+          pointer-events: all;
+
+          opacity: 1;
+
+          transform: translate(-50%, -50%) scale(1);
+
+        }
+
+        modal.hide{
+          pointer-events: none;
+
+          opacity: 0;
+
+          transform: translate(-50%, -45%) scale(0.9);
+
+        }
+
+        modal > header{
+          width: 100%;
+          height: 35px;
+
+          display: flex;
+          justify-content: flex-end;
+
+        }
+
+        modal > main{
+          width: 100%;
+
+          padding: var(--padding);
+
+          display: grid;
+          place-items: center;
+
+        }
+
+
+      `;
+      this.shadow.appendChild(style);
+    }
+
+    // Clone And Append Template
+    this.shadow.appendChild(Modal.#template.content.cloneNode(true));
+    console.log(this.shadow);
+
+    // Content
+    this.shadow.querySelector("modal>main").innerHTML = this.innerHTML;
+
+    Interactions: {
+      // Instant Pop-Up
+      if(
+        !!this.hasAttribute("trigger") === false ||
+        !!this.hasAttribute("trigger") === true &&
+        this.getAttribute("trigger") === "auto"
+      ) setTimeout(()=>{this.#show();}, 500);
+
+      // Create Click Event
+      else if(this.getAttribute("trigger") === "click")
+
+        if(!!this.hasAttribute("type") === true && !!this.hasAttribute("value") === true)
+
+          if(this.getAttribute("type") === "icon"){
+            this.shadow.querySelector("trigger").innerHTML = `<x-icon>${this.getAttribute("value")}</x-icon>`;
+            this.shadow.querySelector("trigger>x-icon").onclick = this.#show;
+          }
+
+
+          else if(this.getAttribute("type") === "text")
+            this.shadow.querySelector("trigger").innerHTML = `<button>${this.getAttribute("value")}</button>`;
+
+
+
+      // Close On Cover Click
+      window.Cover.onClickExecute(this.#hide);
+
+      // Close On X Click
+      this.shadow.querySelector("modal>header>x-icon").onclick = this.#hide;
+
+    }
+
+  }
+
+  #show = ()=>{
+    if(this.shown === true) return;
+
+    this.shown = true;
+
+    this.shadow.querySelector("modal").classList.remove("hide");
+    this.shadow.querySelector("modal").classList.add("show");
+
+    window.Cover.show();
+
+  }
+
+  // Regular Function Missing The Context Of "this" When Passed To "window.Cover.onClickExecute"
+  #hide = ()=>{
+    if(this.shown === false) return;
+
+    this.shown = false;
+
+    this.shadow.querySelector("modal").classList.remove("show");
+    this.shadow.querySelector("modal").classList.add("hide");
+
+    window.Cover.hide();
+
+  }
+
+};
+
+window.customElements.define('x-modal', Modal);
+
+// Make Modal Usable W/O Importing It
+window.Modal = Modal;
