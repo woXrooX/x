b4d8d3e452829d991c614b23e9c363763a2b53e8
woXrooX <woXrooX@gmail.com>
2023-03-29 14:07:20 +0500

    + some of the vars

--- source/js/SPA/css.js
+++ source/js/SPA/css.js
@@ -1,11 +1,82 @@
-const buttonStyles = `
-  background-color: blue;
-  color: white;
-  padding: 10px;
-  border-radius: 5px;
-`;
+// const buttonStyles = `
+//   background-color: blue;
+//   color: white;
+//   padding: 10px;
+//   border-radius: 5px;
+// `;
+//
+// const button = document.createElement('button');
+// button.style.cssText = buttonStyles;
+// button.textContent = 'Click me';
+// document.body.appendChild(button);
 
-const button = document.createElement('button');
-button.style.cssText = buttonStyles;
-button.textContent = 'Click me';
-document.body.appendChild(button);
+"use strict";
+
+export default class CSS{
+  static var = {
+
+    zIndex: {
+      minus: -1,
+      body: 1,
+      footer: 2,
+      main: 3,
+      boxes: 4,
+      header: 5,
+      cover: 6,
+      modal: 7,
+      menu: 8,
+      tooltip: 9,
+      toasts: 10,
+      loading: 11,
+      urgent: 12
+    },
+
+    transition: {
+      velocity: "200ms"
+    },
+
+    header: {
+      height: "40px"
+    },
+
+    footer: {
+      height: "80px"
+    },
+
+    padding: {
+      default: "5px"
+    },
+
+    gap: {
+      default: "20px"
+    },
+
+    radius: {
+      default: "5px"
+    },
+
+    blur: {
+      default: "10px"
+    },
+
+    screenSize: {
+      phone: "600px",
+      tablet: "768px",
+      desktop: "992px",
+      tv: "1200px"
+    },
+
+    color: {
+      success: "hsla(120, 100%, 25%, 1);",    // rgba(0, 128, 0, 1)
+      info: "hsla(211.059, 100%, 50%, 1);",   // rgba(0, 123, 255, 1)
+      warning: "hsla(46.286, 100%, 48%, 1);", // rgba(245, 189, 0, 1)
+      error: "hsla(357.303, 82%, 57%, 1);"    // rgba(235, 57, 65, 1)
+
+    }
+
+  };
+  
+}
+
+// Make CSS Usable W/O Importing It
+window.CSS = CSS;
