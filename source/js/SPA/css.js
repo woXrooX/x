553c8c2c57de509b34667d2b139ee7b02a35db1b
woXrooX <woXrooX@gmail.com>
2023-03-30 22:22:40 +0500

    master styles are ready

--- source/js/SPA/css.js
+++ source/js/SPA/css.js
@@ -21,6 +21,12 @@ export default class CSS{
 
   static val = {
 
+    font: {
+      size: {
+        default: "15"     // px
+      }
+    },
+
     zIndex: {
       minus: -1,
       body: 1,
@@ -98,14 +104,9 @@ export default class CSS{
 
   static {
     CSS.dark();
-    CSS.light();
-    CSS.doMath();
+    // CSS.light();
 
-  }
-
-  static doMath(){
-    // Not Sure If We Ever Used This
-    CSS.val.color.accent = CSS.val.color.main;
+    window.document.querySelector("style[for=INTERNAL_CSS]").innerText = CSS.master();
 
   }
 
@@ -179,6 +180,264 @@ export default class CSS{
 
     CSS.val.shadow.default = `0px 10px 10px -5px hsla(${CSS.val.color.science.hue}deg 10% 2% / 0.2)`;
 
+  }
+
+  //////////// Styles
+  static master(){
+    const all = `
+      :root, html, body, *, *::before, *::after{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: none;
+        text-decoration: none;
+        /* user-select: none; /* Disable copy */
+        /* pointer-events: none; */
+
+        -webkit-tap-highlight-color: rgba(0, 0, 0, 0); /* Mobile link click effect disabling */
+        /* -webkit-tap-highlight-color: transparent; */
+      }
+    `;
+
+    const scrollbar = `
+      /* Removing Default Scroll Bar START */
+      *{
+        -ms-overflow-style: none;
+        scrollbar-width: none;
+      }
+      *::-webkit-scrollbar{
+        display:none;
+      }
+      /* Removing Default Scroll Bar END */
+    `;
+
+    const defaults = `
+      body {
+        background-color: ${CSS.val.color.surface["1"]};
+        color: ${CSS.val.color.text.primary};
+        font-family: Quicksand;
+        font-size: 15px;
+
+      }
+
+      body > loading{
+        background-color: ${CSS.val.color.surface["1"]};
+
+        width: 100vw;
+        height: 100vh;
+
+        position: fixed;
+        z-index: ${CSS.val.zIndex.loading};
+
+        transition: ${CSS.val.transition.velocity} opacity ease-in-out;
+
+      }
+      body > loading::after{
+        content: '';
+        background-color: transparent;
+        height: 20vh;
+        width: 20vh;
+        border-radius: 100%;
+        border: 0px solid transparent;
+        border-right: 2px solid ${CSS.val.color.text.primary};
+
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        transform-origin: center;
+
+        animation: loading 500ms infinite ease-in-out;
+
+      }
+      @keyframes loading{
+        0%{transform: translate(-50%, -50%) rotate(0deg);}
+        100%{transform: translate(-50%, -50%) rotate(720deg);}
+
+      }
+
+      body > toasts{
+        height: auto;
+        max-height: 100vh;
+        width: 400px;
+        /* padding: ${CSS.val.padding.default}; */
+        padding: 5px 5px 20px 5px;
+        overflow-y: scroll;
+
+        display: flex;
+        flex-direction: column-reverse;
+        gap: ${CSS.val.gap.default};
+
+        /* pointer-events: none; */
+
+        position: fixed;
+        top: 0px;
+        right: 0px;
+        z-index: ${CSS.val.zIndex.toasts};
+
+      }
+
+      body > menu{
+        --menu-background-color: hsla(${CSS.val.color.science.hue}, 5%, 20%, 1);
+        background-color: var(--menu-background-color);
+        height: 100vh;
+        width: auto;
+
+        overflow-y: scroll;
+
+        display: grid;
+        grid-template-rows: auto 2fr auto;
+        justify-items: start;
+        gap: ${CSS.val.gap.default};
+
+        position: fixed;
+        z-index: ${CSS.val.zIndex.menu};
+        top: 0;
+        left: 0;
+        transform: translate(-100%, 0);
+        transition-duration: ${CSS.val.transition.velocity};
+        transition-timing-function: ease;
+        transition-property: all;
+
+      }
+
+      body > menu > header{
+        width: 100%;
+
+        padding: calc(${CSS.val.padding.default} * 2);
+
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-end;
+
+      }
+      body > menu > header > div{
+        user-select: none;
+
+        width: 30px;
+        height: 30px;
+      }
+      body > menu > main{
+        width: 100%;
+        padding: calc(${CSS.val.padding.default} * 2);
+
+        display: flex;
+        flex-direction: column;
+        gap: calc(${CSS.val.gap.default} / 2);
+
+      }
+      body > menu > main > a{
+        /* background-color: var(--menu-background-color); */
+
+        /* filter: brightness(70%); */
+
+        color: white;
+        font-size: 2rem;
+
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        gap: ${CSS.val.gap.default};
+
+        border: 1px solid transparent;
+        border-radius: ${CSS.val.radius.default};
+        padding: ${CSS.val.padding.default} calc(${CSS.val.padding.default} * 2);
+
+        transition: ${CSS.val.transition.velocity} ease-in-out;
+        transition-property: filter, background-color, border;
+
+      }
+      body > menu > main > a:where([active], :hover){
+        background-color: var(--menu-background-color);
+        filter: brightness(120%);
+
+      }
+      body > menu > main > a:where([active]){
+        border: 1px solid white;
+
+      }
+      body > menu > main > a > x-icon{
+        height: 40px;
+        width: 40px;
+
+      }
+
+      body > menu > footer{}
+
+      body > cover{
+        pointer-events:auto;
+
+        background: ${CSS.val.color.cover};
+        backdrop-filter: blur(${CSS.val.blur.default});
+        opacity: 0;
+        width: 100vw;
+        height: 100vh;
+
+        position: fixed;
+        z-index: ${CSS.val.zIndex.minus};
+
+        transition: ${CSS.val.transition.velocity} opacity;
+
+      }
+
+      body > header{
+        background-color: ${CSS.val.color.main};
+        color: white;
+
+        width: 100%;
+        height: ${CSS.val.header.height};
+        padding: ${CSS.val.padding.default};
+
+        position: sticky;
+        top: 0;
+        left: 0;
+        z-index: ${CSS.val.zIndex.header};
+
+        display: grid;
+        grid-template-columns: auto 3fr;
+
+        place-items: center;
+
+      }
+      body > header > x-icon[for=menu]{
+        height: calc(${CSS.val.header.height} - ${CSS.val.padding.default} * 2);
+        width: calc(${CSS.val.header.height} - ${CSS.val.padding.default} * 2);
+
+      }
+
+
+      body > main {
+        width: 100vw;
+        min-height: calc(100vh - ${CSS.val.header.height} - ${CSS.val.footer.height});
+
+      }
+
+      body > footer {
+        background-color: ${CSS.val.color.surface["2"]};
+        color: ${CSS.val.color.text.primary};
+
+        width: 100%;
+        height: ${CSS.val.footer.height};
+        padding: ${CSS.val.padding.default};
+
+        display: grid;
+        place-items: center;
+
+      }
+
+    `;
+
+    return `
+      ${all}
+      ${scrollbar}
+      ${defaults}
+
+    `;
+  }
+
+  static common(){
+
+    return ``;
   }
 
 }
