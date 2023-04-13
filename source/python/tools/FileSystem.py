0a5dd43b75ad6602a2da22bae6897be971bde136
woXrooX <woXrooX@gmail.com>
2023-04-13 20:48:12 +0500

    moved to logger

--- source/python/tools/FileSystem.py
+++ source/python/tools/FileSystem.py
@@ -1,78 +1,77 @@
 if __name__ != "__main__":
     import os
     from main import APP_RUNNING_FROM, PARENT_RUNNING_FROM
+    from python.tools.Logger import Log
 
-    class FileStructure:
+    class FileSystem:
         @staticmethod
         def init():
             ################ Folders
-            print("""
---------------------------------------
--------------------------------------- Creating Folders
---------------------------------------
-            """)
+            Log.center("Creating Folders START", '-')
 
             ## x/source/assets
-            FileStructure.createFolder(f'{APP_RUNNING_FROM}/assets/')
+            FileSystem.createFolder(f'{APP_RUNNING_FROM}/assets/')
 
             ## CSS
-            FileStructure.createFolder(f'{PARENT_RUNNING_FROM}/CSS/')
+            FileSystem.createFolder(f'{PARENT_RUNNING_FROM}/CSS/')
 
             ## fonts
-            FileStructure.createFolder(f'{PARENT_RUNNING_FROM}/fonts/')
+            FileSystem.createFolder(f'{PARENT_RUNNING_FROM}/fonts/')
 
             ## JS
-            FileStructure.createFolder(f'{PARENT_RUNNING_FROM}/JS/')
+            FileSystem.createFolder(f'{PARENT_RUNNING_FROM}/JS/')
 
             ## pages
-            FileStructure.createFolder(f'{PARENT_RUNNING_FROM}/pages/')
-            FileStructure.createFolder(f'{PARENT_RUNNING_FROM}/pages/back')
-            FileStructure.createFolder(f'{PARENT_RUNNING_FROM}/pages/front')
+            FileSystem.createFolder(f'{PARENT_RUNNING_FROM}/pages/')
+            FileSystem.createFolder(f'{PARENT_RUNNING_FROM}/pages/back')
+            FileSystem.createFolder(f'{PARENT_RUNNING_FROM}/pages/front')
 
             ## SVG
-            FileStructure.createFolder(f'{PARENT_RUNNING_FROM}/SVG/')
+            FileSystem.createFolder(f'{PARENT_RUNNING_FROM}/SVG/')
+
+            Log.center("Creating Folders END", '-')
 
             ################ Files
-            print("""
---------------------------------------
--------------------------------------- Creating Files
---------------------------------------
-            """)
+            Log.line()
+            Log.center("Creating Files START", '-')
+
             # project.json
-            FileStructure.createFile(f"{PARENT_RUNNING_FROM}/project.json", "{}")
+            FileSystem.createFile(f"{PARENT_RUNNING_FROM}/project.json", "{}")
 
 
             # languageDictionary.json
-            FileStructure.createFile(f"{PARENT_RUNNING_FROM}/languageDictionary.json", '{"theX": {"en": "The X"}}')
+            FileSystem.createFile(f"{PARENT_RUNNING_FROM}/languageDictionary.json", '{"theX": {"en": "The X"}}')
 
             # pages/back/home.py
-            FileStructure.createFile(f"{PARENT_RUNNING_FROM}/pages/back/home.py")
+            FileSystem.createFile(f"{PARENT_RUNNING_FROM}/pages/back/home.py")
 
             # pages/front/home.js
-            FileStructure.createFile(f"{PARENT_RUNNING_FROM}/pages/front/home.js")
+            FileSystem.createFile(f"{PARENT_RUNNING_FROM}/pages/front/home.js")
 
             # styles.css
-            FileStructure.createFile(f"{PARENT_RUNNING_FROM}/CSS/styles.css")
+            FileSystem.createFile(f"{PARENT_RUNNING_FROM}/CSS/styles.css")
+
+            Log.center("Creating Files END", '-')
+            Log.line()
 
         @staticmethod
         def createFolder(path):
             try:
-                print(f"Folder: {path}")
+                Log.info(f"Folder: {path}")
                 os.makedirs(f'{path}', mode=0o777, exist_ok=True)
 
             except:
-                print(f"Error: Could Not Create The Folder: {path}")
+                Log.error(f"Error: Could Not Create The Folder: {path}")
                 sys.exit()
 
         @staticmethod
         def createFile(pathNameExtension, content = ""):
             if not os.path.exists(f"{pathNameExtension}"):
-
                 try:
-                    print(f"File: {pathNameExtension}")
+                    Log.info(f"File: {pathNameExtension}")
                     with open(f'{pathNameExtension}', 'w') as f:
                         f.write(f"{content}")
 
                 except:
-                    print(f"Error: Could Not Create The File: {path}")
+                    Log.error(f"Error: Could Not Create The File: {path}")
                     sys.exit()
