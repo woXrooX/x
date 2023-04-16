914869f7e9c76333bdf73c54732f9778b5c34767
woXrooX <woXrooX@gmail.com>
2023-04-16 23:46:39 +0500

    FileSystem class is responsible for user filesystem as well

--- source/python/tools/FileSystem.py
+++ source/python/tools/FileSystem.py
@@ -1,6 +1,6 @@
 if __name__ != "__main__":
     import os
-    from main import APP_RUNNING_FROM, PARENT_RUNNING_FROM
+    from main import APP_RUNNING_FROM, PARENT_RUNNING_FROM, session
     from python.tools.Logger import Log
 
     class FileSystem:
@@ -75,3 +75,30 @@ if __name__ != "__main__":
                 except:
                     Log.error(f"Could Not Create The File: {path}")
                     sys.exit()
+
+        @staticmethod
+        def initUserFolders():
+            # Check If User Is In Session
+            if "user" not in session: return False
+
+            # Must Match With The Path In .gitignore
+            # source/users/id/...
+            path = f'{APP_RUNNING_FROM}/users/{session["user"]["id"]}/'
+
+            # Try To Create User Folders
+            try:
+                # ID
+                os.makedirs(f'{path}', mode=0o777, exist_ok=True)
+
+                # Images
+                os.makedirs(f'{path}images', mode=0o777, exist_ok=True)
+
+                # PDF
+                os.makedirs(f'{path}PDF', mode=0o777, exist_ok=True)
+
+                return True
+
+            except:
+                Log.error(f"Could Not Create User Folder(s) @: {path}")
+
+                return False
