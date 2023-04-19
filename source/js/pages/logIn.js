b4a91388394e06b6afeb741884ff5795e81b25d2
woXrooX <woXrooX@gmail.com>
2023-04-19 23:01:45 +0500

    Modern way of using language class

--- source/js/pages/logIn.js
+++ source/js/pages/logIn.js
@@ -12,32 +12,32 @@ export default function logIn(){
     <column class="w-50 box-default p-5">
 
       <row>
-        <h1>${window.langDict["logIn"][window.langCode]}</h1>
+        <h1>${window.Lang.use("logIn")}</h1>
       </row>
 
       <row>
         <form action="logIn" for="logIn">
 
           <label>
-              <p for='eMail'>${window.langDict["eMail"][window.langCode]}</p>
+              <p for='eMail'>${window.Lang.use("eMail")}</p>
               <input type='email' name='eMail'>
           </label>
 
           <label>
-              <p for='password'>${window.langDict["password"][window.langCode]}</p>
+              <p for='password'>${window.Lang.use("password")}</p>
               <input type='password' name='password' minlength='${window.CONF["password_min_length"]}' maxlength='${window.CONF["password_max_length"]}'><br>
           </label>
 
           <label>
               <p for='logIn'></p>
-              <input type='submit' name='logIn' value='${window.langDict["logIn"][window.langCode]}'>
+              <input type='submit' name='logIn' value='${window.Lang.use("logIn")}'>
           </label>
 
         </form>
       </row>
 
       <row>
-        <a href="/signUp">${window.langDict["dontHaveAccountGoToSignUp"][window.langCode]}</a>
+        <a href="/signUp">${window.Lang.use("dontHaveAccountGoToSignUp")}</a>
       </row>
 
     </column>
