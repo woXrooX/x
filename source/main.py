58a6feaa768e03566037268d01f2bf4787f9c6ae
woXrooX <woXrooX@gmail.com>
2023-04-19 23:06:20 +0500

    Now file system will lift more weight so main.py is much cleaner 1. Configuration files are handled here 2. External CSS files are handled here 3. External SVGs are handled here 4. Pages coppyings are done here 5. Creating config and merging with external config are done here 6. Main is cleaned up 7. External and Internal lang dicts are handled here

--- source/main.py
+++ source/main.py
@@ -1,195 +1,37 @@
 #################################################### IMPORTS
 from flask import Flask, render_template, request, session, redirect, url_for, make_response
 import json, yaml, re, html
-import pathlib # APP_RUNNING_FROM
+import pathlib # X_RUNNING_FROM
 import os # For Creating Folders, Checking if project.json exists
 import sys # for eixt
 import shutil # To Copy
 # import stripe
 
-#################################################### GLOBAL APP_RUNNING_FROM
-APP_RUNNING_FROM = pathlib.Path(__file__).parent.absolute()
+#################################################### GLOBAL X_RUNNING_FROM
+X_RUNNING_FROM = pathlib.Path(__file__).parent.absolute()
 
-
-#################################################### GLOBAL PARENT_RUNNING_FROM
-# Go Back Two Times From "APP_RUNNING_FROM"
-PARENT_RUNNING_FROM = os.path.abspath(os.path.join(APP_RUNNING_FROM, '../..'))
+#################################################### GLOBAL PROJECT_RUNNING_FROM
+# Go Back Two Times From "X_RUNNING_FROM"
+PROJECT_RUNNING_FROM = os.path.abspath(os.path.join(X_RUNNING_FROM, '../..'))
 
 #################################################### Draw "The X"
 from python.tools.Logger import Log
 Log.clear()
 Log.brand()
 
-
-################################################################
-################################################################ Initializing File Structure START
-################################################################
+#################################################### Initializing File Structure
 from python.tools.FileSystem import FileSystem
 FileSystem.init()
 
-
-################################################################
-################################################################ Initializing File Structure END
-################################################################
-
-
-################################################################
-################################################################ Required Files And Folders To Run The Scipt START
-################################################################
-
-
-#################################################### project.json
-try:
-    with open(f"{PARENT_RUNNING_FROM}/project.json", 'r') as file:
-        PROJECT = json.load(file)
-
-    Log.success("Loaded: project.json")
-
-except:
-    Log.error("Could Not Reading The project.json")
-    sys.exit()
-
-
-#################################################### Copying Pages (Back-End)
-try:
-    files = os.listdir(PARENT_RUNNING_FROM+"/pages/back")
-
-    for file in files:
-        shutil.copy(PARENT_RUNNING_FROM+"/pages/back/"+file, str(APP_RUNNING_FROM)+"/python/pages")
-
-    Log.success("Pages Are Copied (Back-End)")
-
-except:
-    Log.error("Could Not Copy The Pages (Back-End)")
-
-    sys.exit()
-
-
-#################################################### Copying Pages (Front-End)
-try:
-    files = os.listdir(PARENT_RUNNING_FROM+"/pages/front")
-
-    for file in files:
-        shutil.copy(PARENT_RUNNING_FROM+"/pages/front/"+file, str(APP_RUNNING_FROM)+"/js/pages")
-
-    Log.success("Pages Are Copied (Front-End)")
-
-except:
-    Log.error("Could Not Copy The Pages (Front-End)")
-
-    sys.exit()
-
-
-################################################################
-################################################################ Required Files And Folders To Run The Scipt END
-################################################################
-
-
-################################################################
-################################################################ Not So Required Files And Folders To Run The Scipt START
-################################################################
-
-#################################################### EXTERNALS
-EXTERNALS = {
-    "SVG": {},
-    "CSS": None
-}
-
-#################################################### PARENT_RUNNING_FROM/fonts
-try:
-    files = os.listdir(PARENT_RUNNING_FROM+"/fonts")
-
-    for file in files:
-        shutil.copy(PARENT_RUNNING_FROM+"/fonts/"+file, str(APP_RUNNING_FROM)+"/fonts")
-
-    Log.success("Fonts Are Copied")
-
-except:
-    Log.error("Could Not Copy The Fonts")
-
-
-#################################################### PARENT_RUNNING_FROM/SVG
-# SVG FIles
-for file in os.listdir(f'{PARENT_RUNNING_FROM}/SVG'):
-    # Check If File Is A SVG File
-    if not file.endswith(".svg"): continue
-
-    try:
-        with open(f'{PARENT_RUNNING_FROM}/SVG/{file}', "r") as svg:
-            EXTERNALS["SVG"][os.path.splitext(file)[0]] = svg.read()
-
-        Log.success(f"SVG File Loaded: {file}")
-
-    except:
-        Log.error(f"Could Not Load The SVG File: {file}")
-
-
-#################################################### Load External CSS
-try:
-    with open(f'{PARENT_RUNNING_FROM}/CSS/styles.css', "r") as css:
-        EXTERNALS["CSS"] = css.read()
-
-    Log.success("External CSS Files Are Loaded")
-
-except:
-    Log.error("Could Not Read The External CSS")
-
-
-################################################################
-################################################################ Not So Required Files And Folders To Run The Scipt END
-################################################################
-
-
-#################################################### Default Config
-with open(f"{APP_RUNNING_FROM}/yaml/config.yaml", 'r') as file:
-    CONF = yaml.safe_load(file)
-
-#### Merge Project Dependent Configurations To Default Configurations. Override Defaults
-# Database
-if "database" in PROJECT:
-    if "database" in CONF: CONF["database"].update(PROJECT["database"])
-    else: CONF["database"] = PROJECT["database"]
-
-# eMail
-if "eMail" in PROJECT:
-    CONF["eMail"].update(PROJECT["eMail"])
-
-# Defaults
-if "default" in PROJECT:
-    if "default" in CONF: CONF["default"].update(PROJECT["default"])
-    else: CONF["default"] = PROJECT["default"]
-
-# Menu
-if "menu" in PROJECT:
-    CONF["menu"] = PROJECT["menu"]
-
-# Pages
-if "pages" in PROJECT:
-    CONF["pages"] = PROJECT["pages"]
-
-# OpenAI
-if "OpenAI" in PROJECT:
-    CONF["OpenAI"] = PROJECT["OpenAI"]
-
-# Public Version Of CONF (Minus Senstive Data)
-PUBLIC_CONF = {
-    "default": CONF["default"],
-    "menu": CONF["menu"],
-    "pages": CONF["pages"],
-    "username": CONF["username"],
-    "password": CONF["password"],
-    "phoneNumber": CONF["phoneNumber"],
-
-}
-
+#################################################### Imprting Global Data Holders
+from python.tools.FileSystem import CONF, LANG_DICT, PROJECT, PROJECT_CSS, PROJECT_SVG, PROJECT_LANG_DICT, PUBLIC_CONF
 
 #################################################### URL
 URL = f'{CONF["URL"]["prefix"]}://{CONF["URL"]["domain_name"]}:{CONF["URL"]["port"]}/'
 
-
 #################################################### GLOBAL MySQL
 from python.tools.MySQL import MySQL
-# If Database Enabled Then Set Up
+# If Database Enabled Then Set It Up
 if "database" in CONF and CONF["database"]["enabled"] == True:
     MySQL.setUp(
         CONF["database"]["user"],
@@ -199,7 +41,6 @@ if "database" in CONF and CONF["database"]["enabled"] == True:
         CONF["database"]["charset"],
         CONF["database"]["collate"]
     )
-
 
 #################################################### GLOBAL user_types
 USER_TYPES = {}
@@ -211,37 +52,11 @@ if "database" in CONF and CONF["database"]["enabled"] == True:
         # Making USER_TYPES accessible by keyword like "root" or "dev"
         for user_type in dataFetched: USER_TYPES[user_type["name"]] = user_type
 
-
-#################################################### GLOBAL tools
-# import python.tools.tools as tools
-# from python.tools.tools import pageGuard, publicSessionUser
-
-
 #################################################### GLOBAL language
 ### languages
 # with MySQL(False) as db:
 #     db.execute(f"SELECT * FROM languages")
 #     languages = db.fetchAll()
-
-### language Dictionary
-with open(f'{APP_RUNNING_FROM}/json/languageDictionary.json', encoding="utf8") as file:
-    langDict = json.load(file)
-
-
-#################################################### External Language Dictionary
-PROJECT_LANG_DICT = {}
-
-try:
-    with open(f"{PARENT_RUNNING_FROM}/languageDictionary.json", 'r') as file:
-        PROJECT_LANG_DICT = json.load(file)
-
-    Log.success("languageDictionary.json Is Loaded")
-
-except:
-    Log.error("Could Not Read The External languageDictionary.json")
-
-# Override The langDict With External "languageDictionary.json"
-langDict.update(PROJECT_LANG_DICT)
 
 
 #################################################### GLOBAL currencies
@@ -295,7 +110,7 @@ app.secret_key = CONF["flask"]["secret_key"]
 #     return None
 
 
-#################################################### Dynamically Improting All Pages
+#################################################### Dynamically Imprting All Pages
 from python.pages import *
 
 
--- source/python/tools/FileSystem.py
+++ source/python/tools/FileSystem.py
@@ -1,72 +1,36 @@
 if __name__ != "__main__":
     import os
     import shutil
-    from main import APP_RUNNING_FROM, PARENT_RUNNING_FROM, session
+    import json
+    import yaml
+    from main import X_RUNNING_FROM, PROJECT_RUNNING_FROM, session
     from python.tools.Logger import Log
 
+
+    CONF = {}
+    LANG_DICT = {}
+
+    PROJECT = {}
+    PROJECT_CSS = None
+    PROJECT_SVG = {}
+    PROJECT_LANG_DICT = {}
+
+    PUBLIC_CONF = {}
+
     class FileSystem:
+        ####### Helpers
         @staticmethod
-        def init():
-            ################ Folders
-            Log.center("Creating Folders START", '-')
-
-            ## x/source/assets
-            FileSystem.createFolder(f'{APP_RUNNING_FROM}/assets/')
-
-            ## CSS
-            FileSystem.createFolder(f'{PARENT_RUNNING_FROM}/CSS/')
-
-            ## fonts
-            FileSystem.createFolder(f'{PARENT_RUNNING_FROM}/fonts/')
-
-            ## JS
-            FileSystem.createFolder(f'{PARENT_RUNNING_FROM}/JS/')
-
-            ## pages
-            FileSystem.createFolder(f'{PARENT_RUNNING_FROM}/pages/')
-            FileSystem.createFolder(f'{PARENT_RUNNING_FROM}/pages/back')
-            FileSystem.createFolder(f'{PARENT_RUNNING_FROM}/pages/front')
-
-            ## SVG
-            FileSystem.createFolder(f'{PARENT_RUNNING_FROM}/SVG/')
-
-            Log.center("Creating Folders END", '-')
-
-            ################ Files
-            Log.line()
-            Log.center("Creating Files START", '-')
-
-            # project.json
-            FileSystem.createFile(f"{PARENT_RUNNING_FROM}/project.json", "{}")
-
-
-            # languageDictionary.json
-            FileSystem.createFile(f"{PARENT_RUNNING_FROM}/languageDictionary.json", '{"theX": {"en": "The X"}}')
-
-            # pages/back/home.py
-            FileSystem.createFile(f"{PARENT_RUNNING_FROM}/pages/back/home.py")
-
-            # pages/front/home.js
-            FileSystem.createFile(f"{PARENT_RUNNING_FROM}/pages/front/home.js")
-
-            # styles.css
-            FileSystem.createFile(f"{PARENT_RUNNING_FROM}/CSS/styles.css")
-
-            Log.center("Creating Files END", '-')
-            Log.line()
-
-        @staticmethod
-        def createFolder(path):
+        def createFolder(path, strict = True):
             try:
                 Log.info(f"Folder: {path}")
                 os.makedirs(f'{path}', mode=0o777, exist_ok=True)
 
             except:
                 Log.error(f"Could Not Create The Folder: {path}")
-                sys.exit()
+                if strict is True: sys.exit()
 
         @staticmethod
-        def createFile(pathNameExtension, content = ""):
+        def createFile(pathNameExtension, content = "", strict = True):
             if not os.path.exists(f"{pathNameExtension}"):
                 try:
                     Log.info(f"File: {pathNameExtension}")
@@ -75,8 +39,313 @@ if __name__ != "__main__":
 
                 except:
                     Log.error(f"Could Not Create The File: {path}")
-                    sys.exit()
 
+                    if strict is True: sys.exit()
+
+        @staticmethod
+        def copyFile(fromPath, toPath, file, strict = True):
+            try:
+                shutil.copy(f"{fromPath}/{file}", toPath)
+                Log.info(f"File Copied: {file}")
+
+            except:
+                Log.error(f"Could Not Copy The File: {file}")
+                if strict is True: sys.exit()
+
+        @staticmethod
+        # Will Copy All The Files In A Folder
+        def copyFiles(fromPath, toPath, extension = None, strict = True):
+            try:
+                files = os.listdir(fromPath)
+
+                for file in files:
+                    # Add Check Here If Extension Passed Copy Files Only With That Given Extension
+
+                    FileSystem.copyFile(fromPath, toPath, file)
+
+                Log.success(f"Files Are Copied To: {toPath}")
+
+                return True
+
+            except:
+                Log.error(f"Could Not Copy The Files @: {fromPath}")
+
+                # If Strict Then Stop The Server Else Return False So Executor Can Handle
+                if strict is True: sys.exit()
+                else: return False
+
+
+        ############## System
+        # Initiate System Files And Folders
+        @staticmethod
+        def init():
+            ################################ Creating
+            ################ Folders
+            Log.center("Creating Folders", '-')
+
+            ## x/source/assets
+            FileSystem.createFolder(f'{X_RUNNING_FROM}/assets/')
+
+            ## CSS
+            FileSystem.createFolder(f'{PROJECT_RUNNING_FROM}/CSS/')
+
+            ## fonts
+            FileSystem.createFolder(f'{PROJECT_RUNNING_FROM}/fonts/')
+
+            ## JS
+            FileSystem.createFolder(f'{PROJECT_RUNNING_FROM}/JS/')
+
+            ## pages
+            FileSystem.createFolder(f'{PROJECT_RUNNING_FROM}/pages/')
+            FileSystem.createFolder(f'{PROJECT_RUNNING_FROM}/pages/back')
+            FileSystem.createFolder(f'{PROJECT_RUNNING_FROM}/pages/front')
+
+            ## SVG
+            FileSystem.createFolder(f'{PROJECT_RUNNING_FROM}/SVG/')
+
+            Log.line()
+
+            ################ Files
+            Log.center("Creating Files", '-')
+
+            # project.json
+            FileSystem.createFile(f"{PROJECT_RUNNING_FROM}/project.json", "{}")
+
+            # languageDictionary.json
+            FileSystem.createFile(f"{PROJECT_RUNNING_FROM}/languageDictionary.json", '{"theX": {"en": "The X"}}')
+
+            # pages/back/home.py
+            FileSystem.createFile(f"{PROJECT_RUNNING_FROM}/pages/back/home.py")
+
+            # pages/front/home.js
+            FileSystem.createFile(f"{PROJECT_RUNNING_FROM}/pages/front/home.js")
+
+            # styles.css
+            FileSystem.createFile(f"{PROJECT_RUNNING_FROM}/CSS/styles.css")
+
+            Log.line()
+
+            ################################ Reading
+            ################ Loading Files
+            Log.center("Loading Files", '-')
+
+            ######### Internals
+            # config.yaml
+            FileSystem.loadDefaultConfigurations()
+
+            # Internal languageDictionary.json
+            FileSystem.loadInternalLanguageDictionary()
+
+            ######### Externals
+            # project.json
+            FileSystem.loadProjectConfigurations()
+
+            # CSS
+            FileSystem.loadExternalCSS()
+
+            # SVG
+            FileSystem.loadExternalSVG()
+
+            # External languageDictionary.json
+            FileSystem.loadExternalLanguageDictionary()
+
+            ######### Merge
+            # Merge Configurations
+            FileSystem.mergeConfigurations()
+
+            # Merge Configurations
+            FileSystem.mergeLanguageDictionaries()
+
+            Log.line()
+
+            ################################ Copying
+            Log.center("Copying Pages", '-')
+
+            # (Back-End)
+            FileSystem.copyPagesBackEnd()
+
+            # (Front-End)
+            FileSystem.copyPagesFrontEnd()
+
+            # Fonts
+            Log.center("Copying FONTS", '-')
+            FileSystem.copyFonts()
+
+            Log.line()
+
+        ####### Copy
+        # Copy Pages (Back-End)
+        @staticmethod
+        def copyPagesBackEnd():
+            if FileSystem.copyFiles(f"{PROJECT_RUNNING_FROM}/pages/back", f"{str(X_RUNNING_FROM)}/python/pages") is True:
+                Log.success("Pages Are Copied (Back-End)")
+
+            else:
+                Log.error("Could Not Copy The Pages (Back-End)")
+                sys.exit()
+
+        # Copy Pages (Back-End)
+        @staticmethod
+        def copyPagesFrontEnd():
+            if FileSystem.copyFiles(f"{PROJECT_RUNNING_FROM}/pages/front", f"{str(X_RUNNING_FROM)}/js/pages") is True:
+                Log.success("Pages Are Copied (Front-End)")
+
+            else:
+                Log.error("Could Not Copy The Pages (Front-End)")
+                sys.exit()
+
+        # Copy Fonts
+        @staticmethod
+        def copyFonts():
+            if FileSystem.copyFiles(f"{PROJECT_RUNNING_FROM}/fonts", f"{str(X_RUNNING_FROM)}/fonts") is True:
+                Log.success("Fonts Are Copied")
+
+            else: Log.error("Could Not Copy The Fonts")
+
+        ####### Load
+        #### Internals
+        # Configurations - config.yaml
+        @staticmethod
+        def loadDefaultConfigurations():
+            global CONF
+
+            try:
+                with open(f"{X_RUNNING_FROM}/yaml/config.yaml", 'r') as file:
+                    CONF = yaml.safe_load(file)
+
+                Log.success("Loaded: config.yaml")
+
+            except:
+                Log.error("Could Not Load The config.yaml")
+                sys.exit()
+
+        # Language Dictionary
+        @staticmethod
+        def loadInternalLanguageDictionary():
+            global LANG_DICT
+
+            try:
+                with open(f'{X_RUNNING_FROM}/json/languageDictionary.json', encoding="utf8") as file:
+                    LANG_DICT = json.load(file)
+
+                Log.success("Internal languageDictionary.json Is Loaded")
+
+            except:
+                Log.error("Could Not Read The Internal languageDictionary.json")
+
+        #### Externals
+        # Project Configurations - project.json
+        @staticmethod
+        def loadProjectConfigurations():
+            global PROJECT
+
+            try:
+                with open(f"{PROJECT_RUNNING_FROM}/project.json", 'r') as file:
+                    PROJECT = json.load(file)
+
+                Log.success("Loaded: project.json")
+
+            except:
+                Log.error("Could Not Load The project.json")
+                sys.exit()
+
+        # Load External CSS
+        @staticmethod
+        def loadExternalCSS():
+            global PROJECT_CSS
+
+            try:
+                with open(f'{PROJECT_RUNNING_FROM}/CSS/styles.css', "r") as css:
+                    PROJECT_CSS = css.read()
+
+                Log.success("External CSS Files Are Loaded")
+
+            except:
+                Log.warning("Could Not Read The External CSS")
+
+        # Load External SVG
+        @staticmethod
+        def loadExternalSVG():
+            global PROJECT_SVG
+
+            for file in os.listdir(f'{PROJECT_RUNNING_FROM}/SVG'):
+                # Check If File Is A SVG File
+                if not file.endswith(".svg"): continue
+
+                try:
+                    with open(f'{PROJECT_RUNNING_FROM}/SVG/{file}', "r") as svg:
+                        PROJECT_SVG[os.path.splitext(file)[0]] = svg.read()
+
+                    Log.success(f"SVG Loaded: {file}")
+
+                except:
+                    Log.warning(f"Could Not Load The SVG: {file}")
+
+        @staticmethod
+        def loadExternalLanguageDictionary():
+            global PROJECT_LANG_DICT
+
+            try:
+                with open(f"{PROJECT_RUNNING_FROM}/languageDictionary.json", 'r') as file:
+                    PROJECT_LANG_DICT = json.load(file)
+
+                Log.success("External languageDictionary.json Is Loaded")
+
+            except:
+                Log.error("Could Not Read The External languageDictionary.json")
+
+
+        #### Merge
+        # Merge config.yaml And project.json
+        # Merge Project Dependent Configurations To Default Configurations. Override Defaults
+        @staticmethod
+        def mergeConfigurations():
+            global CONF, PROJECT, PUBLIC_CONF
+
+            #### Merge
+            # Database
+            if "database" in PROJECT:
+                if "database" in CONF: CONF["database"].update(PROJECT["database"])
+                else: CONF["database"] = PROJECT["database"]
+
+            # eMail
+            if "eMail" in PROJECT:
+                CONF["eMail"].update(PROJECT["eMail"])
+
+            # Defaults
+            if "default" in PROJECT:
+                if "default" in CONF: CONF["default"].update(PROJECT["default"])
+                else: CONF["default"] = PROJECT["default"]
+
+            # Menu
+            if "menu" in PROJECT:
+                CONF["menu"] = PROJECT["menu"]
+
+            # Pages
+            if "pages" in PROJECT:
+                CONF["pages"] = PROJECT["pages"]
+
+            # OpenAI
+            if "OpenAI" in PROJECT:
+                CONF["OpenAI"] = PROJECT["OpenAI"]
+
+            #### Public Configurations
+            PUBLIC_CONF = {
+                "default": CONF["default"],
+                "menu": CONF["menu"],
+                "pages": CONF["pages"],
+                "username": CONF["username"],
+                "password": CONF["password"],
+                "phoneNumber": CONF["phoneNumber"],
+
+            }
+
+        def mergeLanguageDictionaries():
+            # Override The LANG_DICT w/ The PROJECT_LANG_DICT
+            LANG_DICT.update(PROJECT_LANG_DICT)
+
+
+        ############## User
         @staticmethod
         def initUserFolders(id = None):
             ID = None
@@ -85,7 +354,7 @@ if __name__ != "__main__":
                 if isinstance(id, int) and id > 0: ID = id
                 else:
                     Log.warning(f"Invalid Argument Passed To The Method @ FileSystem.initUserFolders(): {id}. Due To That Could Not Initiate User Folders")
-                    
+
                     return False
 
             elif "user" in session: ID = session["user"]["id"]
@@ -95,7 +364,7 @@ if __name__ != "__main__":
 
             # Must Match With The Path In .gitignore
             # source/users/id/...
-            path = f'{APP_RUNNING_FROM}/users/{ID}/'
+            path = f'{X_RUNNING_FROM}/users/{ID}/'
 
             # Try To Create User Folders
             try:
@@ -117,11 +386,10 @@ if __name__ != "__main__":
 
                 return False
 
-
         @staticmethod
         def deleteUserFiles(id):
             try:
-                shutil.rmtree(f'{APP_RUNNING_FROM}/users/{id}/')
+                shutil.rmtree(f'{X_RUNNING_FROM}/users/{id}/')
 
                 Log.success(f"User Files Deleted. User ID: {id}")
 
