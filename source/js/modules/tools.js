946688515286a3c882618c1eb5c4f8cd26e3f251
woXrooX <woXrooX@gmail.com>
2023-05-07 13:19:44 +0500

    tool: IntersectionObserver

--- source/js/modules/tools.js
+++ source/js/modules/tools.js
@@ -1,7 +1,28 @@
-// import {elementExists} from "../modules/tools.js"
-export function elementExists(selector){
-  const element = document.querySelector(selector);
+"use strict";
 
-  return !!element;
+// import {name} from "../modules/tools.js"
+
+export function observe(
+  selector,
+  onObservedAddClassName,
+  onOutOfObservingRemoveClass = false,
+  options={
+    threshhold: 0,
+    rootMargin: "0px"
+  }
+){
+
+  const observer = new IntersectionObserver(elements =>{
+    for(const element of elements)
+      if(element.isIntersecting) element.target.classList.add(onObservedAddClassName);
+      else if(onOutOfObservingRemoveClass === true) element.target.classList.remove(onObservedAddClassName);
+
+  },options);
+
+  const elements = document.querySelectorAll(selector);
+
+  if(!!elements === false) return;
+
+  for(const element of elements) observer.observe(element);
 
 }
