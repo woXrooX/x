af0c098c56619bab54912e9ccd106c8e51993a77
woXrooX <woXrooX@gmail.com>
2023-06-02 21:31:42 +0500

    App is down little change and fav icon on def

--- source/js/pages/appIsDown.js
+++ source/js/pages/appIsDown.js
@@ -7,7 +7,7 @@ export default function content(){
 <container class="p-5">
   <row>
     <column class="w-50 box-default p-5">
-      <h1>${window.CONF["default"]["appIsDown"]["reason"]}</h1>
+      <h1>${window.CONF["default"]["appIsDown"]["title"]}</h1>
       <h3 style="color: var(--color-text-secondary);">${window.CONF["default"]["appIsDown"]["explanation"]}</h3>
     </column>
   </row>
