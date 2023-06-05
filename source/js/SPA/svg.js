cf29c11e5a7ab3eb7b9af6f3a9221baeea849cf8
woXrooX <woXrooX@gmail.com>
2023-06-05 17:22:05 +0500

    change: Now SVG.all is public

--- source/js/SPA/svg.js
+++ source/js/SPA/svg.js
@@ -1,7 +1,7 @@
 "use strict";
 
 export default class SVG{
-  static #all = {
+  static all = {
     "home": `<path d="M0 0h24v24H0V0z" fill="none"/><path d="M10 19v-5h4v5c0 .55.45 1 1 1h3c.55 0 1-.45 1-1v-7h1.7c.46 0 .68-.57.33-.87L12.67 3.6c-.38-.34-.96-.34-1.34 0l-8.36 7.53c-.34.3-.13.87.33.87H5v7c0 .55.45 1 1 1h3c.55 0 1-.45 1-1z"/>`,
     "menu": `<path d="M0 0h24v24H0V0z" fill="none"/><path d="M4 18h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zm0-5h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zM3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1z"/>`,
     "arrow_bottom_small": `<path d="M24 24H0V0h24v24z" fill="none" opacity=".87"/><path d="M15.88 9.29L12 13.17 8.12 9.29c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41l4.59 4.59c.39.39 1.02.39 1.41 0l4.59-4.59c.39-.39.39-1.02 0-1.41-.39-.38-1.03-.39-1.42 0z"/>`,
@@ -66,13 +66,13 @@ export default class SVG{
     let body = "";
 
     // Invalid Name Value
-    if(!!name === false) body = SVG.#all["block_circle"];
+    if(!!name === false) body = SVG.all["block_circle"];
 
     // If Exists
-    if(name in SVG.#all) body = SVG.#all[name];
+    if(name in SVG.all) body = SVG.all[name];
 
     // Fallback SVG
-    else body = SVG.#all["block_circle"];
+    else body = SVG.all["block_circle"];
 
     // Build And Return
     return SVG.#open + body + SVG.#close;
@@ -82,7 +82,7 @@ export default class SVG{
   static set(SVGs){
     if(!!SVGs === false) return;
 
-    for(const svg in SVGs) SVG.#all[svg] = SVGs[svg];
+    for(const svg in SVGs) SVG.all[svg] = SVGs[svg];
 
   }
 
