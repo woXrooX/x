1f631fa55b048aa77172905523ff339f4804a62b
woXrooX <woXrooX@gmail.com>
2023-06-10 22:41:46 +0500

    + guards and info

--- source/python/modules/PDF.py
+++ source/python/modules/PDF.py
@@ -1,3 +1,21 @@
+# options = {
+#     'page-size': 'Letter',
+#     'margin-top': '0.75in',
+#     'margin-right': '0.75in',
+#     'margin-bottom': '0.75in',
+#     'margin-left': '0.75in',
+#     'encoding': "UTF-8",
+#     'custom-header': [
+#         ('Accept-Encoding', 'gzip')
+#     ],
+#     'cookie': [
+#         ('cookie-empty-value', '""')
+#         ('cookie-name1', 'cookie-value1'),
+#         ('cookie-name2', 'cookie-value2'),
+#     ],
+#     'no-outline': None
+# }
+
 # https://pypi.org/project/pdfkit/
 # pip install pdfkit
 # sudo apt-get install wkhtmltopdf
@@ -8,5 +26,10 @@ if __name__ != "__main__":
     class PDF:
         @staticmethod
         def generate(content, pathToSave, fileNameToSave, options = {}):
+            # Check For Invalid Arguments
+            if not content or not pathToSave or not fileNameToSave: return False
+
+            file_path_and_name = f"{pathToSave}/{fileNameToSave}.pdf"
+            
             # Returns True If Creation Successful Else False
-            return pdfkit.from_string(content, f"{pathToSave}/{fileNameToSave}.pdf", options=options)        
+            return pdfkit.from_string(content, file_path_and_name, options=options)        
