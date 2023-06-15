3c8a437272d33cd753a9fb577111ae8952d7c59d
woXrooX <woXrooX@gmail.com>
2023-06-15 16:51:47 +0500

    Logger 2.0

--- source/python/modules/Logger.py
+++ source/python/modules/Logger.py
@@ -3,62 +3,62 @@ if __name__ != "__main__":
     from datetime import datetime
     import random
 
+    from python.modules.Globals import Globals
+
     class Logger:
 
         #### Variables
         columns, lines = shutil.get_terminal_size()
         lineLength = columns - 1
 
+        colors = {
+            "success": "\033[92m", # bright_green
+            "info": "\033[96m", # bright_cyan
+            "warning": "\033[93m", # bright_yellow
+            "error": "\033[91m", # bright_red
+
+            "black": "\033[30m",
+            "red": "\033[31m",
+            "green": "\033[32m",
+            "yellow": "\033[33m",
+            "blue": "\033[34m",
+            "magenta": "\033[35m",
+            "cyan": "\033[36m",
+            "white": "\033[37m",
+            "bright_black": "\033[90m",
+            "bright_red": "\033[91m",
+            "bright_green": "\033[92m",
+            "bright_yellow": "\033[93m",
+            "bright_blue": "\033[94m",
+            "bright_magenta": "\033[95m",
+            "bright_cyan": "\033[96m",
+            "bright_white": "\033[97m",
+
+            "reset": "\033[0m"
+        }
+
+
+        ######### APIs / Log Types
         @staticmethod
-        def success(text):
-            print(Logger.coloredText("bright_green", f"[{Logger.timestamp()}] [SUCCESS] ") + text)
-
-        @staticmethod
-        def info(text):
-            print(Logger.coloredText("bright_cyan", f"[{Logger.timestamp()}] [INFO] ") + text)
-
-        @staticmethod
-        def warning(text):
-            print(Logger.coloredText("bright_yellow", f"[{Logger.timestamp()}] [WARNING] ") + text)
-
-        @staticmethod
-        def error(text):
-            print(Logger.coloredText("bright_red", f"[{Logger.timestamp()}] [ERROR] ") + text)
-
-
-        @staticmethod
-        def clear():
-            # import os
-            # os.system('cls' if os.name == 'nt' else 'clear')
-
-            print("\033[H\033[J")
-
-
-        @staticmethod
-        def line(char = "\u2588"):
-            print(Logger.coloredText("bright_cyan", char) * Logger.lineLength)
-
-
-        @staticmethod
-        def coloredText(color, text):
-            return f"{colors[color]}{text}{colors['reset']}"
-
+        def success(text, force = False): Logger.log(text, "success", force)
 
         @staticmethod
-        def centeredText(text, fillChar = ' '):
-            padding = (Logger.columns - 2 - len(text)) // 2
-            return fillChar * padding +' '+ text +' '+ fillChar * padding
-
+        def info(text, force = False): Logger.log(text, "info", force)
 
         @staticmethod
-        def center(text, fillChar = ' '):
-            print(Logger.centeredText(text, fillChar))
-
+        def warning(text, force = False): Logger.log(text, "warning", force)
 
         @staticmethod
-        def timestamp():
-            return datetime.now().strftime('%Y.%m.%d %H:%M:%S')
+        def error(text, force = False): Logger.log(text, "error", force)
 
+        @staticmethod
+        def raw(data): print(data)
+
+        @staticmethod
+        def center(text, fillChar = ' '): print(Logger.centeredText(text, fillChar))
+
+        @staticmethod
+        def line(char = "\u2588"): print(Logger.coloredText("bright_cyan", char) * Logger.lineLength)
 
         @staticmethod
         def brand():
@@ -70,28 +70,32 @@ if __name__ != "__main__":
             print(Logger.coloredText("bright_cyan", brand))
 
             Logger.line()
+        
+
+        ######### Helpers - Supposed To Be "Private"
+        @staticmethod
+        def log(text, type, force = False):
+            # Check If Debugging Mode Is Enabled
+            if(
+                force is True or
+                "default" not in Globals.CONF or
+                "default" in Globals.CONF and Globals.CONF["default"]["debug"] is not False
+            ): print(Logger.coloredText(type, f"[{Logger.timestamp()}] [{type.upper()}]"), text)
 
 
-    colors = {
-        "black": "\033[30m",
-        "red": "\033[31m",
-        "green": "\033[32m",
-        "yellow": "\033[33m",
-        "blue": "\033[34m",
-        "magenta": "\033[35m",
-        "cyan": "\033[36m",
-        "white": "\033[37m",
-        "bright_black": "\033[90m",
-        "bright_red": "\033[91m",
-        "bright_green": "\033[92m",
-        "bright_yellow": "\033[93m",
-        "bright_blue": "\033[94m",
-        "bright_magenta": "\033[95m",
-        "bright_cyan": "\033[96m",
-        "bright_white": "\033[97m",
+        @staticmethod
+        def clear(): print("\033[H\033[J")
 
-        "reset": "\033[0m"
-    }
+        @staticmethod
+        def coloredText(color, text): return f"{Logger.colors[color]}{text}{Logger.colors['reset']}"
+
+        @staticmethod
+        def centeredText(text, fillChar = ' '):
+            padding = (Logger.columns - 2 - len(text)) // 2
+            return fillChar * padding +' '+ text +' '+ fillChar * padding
+
+        @staticmethod
+        def timestamp(): return datetime.now().strftime('%Y.%m.%d %H:%M:%S')
 
 
     brands = [
@@ -102,9 +106,9 @@ if __name__ != "__main__":
    ██║   ██╔══██║██╔══╝       ██╔██╗
    ██║   ██║  ██║███████╗    ██╔╝ ██╗
    ╚═╝   ╚═╝  ╚═╝╚══════╝    ╚═╝  ╚═╝
-            """,
+        """,
 
-            """
+        """
 TTTTTTTTTTTTTTTTTTTTTTThhhhhhh                                      XXXXXXX       XXXXXXX
 T:::::::::::::::::::::Th:::::h                                      X:::::X       X:::::X
 T:::::::::::::::::::::Th:::::h                                      X:::::X       X:::::X
