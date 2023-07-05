38878ad78a183427748cbe7b770a3e69640aa608
woXrooX <woXrooX@gmail.com>
2023-07-05 19:07:20 +0500

    fix: missing icon on appIsDown,js

--- source/js/pages/appIsDown.js
+++ source/js/pages/appIsDown.js
@@ -4,14 +4,15 @@ export const TITLE = "App Is Down";
 export default function content(){
 
   return `
-<container class="p-5">
-  <row>
-    <column class="w-50 box-default p-5">
-      <h1>${window.CONF["default"]["appIsDown"]["title"]}</h1>
-      <h3 style="color: var(--color-text-secondary);">${window.CONF["default"]["appIsDown"]["explanation"]}</h3>
-    </column>
-  </row>
-</container>
+    <container class="p-5">
+      <row>
+        <column class="w-50 box-default p-5">
+          <x-icon name="${window.CONF["default"]["appIsDown"]["icon"]}"></x-icon>
+          <h1>${window.CONF["default"]["appIsDown"]["title"]}</h1>
+          <h3 class="color-text-secondary">${window.CONF["default"]["appIsDown"]["explanation"]}</h3>
+        </column>
+      </row>
+    </container>
   `;
 
 }
