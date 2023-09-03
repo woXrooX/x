53dfb4cbfe772272dac7f580936b14d684c4ab9c
woXrooX <woXrooX@gmail.com>
2023-09-03 22:44:21 +0500

    Added sub nav support for x-nav

--- source/js/SPA/elements/nav.js
+++ source/js/SPA/elements/nav.js
@@ -1,12 +1,6 @@
 "use strict";
 
 export default class Nav extends HTMLElement{
-    static #template = document.createElement("template");
-
-    static {
-        Nav.#template.innerHTML = `<nav></nav>`;
-    }
-
     constructor(){
         super();
 
@@ -14,41 +8,61 @@ export default class Nav extends HTMLElement{
         this.JSON = this.innerHTML;
 
         // Clean the inner data
-        this.innerHTML = "";
+        this.innerHTML = `<nav class="w-100 d-flex flex-column text-left gap-0-2 p-1"></nav>`;
 
-        // Selector
+        // Check if this has "selector" attribute
+        if(this.hasAttribute("selector") === false) return;
         this.selector = this.getAttribute("selector");
 
-        // Clone And Append Template
-        this.appendChild(Nav.#template.content.cloneNode(true));
+        // Set nav class
+        this.querySelector("nav").classList.add(this.getAttribute("class") || "surface-clean");
 
-        // Set Nav Class
-        this.querySelector("nav").setAttribute("class", this.getAttribute("class") || "surface-clean");
+        BuildHTML: {
+          this.HTML = "";
+          this.JSON = JSON.parse(this.JSON).constructor === Array ? JSON.parse(this.JSON) : [];
 
-        // Make scrollable on x axis when the media condition matches
-        this.#ifMobileMakeScrollable();
-        window.matchMedia(`(max-width: ${window.CSS.getValue("--screen-size-phone")})`).onchange = this.#ifMobileMakeScrollable;
+          this.#buildNavs(this.JSON);
 
-        BuildButtons: {
-            this.buttonsHTML = "";
-            this.buttons = JSON.parse(this.JSON).constructor === Array ? JSON.parse(this.JSON) : [];
-
-            for(const button of this.buttons)
-                this.buttonsHTML += `<a href="#${button.link}">${window.Lang.use("name" in button ? button.name : button.link)}</a>`;
-
-            // Placing the buttons/links
-            this.querySelector("nav").innerHTML = this.buttonsHTML;
+          // Placing the buttons/links
+          this.querySelector("nav").innerHTML = this.HTML;
         }
 
 
         Listeners: {
-            // Init Active Nav Button
-            this.#setActive();
+          // Make scrollable on x axis when the media condition matches
+          this.#ifMobileMakeScrollable();
+          window.matchMedia(`(max-width: ${window.CSS.getValue("--screen-size-phone")})`).onchange = this.#ifMobileMakeScrollable;
 
-            // On Hash Change Trigger
-            window.onhashchange = this.#setActive;
+          // Init Active Nav Button
+          this.#setActive();
+
+          // On Hash Change Trigger
+          window.onhashchange = this.#setActive;
+
+          this.#toggleSubNav();
         }
+    }
 
+    #buildNavs = (data)=>{
+      for(const obj of data){
+        if("link" in obj){
+          this.HTML += `
+            <div class="w-100 d-flex flex-column gap-0-2 pl-3 p-1 radius-default">
+              <section for="link">
+                ${"subNav" in obj ? '<x-icon name="arrow_bottom_small" for="toggleSubNav"></x-icon>' : "<span></span>"}
+                <a href="#${obj.link}">${window.Lang.use("name" in obj ? obj.name : obj.link)}</a>
+              </section>
+          `;
+
+          if("subNav" in obj){
+            this.HTML += `<section for="subNav" class="d-flex flex-column gap-0-2 radius-default">`;
+            this.#buildNavs(obj.subNav);
+            this.HTML += `</section>`;
+          }
+
+          this.HTML += `</div>`;
+        }
+      }
     }
 
     #ifMobileMakeScrollable = ()=>{
@@ -58,29 +72,32 @@ export default class Nav extends HTMLElement{
       else this.querySelector("nav").classList.remove("scrollbar-x");
     }
 
+    #toggleSubNav = ()=>{
+      const subNavTogglers = this.querySelectorAll("x-icon[for=toggleSubNav]");
+
+      for(const toggler of subNavTogglers)
+        toggler.onclick = ()=> {
+          toggler.classList.toggle("open");
+          toggler.parentElement.parentElement.querySelector("section[for=subNav]").classList.toggle("show");
+        }
+    }
+
     #setActive = ()=>{
         let isHashValid = false;
         const hyperlinks = this.querySelector("nav").getElementsByTagName("a");
 
         for(const a of hyperlinks)
-            if(a.getAttribute("href") === window.location.hash){
-                a.classList.add("active");
-
-                isHashValid = true;
-
-            }else a.classList.remove("active");
+          if(a.getAttribute("href") === window.location.hash){
+            a.classList.add("active");
+            isHashValid = true;
+          }else a.classList.remove("active");
 
         // If Still No Valid Hash Found
         if(isHashValid === false){
-            hyperlinks[0].classList.add("active");
-
-            window.location.hash = hyperlinks[0].getAttribute("href");
-
+          hyperlinks[0].classList.add("active");
+          window.location.hash = hyperlinks[0].getAttribute("href");
         }
-
-
-
-    };
+    }
 
     connectedCallback(){
         // CSS Rules For Child Elements Animation
@@ -113,7 +130,6 @@ export default class Nav extends HTMLElement{
     disconnectedCallback(){
         document.head.removeChild(document.querySelector("style[for=x-nav]"));
     }
-
 };
 
 window.customElements.define('x-nav', Nav);
