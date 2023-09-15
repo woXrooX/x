6590f347cafdb90930e281807f0b68bd19e7bb01
woXrooX <woXrooX@gmail.com>
2023-09-15 15:19:56 +0500

    New module for working with randomizing content.

---
+++ source/python/modules/Random.py
@@ -0,0 +1,10 @@
+if __name__ != "__main__":
+    import random
+    import string
+
+    class Random():
+        password_allowed_characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_-0123456789!@#$%^&"
+
+        @staticmethod
+        def generatePassword(length=12):
+            return ''.join(random.choice(Random.password_allowed_characters) for _ in range(length))
