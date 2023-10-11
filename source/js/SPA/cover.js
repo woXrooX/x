fb3d85c2ba7477d05ec22f7bf98997df371e0ef4
woXrooX <woXrooX@gmail.com>
2023-10-11 18:07:50 +0500

    white spaces to tabs

--- source/js/SPA/cover.js
+++ source/js/SPA/cover.js
@@ -1,48 +1,46 @@
 "use strict";
 
 export default class Cover{
-  static selector = "body > cover";
-  static #elementCover = null;
+	static selector = "body > cover";
+	static #elementCover = null;
 
-  static{
-    Cover.#elementCover = document.querySelector(Cover.selector);
-  }
+	static{
+		Cover.#elementCover = document.querySelector(Cover.selector);
+	}
 
-  // Can be set the cover z-index if needed
-  static show(zIndex = window.CSS.getValue("--z-cover")){
-    // Check if body > cover exists
-    if(!!Cover.#elementCover === false) return;
+	// Can be set the cover z-index if needed
+	static show(zIndex = window.CSS.getValue("--z-cover")){
+		// Check if body > cover exists
+		if(!!Cover.#elementCover === false) return;
 
-    // If "auto" was passed set to default zIndex
-    if(zIndex === "auto") zIndex = window.CSS.getValue("--z-cover");
+		// If "auto" was passed set to default zIndex
+		if(zIndex === "auto") zIndex = window.CSS.getValue("--z-cover");
 
-    Cover.#elementCover.style.opacity = 1;
-    Cover.#elementCover.style.zIndex = zIndex;
+		Cover.#elementCover.style.opacity = 1;
+		Cover.#elementCover.style.zIndex = zIndex;
 
-    // disable scrolling
-    document.body.style = "overflow: hidden";
-  }
+		// disable scrolling
+		document.body.style = "overflow: hidden";
+	}
 
-  static hide(){
-    // Check if body > cover exists
-    if(!!Cover.#elementCover === false) return;
+	static hide(){
+		// Check if body > cover exists
+		if(!!Cover.#elementCover === false) return;
 
-    Cover.#elementCover.style.opacity = 0;
+		Cover.#elementCover.style.opacity = 0;
 
-    setTimeout(()=>{
-      Cover.#elementCover.style.zIndex = window.CSS.getValue("--z-minus");
-    }, parseInt(window.CSS.getValue("--transition-velocity")));
+		setTimeout(()=>{
+			Cover.#elementCover.style.zIndex = window.CSS.getValue("--z-minus");
+		}, parseInt(window.CSS.getValue("--transition-velocity")));
 
-    // enable scrolling
-    document.body.removeAttribute("style");
+		// enable scrolling
+		document.body.removeAttribute("style");
+	}
 
-  }
-
-  static onClickExecute(func){
-    // Execute External Function
-    document.querySelector(Cover.selector).addEventListener("click", func);
-  }
-
+	static onClickExecute(func){
+		// Execute External Function
+		document.querySelector(Cover.selector).addEventListener("click", func);
+	}
 }
 
 // Make Cover Usable W/O Importing It
