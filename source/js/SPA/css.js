79aa8fde5512005407487bb3cb99c602aac9b30e
woXrooX <woXrooX@gmail.com>
2023-10-22 21:19:37 +0500

    Clean up

--- source/js/SPA/css.js
+++ source/js/SPA/css.js
@@ -8,6 +8,31 @@
 // // Get computed value
 // console.log(computedRootStyles.getPropertyValue("--color-success"));
 
+
+// let root = document.querySelector(':root');
+// let currentColorScheme = root.getAttribute('color-scheme');
+// console.log(root);
+// console.log(currentColorScheme);
+// console.log(getComputedStyle(document.body).getPropertyValue('--z-minus'));
+
+
+// prefers-color-scheme TMP
+// console.log(window.matchMedia("(prefers-color-scheme: dark)"));
+// console.log(window.matchMedia("(prefers-color-scheme: dark)").matches);
+// const mq = window.matchMedia("(prefers-color-scheme: dark)");
+// mq.addEventListener('change', ({matches: isDark})=>{
+//   console.log(matches);
+//   console.log("Changed");
+// });
+// mq.dispatchEvent(new Event('change'));
+
+
+///////////////////////////// CSS
+// import styles from '../css/styles.css' assert {type: 'css'};
+// console.log(styles.rules);
+// styles.rules[0].style.color = "blue";
+// console.log(styles.rules[0].style.color);
+
 // background -> middleground -> foreground
 
 "use strict";
--- source/js/main.js
+++ source/js/main.js
@@ -2,31 +2,6 @@
 ///////////////////////////// SPA / Core
 import Core from "./SPA/core.js";
 
-// let root = document.querySelector(':root');
-// let currentColorScheme = root.getAttribute('color-scheme');
-// console.log(root);
-// console.log(currentColorScheme);
-// console.log(getComputedStyle(document.body).getPropertyValue('--z-minus'));
-
-
-// prefers-color-scheme TMP
-// console.log(window.matchMedia("(prefers-color-scheme: dark)"));
-// console.log(window.matchMedia("(prefers-color-scheme: dark)").matches);
-// const mq = window.matchMedia("(prefers-color-scheme: dark)");
-// mq.addEventListener('change', ({matches: isDark})=>{
-//   console.log(matches);
-//   console.log("Changed");
-// });
-// mq.dispatchEvent(new Event('change'));
-
-
-///////////////////////////// CSS
-// import styles from '../css/styles.css' assert {type: 'css'};
-// console.log(styles.rules);
-// styles.rules[0].style.color = "blue";
-// console.log(styles.rules[0].style.color);
-
-
 ///////////////////////////// PWA serviceWorker
 // if(document.readyState === "complete"){
 //   if('serviceWorker' in navigator){
