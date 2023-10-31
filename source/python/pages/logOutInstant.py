a736557068b92ee793076ac0273dbc51bd96058a
woXrooX <woXrooX@gmail.com>
2023-10-31 13:43:13 +0100

    New proto for instant log out function

---
+++ source/python/pages/logOutInstant.py
@@ -0,0 +1,29 @@
+from main import app, request, render_template, session
+from python.modules.pageGuard import pageGuard
+from python.modules.response import response
+from python.modules.Globals import Globals
+
+
+#################################################### Log Out
+@app.route("/logOut", methods=["GET", "POST"])
+@pageGuard("logOut")
+def logOutInstant():
+    if request.method == "GET": return render_template("index.html", **globals())
+
+
+    elif request.method == "POST":
+        # unknownError
+        if request.form["for"] != "logOut": return response(type="warning", message="unknownError")
+
+        # Remove User From Session
+        session.pop('user')
+
+
+        # Redirect To Home
+        return response(
+            type="success",
+            message="success",
+            deleteSessionUser=True,
+            redirect="home",
+            domChange=["menu"]
+        )
