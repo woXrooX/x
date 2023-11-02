6a67c82ff50631a405e5507a5bf196c197008154
woXrooX <woXrooX@gmail.com>
2023-11-02 13:47:54 +0100

    languagesLibrary is obsolete

--- source/python/pages/languagesLibrary.py
+++
@@ -1,9 +0,0 @@
-from main import app, request, render_template
-from python.modules.pageGuard import pageGuard
-from python.modules.Globals import Globals
-
-#################################################### Demo
-@app.route("/languagesLibrary", methods=["GET"])
-@pageGuard("languagesLibrary")
-def languagesLibrary():
-    if request.method == "GET": return render_template("index.html", **globals())
