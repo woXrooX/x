966f0ff54324cc74edcd5621c1d41f5851fdc944
woXrooX <woXrooX@gmail.com>
2023-11-05 19:08:31 +0100

    Modal update fixes

--- source/js/SPA/elements/share.js
+++ source/js/SPA/elements/share.js
@@ -1,87 +1,70 @@
 "use strict";
 
 export default class Share extends HTMLElement{
-  static #template = document.createElement("template");
+	constructor(){
+		super();
 
-  static {
-    Share.#template.innerHTML = `
-      <share>
-        <x-icon name="share"></x-icon>
-        <section for="modal"></section>
-      </share>
-    `;
-  }
+		this.textToShare = null;
+		if(!!this.getAttribute("selector") === true) this.textToShare = this.getAttribute("selector");
+		else if(!!this.getAttribute("text") === true) this.textToShare = this.getAttribute("text");
 
-  constructor(){
-    super();
+		const shareData = {
+			title: encodeURIComponent(this.getAttribute("title") || document.title),
+			text: encodeURIComponent(this.textToShare || window.Lang.use("shareDefaultText")),
 
-    Text: {
-      this.textToShare = null;
-      if(!!this.getAttribute("selector") === true) this.textToShare = this.getAttribute("selector");
-      else if(!!this.getAttribute("text") === true) this.textToShare = this.getAttribute("text");
-    }
+			// If Share URL Is Not HTTPS It Won't Work
+			// On Facebook
+			// On Linkedin
+			url: encodeURIComponent(this.getAttribute("url") || window.location.href)
+		};
 
-    // Clone And Append Template
-    this.appendChild(Share.#template.content.cloneNode(true));
 
-    this.querySelector("x-icon").onclick = async ()=>{
+		this.innerHTML = `
+			<x-modal type="icon" value="share">
+				<column class="pt-2 pb-0-5 gap-0-5">
+					<a href="https://twitter.com/intent/tweet?url=${shareData.url}&text=${shareData.text}" target="_blank">
+						<x-icon name="twitter_bird_original"></x-icon>
+					</a>
 
-      const shareData = {
-        title: encodeURIComponent(this.getAttribute("title") || document.title),
-        text: encodeURIComponent(this.textToShare || window.Lang.use("shareDefaultText")),
+					<a href="https://www.linkedin.com/sharing/share-offsite/?url=${shareData.url}&title=${shareData.title}&summary=${shareData.text}&source=${shareData.url}" target="_blank">
+						<x-icon name="linkedin_original"></x-icon>
+					</a>
 
-        // If Share URL Is Not HTTPS It Won't Work
-        // On Facebook
-        // On Linkedin
-        url: encodeURIComponent(this.getAttribute("url") || window.location.href)
+					<a href="https://www.facebook.com/sharer.php?u=${shareData.url}&quote=${shareData.text}" target="_blank">
+						<x-icon name="facebook_original"></x-icon>
+					</a>
 
-      };
+					<a href="https://www.instagram.com/share?url=${shareData.url}&caption=${shareData.text}" target="_blank">
+						<x-icon name="instagram_original"></x-icon>
+					</a>
 
-      // Check If navigator.share
-      if(!!navigator.share === true)
-        try{
-          await navigator.share(shareData);
-          window.Log.success("MDN shared successfully");
-        }catch(err){
-          window.Log.error(`Error: ${err}`);
-        }
+					<a href="https://t.me/share/url?url=${shareData.url}&text=${shareData.text}" target="_blank">
+						<x-icon name="telegram_original"></x-icon>
+					</a>
 
-      else{
-        window.Log.warning("No Native Support For 'navigator.share' On Your Device!");
+					<a href="https://www.reddit.com/submit?selftext=${shareData.text}&title=${shareData.title}&url=${shareData.url}" target="_blank">
+						<x-icon name="reddit_original"></x-icon>
+					</a>
+				</column>
+			</x-modal>
+		`;
 
-        this.querySelector("section").innerHTML = `
-          <x-modal trigger="auto">
+		this.querySelector("x-modal").onclick = async ()=>{
 
-            <a href="https://twitter.com/intent/tweet?url=${shareData.url}&text=${shareData.text}" target="_blank">
-              <x-icon name="twitter_bird_original"></x-icon>
-            </a>
+			// Check If navigator.share
+			if(!!navigator.share === true)
+				try{
+					await navigator.share(shareData);
+					window.Log.success("MDN shared successfully");
+				}catch(err){
+					window.Log.error(`Error: ${err}`);
+				}
 
-            <a href="https://www.linkedin.com/sharing/share-offsite/?url=${shareData.url}&title=${shareData.title}&summary=${shareData.text}&source=${shareData.url}" target="_blank">
-              <x-icon name="linkedin_original"></x-icon>
-            </a>
-
-            <a href="https://www.facebook.com/sharer.php?u=${shareData.url}&quote=${shareData.text}" target="_blank">
-              <x-icon name="facebook_original"></x-icon>
-            </a>
-
-            <a href="https://www.instagram.com/share?url=${shareData.url}&caption=${shareData.text}" target="_blank">
-              <x-icon name="instagram_original"></x-icon>
-            </a>
-
-            <a href="https://t.me/share/url?url=${shareData.url}&text=${shareData.text}" target="_blank">
-              <x-icon name="telegram_original"></x-icon>
-            </a>
-
-            <a href="https://www.reddit.com/submit?selftext=${shareData.text}&title=${shareData.title}&url=${shareData.url}" target="_blank">
-              <x-icon name="reddit_original"></x-icon>
-            </a>
-
-          </x-modal>
-        `;
-
-      }
-    };
-  }
+			else{
+				window.Log.warning("No Native Support For 'navigator.share' On Your Device!");
+			}
+		};
+	}
 };
 
 window.customElements.define('x-share', Share);
