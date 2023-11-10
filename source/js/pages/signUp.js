535937ab3e9678a217deb0c0ab839add27044cd3
woXrooX <woXrooX@gmail.com>
2023-11-10 11:24:50 +0100

    UI fix

--- source/js/pages/signUp.js
+++ source/js/pages/signUp.js
@@ -1,47 +1,37 @@
 "use strict";
 
-export const TITLE = "Sign Up";
+export const TITLE = window.Lang.use("signUp");
+
 export default function content() {
-  let dom = `
-<container class="p-5">
-  <row>
-    <column class="w-50 surface-clean p-5">
+	return `
+		<container class="p-5">
+			<column class="w-50 surface-clean p-5 gap-1">
 
-      <row>
-        <h1>${window.Lang.use("signUp")}</h1>
-      </row>
+				<h2>${window.Lang.use("signUp")}</h2>
 
-      <row>
-        <form action="signUp" for='signUp'>
+				<form action="signUp" for='signUp'>
 
-          <label>
-            <p for='eMail'>${window.Lang.use("eMail")}</p>
-            <input type='eMail' name='eMail'>
-          </label>
+					<label>
+						<p for='eMail'>${window.Lang.use("eMail")}</p>
+						<input type='eMail' name='eMail'>
+					</label>
 
-          <label>
-            <p for='password'>${window.Lang.use("password")}</p>
-            <input type='password' name='password' minlength='${window.CONF["password"]["min_length"]}' maxlength='${window.CONF["password"]["max_length"]}'><br>
-          </label>
+					<label>
+						<p for='password'>${window.Lang.use("password")}</p>
+						<input type='password' name='password' minlength='${window.CONF["password"]["min_length"]}' maxlength='${window.CONF["password"]["max_length"]}'><br>
+					</label>
 
-          <label>
-            <input type='submit' name='signUp' value='${window.Lang.use("signUp")}'>
-            <p for='signUp'></p>
-          </label>
+					<label>
+						<input type='submit' name='signUp' value='${window.Lang.use("signUp")}'>
+						<p for='signUp'></p>
+					</label>
 
-        </form>
-      </row>
+				</form>
 
-      <row>
-        <a href="/logIn">${window.Lang.use("haveAccountGoToLogIn")}</a>
-      </row>
+				<a href="/logIn" class="text-size-0-7">${window.Lang.use("haveAccountGoToLogIn")}</a>
 
-    </column>
+			</column>
 
-  </row>
-</container>
+		</container>
   `;
-
-  return dom;
-
 }
