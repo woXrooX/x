ba387d211aabfc2ce430aae80804476cbca184eb
woXrooX <woXrooX@gmail.com>
2023-11-14 19:43:52 +0100

    logOut UI fix

--- source/js/pages/logOut.js
+++ source/js/pages/logOut.js
@@ -1,26 +1,19 @@
 "use strict";
 
 export const TITLE = window.Lang.use("logOut");
+
 export default function content(){
 
-  const dom = `
-<container class="p-5">
-  <row>
-    <p>${window.Lang.use("ifLogOutWontBeAbleTo")}</p>
-  </row>
+	return `
+		<container class="p-5 gap-1">
+			<p class="text-size-0-8 text-center">${window.Lang.use("ifLogOutWontBeAbleTo")}</p>
 
-  <row>
-    <column class="w-50 surface-clean p-5">
-      <form action="logOut" for="logOut">
-        <label>
-          <input type='submit' name='logOut' value='${window.Lang.use("logOut")}'>
-          <p for='logOut'>${window.Lang.use("areYouSure")}</p>
-        </label>
-      </form>
-    </column>
-  </row>
-</container>
-  `;
-
-  return dom;
+			<form action="logOut" for="logOut" class="w-50 surface-clean p-5">
+				<label>
+				<input type='submit' name='logOut' value='${window.Lang.use("logOut")}'>
+				<p for='logOut'>${window.Lang.use("areYouSure")}</p>
+				</label>
+			</form>
+		</container>
+	`;
 }
