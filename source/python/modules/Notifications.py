f5ac4d3bc65537e12984523a0cee134da63ce5e5
woXrooX <woXrooX@gmail.com>
2023-11-28 20:41:06 +0100

    Notifications basics

---
+++ source/python/modules/Notifications.py
@@ -0,0 +1,17 @@
+if __name__ != "__main__":
+	from main import session
+	from python.modules.Globals import Globals
+
+	class Notifications():
+		@staticmethod
+		def new(
+			user = None,
+			content,
+			type = None
+		):
+			pass
+
+		@staticmethod
+		def getAll(user = None):
+			pass
+
