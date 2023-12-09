2f7bdb2cfea0a746ea53950d421ddefef48bc860
woXrooX <woXrooX@gmail.com>
2023-12-09 10:40:54 +0100

    Basics of backup class and files set up

---
+++ source/python/modules/Backup.py
@@ -0,0 +1,19 @@
+if __name__ != "__main__":
+	class Backup():
+		@staticmethod
+		def all():
+			pass
+
+		@staticmethod
+		def database():
+			pass
+
+		# Assets folder
+		@staticmethod
+		def assets():
+			pass
+
+		# Users folder
+		@staticmethod
+		def users():
+			pass
