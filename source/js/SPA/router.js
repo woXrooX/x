3bdb4a8083a635d7d185eb73d3eb12f36b64f2fa
woXrooX <woXrooX@gmail.com>
2023-12-10 11:26:20 +0100

    Xlean up + new error UI

--- source/js/SPA/router.js
+++ source/js/SPA/router.js
@@ -1,218 +1,200 @@
 "use strict";
 
 export default class Router{
-  static currentPage = {
-    "name": null,
-    "endpoint": null,
-    "urlArgs": null
-  }
+	static currentPage = {
+		"name": null,
+		"endpoint": null,
+		"urlArgs": null
+	}
 
-  static async handle(){
-    // Check If App Is Down If So Stop Handling Set appIsDown As Current Page
-    if("appIsDown" in window.CONF["default"]){
-      window.DOM.setPage(await import(`../pages/appIsDown.js`));
+	static async handle(){
+		// Check If App Is Down If So Stop Handling Set appIsDown As Current Page
+		if("appIsDown" in window.CONF["default"]){
+			window.DOM.setPage(await import(`../pages/appIsDown.js`));
 
-      // Force-End Loading Effect
-      window.Loading.end();
+			// Force-End Loading Effect
+			window.Loading.end();
 
-      return;
-    }
+			return;
+		}
 
-    // We have much efficient way of detecting if page exists if we give up on endpoints system
-    // Loop Through Pages
-    loopPages:
-    for(const page in window.CONF["pages"]){
-      // console.log(page);
+		// NOTE: We have much efficient way of detecting "if page exists" if we give up on endpoints system
+		// Loop Through Pages
+		loopPages:
+		for(const page in window.CONF["pages"]){
+			// Pass The Page To routeGuard Tests
+			if(Router.routeGuard(page) === false) continue;
 
-      // Pass The Page To routeGuard Tests
-      if(Router.routeGuard(page) === false) continue;
+			// Window Path Name
+			let pathname = window.location.pathname;
 
-      // Window Path Name
-      let pathname = window.location.pathname;
+			// If Url Args Exists In This Page Drop Url Arguments From The Path Name
+			// If Url Args Exists In This Page Extract Url Arguments From The Path Name
+			if(window.CONF.pages[page]?.urlArgs){
+				//// NOTE: Order Matters In This Scope
 
-      // If Url Args Exists In This Page Drop Url Arguments From The Path Name
-      // If Url Args Exists In This Page Extract Url Arguments From The Path Name
-      if(window.CONF.pages[page]?.urlArgs){
-        //// NOTE: Order Matters In This Scope
+				// Current Page Arguments
+				Router.currentPage.urlArgs = pathname.split('/').splice(-window.CONF.pages[page].urlArgs.length);
 
-        // Current Page Arguments
-        Router.currentPage.urlArgs = pathname.split('/').splice(-window.CONF.pages[page].urlArgs.length);
+				// Modified Path Name
+				pathname = pathname.split('/').slice(0, -window.CONF.pages[page].urlArgs.length).join('/');
+			}
 
-        // Modified Path Name
-        pathname = pathname.split('/').slice(0, -window.CONF.pages[page].urlArgs.length).join('/');
+			// Endpoints
+			loopEndpoints:
+			for(const endpoint of window.CONF["pages"][page]["endpoints"])
+				// Check If Page Endpoint Equals To Currnt Endpoint
+				if(endpoint == pathname){
+					//// Check If Page Is Not Current Loaded Page
+					// If yes then exit this method
+					// NOTE: endpoint should be unique.
+					// Page a and page b can not have the same endpoint.
+					// It may seem working but there will be bugs.
+					// It will make return false the expression below in rare cases.
+					if(
+						Router.currentPage.endpoint == pathname ||
+						Router.currentPage.endpoint === "home" && pathname === '/'
+					) return;
 
-      }
+					Router.currentPage.name = page;
+					Router.currentPage.endpoint = endpoint;
 
-      // Endpoints
-      loopEndpoints:
-      for(const endpoint of window.CONF["pages"][page]["endpoints"])
-        // Check If Page Endpoint Equals To Currnt Endpoint
-        if(endpoint == pathname){
+					// Break Out The Loops
+					break loopPages;
+				}
+		}
 
-          //// Check If Page Is Not Current Loaded Page
-          // If yes then exit this method
-          // NOTE: endpoint should be unique.
-          // Page a and page b can not have same endpoint.
-          // It may seem working but there will be bugs.
-          // It will make return false the expression below in rare cases.
-          if(
-            Router.currentPage.endpoint == pathname ||
-            Router.currentPage.endpoint === "home" && pathname === '/'
-          ) return;
+		// If Still No Endpoint Matched Then Set It To "pageNotFound" AKA 404
+		if(Router.currentPage.name === null) Router.currentPage.name = "pageNotFound";
 
-          Router.currentPage.name = page;
-          Router.currentPage.endpoint = endpoint;
+		// Load Page File
+		Router.#loadPageFile();
 
-          // Break Out The Loops
-          break loopPages;
+	}
 
-        }
+	static async #loadPageFile(){
+		window.Log.info(`Page file is loading: ${Router.currentPage.name}.js`)
 
-    }
+		try{
+			// Start Loading Effect
+			window.Loading.start();
 
-    // If Still No Endpoint Matched Then Set It To "pageNotFound" AKA 404
-    if(Router.currentPage.name === null) Router.currentPage.name = "pageNotFound";
+			// Change URL To /404 In Case endpoint Is /404
+			// Currntly causing infintive back and forth page looping
+			// if(endpoint === "/404") window.history.pushState("", "", URL+"404");
 
-    // Load Page File
-    Router.#loadPageFile();
+			// Load The Page
+			window.DOM.setPage(await import(`../pages/${Router.currentPage.name}.js`));
+		}catch(error){
+			// console.log(error);
+			// console.log(error.name);
+			// console.log(error.stack);
 
-  }
+			// Set Title To Error
+			window.Title.set("error");
 
-  static async #loadPageFile(){
-    window.Log.info(`Page file is loading: ${Router.currentPage.name}.js`)
+			// Render The Error
+			window.DOM.render(`
+				<container class="p-5">
+					<column class="flex-y-start surface-error p-3 gap-0-5">
+						<error>${error.name}</error>
+						<info class="text-size-0-8">${error.stack}</info>
+					</column>
+				</container>
+			`);
+		}finally{
+			// End Loading Effect
+			window.Loading.end();
+		}
+	}
 
-    try{
-      // Start Loading Effect
-      window.Loading.start();
-
-      // Change URL To /404 In Case endpoint Is /404
-      // Currntly causing infintive back and forth page looping
-      // if(endpoint === "/404") window.history.pushState("", "", URL+"404");
-
-      // Load The Page
-      window.DOM.setPage(await import(`../pages/${Router.currentPage.name}.js`));
-
-    }catch(error){
-      // console.log(error);
-      // console.log(error.name);
-      // console.log(error.stack);
-
-      // Set Title To Error
-      window.Title.set("error");
-
-      // Render The Error
-      window.DOM.render(`
-        <container class="p-5">
-          <row class="m-t-5 surface-clean p-5 w-50">
-            <column>
-              <error>${error.name}</error>
-              <info class="text-size-0-7">${error.stack}</info>
-            </column>
-          </row>
-        </container>
-      `);
-
-    }finally{
-      // End Loading Effect
-      window.Loading.end();
-
-    }
-
-  }
-
-  static routeGuard(page){
-    // Check If Page Exists
-    // Already Looping Through Existent Pages
+	static routeGuard(page){
+		// Check If Page Exists
+		// Already Looping Through Existent Pages
 
 
-    // Check If Page Is Enabled
-    if(window.CONF["pages"][page]["enabled"] === false) return false;
+		// Check If Page Is Enabled
+		if(window.CONF["pages"][page]["enabled"] === false) return false;
 
 
-    // Everyone
-    if(
-      !("authenticity_statuses" in window.CONF["pages"][page]) &&
-      !("roles" in window.CONF["pages"][page]) &&
-      !("plans" in window.CONF["pages"][page])
-    ) return true;
+		// Everyone
+		if(
+			!("authenticity_statuses" in window.CONF["pages"][page]) &&
+			!("roles" in window.CONF["pages"][page]) &&
+			!("plans" in window.CONF["pages"][page])
+		) return true;
 
 
-    // Session Dependent Checks
-    if("user" in window.session){
+		// Session Dependent Checks
+		if("user" in window.session){
+			// Root
+			if(window.session["user"]["roles"].includes("root")) return true;
 
-      // Root
-      if(window.session["user"]["roles"].includes("root")) return true;
+			///// Authenticity Statuses
+			let authenticity_check = false;
+			if("authenticity_statuses" in window.CONF["pages"][page]){
+				for(const authenticity_status in window.USER_AUTHENTICITY_STATUSES)
+					if(
+						window.session["user"]["authenticity_status"] == window.USER_AUTHENTICITY_STATUSES[authenticity_status]["id"] &&
+						window.CONF["pages"][page]["authenticity_statuses"].includes(authenticity_status)
+					) authenticity_check = true;
+			}else authenticity_check = true;
 
-      ///// Authenticity Statuses
-      let authenticity_check = false;
-      if("authenticity_statuses" in window.CONF["pages"][page]){
-        for(const authenticity_status in window.USER_AUTHENTICITY_STATUSES)
-          if(
-            window.session["user"]["authenticity_status"] == window.USER_AUTHENTICITY_STATUSES[authenticity_status]["id"] &&
-            window.CONF["pages"][page]["authenticity_statuses"].includes(authenticity_status)
-          ) authenticity_check = true;
+			///// Roles
+			let role_check = false;
+			if("roles" in window.CONF["pages"][page]){
+				// Check If One Of The User Assigned Roles Match With The CONF[page]["roles"]
+				for(let i = 0; i < window.session["user"]["roles"].length; i++)
+					if(window.CONF["pages"][page]["roles"].includes(window.session["user"]["roles"][i])){
+						role_check = true;
+						break;
+					}
+			}else role_check = true;
 
-      }else authenticity_check = true;
+			///// Plans - similar to role check
+			let plan_check = true; // Should be false in actual implementation
 
-      ///// Roles
-      let role_check = false;
-      if("roles" in window.CONF["pages"][page]){
-        // Check If One Of The User Assigned Roles Match With The CONF[page]["roles"]
-        for(let i = 0; i < window.session["user"]["roles"].length; i++)
-          if(window.CONF["pages"][page]["roles"].includes(window.session["user"]["roles"][i])){
-            role_check = true;
-            break;
-          }
-
-      }else role_check = true;
-
-      ///// Plans - similar to role check
-      let plan_check = true; // Should be false in actual implementation
-
-      ///// Final Check: IF All Checks Passed
-      if(
-        authenticity_check === true &&
-        role_check === true &&
-        plan_check === true
-      ) return true;
-
-    }
+			///// Final Check: IF All Checks Passed
+			if(
+				authenticity_check === true &&
+				role_check === true &&
+				plan_check === true
+			) return true;
+		}
 
 
-    // Session Independent Checks
-    if(!("user" in window.session)){
-      ///// Authenticity Statuses
-      let authenticity_check = false;
-      // Unauthenticated User
-      if(
-        !("authenticity_statuses" in window.CONF["pages"][page]) ||
-        "authenticity_statuses" in window.CONF["pages"][page] &&
-        window.CONF["pages"][page]["authenticity_statuses"].includes("unauthenticated")
-      ) authenticity_check = true;
+		// Session Independent Checks
+		if(!("user" in window.session)){
+			///// Authenticity Statuses
+			let authenticity_check = false;
 
-      ///// Roles
-      let role_check = false;
-      if(!("roles" in window.CONF["pages"][page])) role_check = true;
+			// Unauthenticated User
+			if(
+				!("authenticity_statuses" in window.CONF["pages"][page]) ||
+				"authenticity_statuses" in window.CONF["pages"][page] &&
+				window.CONF["pages"][page]["authenticity_statuses"].includes("unauthenticated")
+			) authenticity_check = true;
 
-      ///// Plans
-      let plan_check = false;
-      if(!("plans" in window.CONF["pages"][page])) plan_check = true;
+			///// Roles
+			let role_check = false;
+			if(!("roles" in window.CONF["pages"][page])) role_check = true;
 
-      ///// Final Check: IF All Checks Passed
-      if(
-        authenticity_check === true &&
-        role_check === true &&
-        plan_check === true
-      ) return true;
+			///// Plans
+			let plan_check = false;
+			if(!("plans" in window.CONF["pages"][page])) plan_check = true;
 
-    }
+			///// Final Check: IF All Checks Passed
+			if(
+				authenticity_check === true &&
+				role_check === true &&
+				plan_check === true
+			) return true;
+		}
 
 
-    // Failed The Guard Checks
-    return false;
-
-  }
-
+		// Failed The Guard Checks
+		return false;
+	}
 }
 
 // Make Router Usable W/O Importing It
