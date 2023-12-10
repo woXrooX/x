73f72ca62d2b52404c5ab29cf78e839bc9862bc7
woXrooX <woXrooX@gmail.com>
2023-12-10 11:26:28 +0100

    New 404 UI

--- source/js/pages/pageNotFound.js
+++ source/js/pages/pageNotFound.js
@@ -5,9 +5,9 @@ export const TITLE = window.Lang.use("pageNotFound");
 export default function content(){
 	return `
 		<container class="p-5">
-			<column class="w-50 surface-clean p-5">
+			<column class="w-50 surface-error p-3 gap-0-5">
 				<h1>404</h1>
-				<h3 style="color: var(--color-text-secondary);">${window.Lang.use("pageNotFound")}</h3>
+				<h3 class="text-color-secondary">${window.Lang.use("pageNotFound")}</h3>
 			</column>
 		</container>
 	`;
