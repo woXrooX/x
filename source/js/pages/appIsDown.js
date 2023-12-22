603fd86ad60ff16045ff02dd590ad6063b3a4ac5
woXrooX <woXrooX@gmail.com>
2023-12-23 00:36:15 +0100

    XUI updates

--- source/js/pages/appIsDown.js
+++ source/js/pages/appIsDown.js
@@ -9,7 +9,7 @@ export default function content(){
         <column class="w-50 surface-clean p-5">
           <x-icon name="${window.CONF["default"]["appIsDown"]["icon"]}"></x-icon>
           <h1>${window.CONF["default"]["appIsDown"]["title"]}</h1>
-          <h3 class="color-text-secondary">${window.CONF["default"]["appIsDown"]["explanation"]}</h3>
+          <h3 class="text-color-secondary">${window.CONF["default"]["appIsDown"]["explanation"]}</h3>
         </column>
       </row>
     </container>
