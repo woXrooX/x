da1044498a0782f3b5154349cf0387249fdfbd8d
woXrooX <woXrooX@gmail.com>
2023-12-25 12:54:26 +0100

    Exptl: page tr

--- source/js/SPA/main.js
+++ source/js/SPA/main.js
@@ -1,8 +1,16 @@
 export default class Main{
-  static selector = "body > main";
+	static selector = "body > main";
 
-  // As index.html Already Has body > main Element We Do Not Have To Wait For DOM Creation To Use Query Selector
-  static element = Main.element = document.querySelector(Main.selector);
+	// As index.html Already Has body > main Element We Do Not Have To Wait For DOM Creation To Use Query Selector
+	static element = Main.element = document.querySelector(Main.selector);
+
+	static animationStart(){
+		Main.element.classList.remove("show");
+	}
+
+	static animationEnd(){
+		Main.element.classList.add("show");
+	}
 
 }
 
