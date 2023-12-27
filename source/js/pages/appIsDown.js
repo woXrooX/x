15adcbd02b56410c9c851888561d8f07e65b6f91
woXrooX <woXrooX@gmail.com>
2023-12-27 16:51:39 +0100

    Fix dom tree

--- source/js/pages/appIsDown.js
+++ source/js/pages/appIsDown.js
@@ -1,18 +1,15 @@
 "use strict";
 
 export const TITLE = "App Is Down";
+
 export default function content(){
-
-  return `
-    <container class="p-5">
-      <row>
-        <column class="w-50 surface-clean p-5">
-          <x-icon name="${window.CONF["default"]["appIsDown"]["icon"]}"></x-icon>
-          <h1>${window.CONF["default"]["appIsDown"]["title"]}</h1>
-          <h3 class="text-color-secondary">${window.CONF["default"]["appIsDown"]["explanation"]}</h3>
-        </column>
-      </row>
-    </container>
-  `;
-
+	return `
+		<container class="p-5">
+			<column class="w-50 surface-clean p-5">
+				<x-icon name="${window.CONF["default"]["appIsDown"]["icon"]}"></x-icon>
+				<h1>${window.CONF["default"]["appIsDown"]["title"]}</h1>
+				<h3 class="text-color-secondary">${window.CONF["default"]["appIsDown"]["explanation"]}</h3>
+			</column>
+		</container>
+	`;
 }
