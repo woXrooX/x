093d84555eda5841d303070653ea9cb244160c10
woXrooX <woXrooX@gmail.com>
2024-01-03 09:29:21 +0100

    Prep for pay for what you use

--- source/js/SPA/core.js
+++ source/js/SPA/core.js
@@ -10,7 +10,7 @@ import Logger from "../modules/logger.js";
 import bridge from "../modules/bridge.js";
 
 // Observer -> observe()
-import {observe} from "../modules/tools.js";
+// import {observe} from "../modules/tools.js";
 
 // import CSS from "./css.js";
 // document.head.innerHTML += `<style>${CSS}</style>`;
@@ -52,115 +52,111 @@ import CodeSnippet from "./elements/CodeSnippet/CodeSnippet.js";
 
 export default class Core{
 
-  // Class Static Initialization Block
-  static {
-    // Try To Get Initial Data Then Init The Methods
-    Core.#getInitialData()
-    .then(()=>{
-      Core.#init();
-      Core.#onLoad();
-      Core.#onUrlChange();
-      Core.#onHashChange();
-      Core.#onHistoryButtonClicked();
-      Core.#onDomChange();
-    });
-  }
+	// Class Static Initialization Block
+	static {
+		// Try To Get Initial Data Then Init The Methods
+		Core.#getInitialData()
+		.then(()=>{
+			Core.#init();
+			Core.#onLoad();
+			Core.#onUrlChange();
+			Core.#onHashChange();
+			Core.#onHistoryButtonClicked();
+			Core.#onDomChange();
+		});
+	}
 
-  /////// Initial Data
-  static async #getInitialData(){
-      let response = await window.bridge("api", {for:"initialData"});
+	/////// Initial Data
+	static async #getInitialData(){
+		let response = await window.bridge("api", {for:"initialData"});
 
-      Log.success(response);
+		Log.success(response);
 
-      window.CONF = response["CONF"];
-      window.session = response["session"];
+		window.CONF = response["CONF"];
+		window.session = response["session"];
 
-      window.Language.DICT = response["LANG_DICT"];
+		window.Language.DICT = response["LANG_DICT"];
 
-      window.USER_AUTHENTICITY_STATUSES = response["USER_AUTHENTICITY_STATUSES"];
-      window.USER_ROLES = response["USER_ROLES"];
+		window.USER_AUTHENTICITY_STATUSES = response["USER_AUTHENTICITY_STATUSES"];
+		window.USER_ROLES = response["USER_ROLES"];
 
-      // Load External SVGs To SVG Class
-      window.SVG.set(response["PROJECT_SVG"]);
-  }
+		// Load External SVGs To SVG Class
+		window.SVG.set(response["PROJECT_SVG"]);
+	}
 
-  // Initialize all the initial methods
-  static #init(){
-    Log.info("Core.#init()");
+	// Initialize all the initial methods
+	static #init(){
+		Log.info("Core.#init()");
 
-    Language.init();
+		Language.init();
 
-    CSS.init();
+		CSS.init();
 
-    Menu.init();
+		Menu.init();
 
-    Header.init();
-    Footer.init();
+		Header.init();
+		Footer.init();
 
-    Router.handle();
-  }
+		Router.handle();
+	}
 
-  /////// Event Handlers
-  static #onLoad(){
-    // Works On The First Visit
-    document.addEventListener('readystatechange', ()=>{
-      if(event.target.readyState === 'loading') return;
-      if(event.target.readyState === 'interactive') return;
-      // if(event.target.readyState === 'complete');
+	/////// Event Handlers
+	static #onLoad(){
+		// Works On The First Visit
+		document.addEventListener('readystatechange', ()=>{
+			if(event.target.readyState === 'loading') return;
+			if(event.target.readyState === 'interactive') return;
+			// if(event.target.readyState === 'complete');
 
-      // window.dispatchEvent(new Event('load'));
-      Log.info("onLoad");
+			// window.dispatchEvent(new Event('load'));
+			Log.info("onLoad");
 
-      Router.handle();
+			Router.handle();
 
-      Menu.setActive();
-    });
-  }
+			Menu.setActive();
+		});
+	}
 
-  static #onUrlChange(){
-    window.addEventListener('locationchange', ()=>{
-      // window.dispatchEvent(new Event('locationchange'));
-      Log.info("onUrlChange");
+	static #onUrlChange(){
+		window.addEventListener('locationchange', ()=>{
+			// window.dispatchEvent(new Event('locationchange'));
+			Log.info("onUrlChange");
 
-      Router.handle();
+			Router.handle();
 
-      Menu.setActive();
+			Menu.setActive();
+		});
+	}
 
-    });
-  }
+	static #onHashChange(){
+		window.addEventListener('hashchange', ()=>{
+			// window.dispatchEvent(new Event('hashchange'));
+			Log.info("onHashChange");
+		});
+	}
 
-  static #onHashChange(){
-    window.addEventListener('hashchange', ()=>{
-      // window.dispatchEvent(new Event('hashchange'));
-      Log.info("onHashChange");
+	static #onHistoryButtonClicked(){
+		window.addEventListener('popstate', ()=>{
+			// window.dispatchEvent(new Event('popstate'));
+			Log.info("onHistoryButtonClicked");
 
-    });
-  }
+			Router.handle();
+		});
+	}
 
-  static #onHistoryButtonClicked(){
-    window.addEventListener('popstate', ()=>{
-      // window.dispatchEvent(new Event('popstate'));
-      Log.info("onHistoryButtonClicked");
+	static #onDomChange(){
+		window.addEventListener('domChange', ()=>{
+			// window.dispatchEvent(new CustomEvent('domChange'));
+			// window.dispatchEvent(new CustomEvent("domChange", {detail:"menu"}));
+			Log.info("Core.#onDomChange()");
 
+			// Targets sample event.detail = ["menu", "main"...]
+			// If has target(s) then update the dom. body > target
+			if(!!event.detail === true) DOM.update(event.detail);
 
-      Router.handle();
-
-    });
-  }
-
-  static #onDomChange(){
-    window.addEventListener('domChange', ()=>{
-      // window.dispatchEvent(new CustomEvent('domChange'));
-      // window.dispatchEvent(new CustomEvent("domChange", {detail:"menu"}));
-      Log.info("Core.#onDomChange()");
-
-      // Targets sample event.detail = ["menu", "main"...]
-      // If has target(s) then update the dom. body > target
-      if(!!event.detail === true) DOM.update(event.detail);
-
-      // Globals
-      Hyperlink.collect();
-      Form.collect();
-    });
-  }
+			// Globals
+			Hyperlink.collect();
+			Form.collect();
+		});
+	}
 };
