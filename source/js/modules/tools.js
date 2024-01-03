5f942b70f27396ad1f0a5198912d7ef860c13fd3
woXrooX <woXrooX@gmail.com>
2024-01-03 09:28:55 +0100

    Clean up

--- source/js/modules/tools.js
+++ source/js/modules/tools.js
@@ -3,29 +3,23 @@
 // import {name} from "../modules/tools.js"
 
 export function observe(
-  selector,
-  onObservedAddClassName,
-  onOutOfObservingRemoveClass = false,
-  options={
-    threshhold: 0,
-    rootMargin: "0px"
-  }
+	selector,
+	onObservedAddClassName,
+	onOutOfObservingRemoveClass = false,
+	options={
+		threshhold: 0,
+		rootMargin: "0px"
+	}
 ){
+	const observer = new IntersectionObserver(elements =>{
+		for(const element of elements)
+		if(element.isIntersecting) element.target.classList.add(onObservedAddClassName);
+		else if(onOutOfObservingRemoveClass === true) element.target.classList.remove(onObservedAddClassName);
+	},options);
 
-  const observer = new IntersectionObserver(elements =>{
-    for(const element of elements)
-      if(element.isIntersecting) element.target.classList.add(onObservedAddClassName);
-      else if(onOutOfObservingRemoveClass === true) element.target.classList.remove(onObservedAddClassName);
+	const elements = document.querySelectorAll(selector);
 
-  },options);
+	if(!!elements === false) return;
 
-  const elements = document.querySelectorAll(selector);
-
-  if(!!elements === false) return;
-
-  for(const element of elements) observer.observe(element);
-
+	for(const element of elements) observer.observe(element);
 }
-
-// Make observe() Usable W/O Importing It
-window.observe = observe;
