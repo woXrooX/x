a4747bdddff4fc9629ce74070261ac398bd96f08
woXrooX <woXrooX@gmail.com>
2024-01-04 02:09:36 +0100

    manual guarding

--- source/python/pages/api.py
+++ source/python/pages/api.py
@@ -6,20 +6,32 @@ from python.modules.response import response
 
 @app.route("/api", methods=["POST"])
 def api():
-	if request.get_json()["for"] == "initialData":
-		return make_response(
-			{
-				"CONF": Globals.PUBLIC_CONF,
-				"session": {"user": User.generatePublicSession()} if "user" in session else {},
-				"LANG_DICT": Globals.LANG_DICT,
-				"USER_AUTHENTICITY_STATUSES": Globals.USER_AUTHENTICITY_STATUSES,
-				"USER_ROLES": Globals.USER_ROLES,
-				"PROJECT_SVG": Globals.PROJECT_SVG
-			}, 200)
+	##### Manually guarding since this route is not part of the automated pages.
+	### "application/json"
+	if request.content_type == "application/json":
+		# Invalid JSON
+		if request.get_json() is None: return response(type="warning", message="invalidRequest")
 
-	if request.get_json()["for"] == "changeUserAppColorMode":
-		if "colorMode" not in request.get_json(): return response(type="error", message="invalidValue")
+		# Check if "for" in request
+		if "for" not in request.get_json(): return response(type="warning", message="invalidRequest")
 
-		if not User.setAppColorMode(request.get_json()["colorMode"]): return response(type="error", message="somethingWentWrong")
 
-		return response(type="success", message="saved")
+
+
+		if request.get_json()["for"] == "initialData":
+			return make_response(
+				{
+					"CONF": Globals.PUBLIC_CONF,
+					"session": {"user": User.generatePublicSession()} if "user" in session else {},
+					"LANG_DICT": Globals.LANG_DICT,
+					"USER_AUTHENTICITY_STATUSES": Globals.USER_AUTHENTICITY_STATUSES,
+					"USER_ROLES": Globals.USER_ROLES,
+					"PROJECT_SVG": Globals.PROJECT_SVG
+				}, 200)
+
+		if request.get_json()["for"] == "changeUserAppColorMode":
+			if "colorMode" not in request.get_json(): return response(type="error", message="invalidValue")
+
+			if not User.setAppColorMode(request.get_json()["colorMode"]): return response(type="error", message="somethingWentWrong")
+
+			return response(type="success", message="saved")
