b5bcbbf1f63952958629ecee46b1900fb6fcaa21
woXrooX <woXrooX@gmail.com>
2024-01-14 10:29:39 +0100

    rm jinja usage for title

--- source/js/SPA/title.js
+++ source/js/SPA/title.js
@@ -1,12 +1,10 @@
 "use strict";
 
 export default class Title{
-  static set(newTitle = ""){
-    if(!!newTitle === false)
-      document.title = window.Lang.use(window.CONF["default"]["title"]);
-    else
-      document.title = `${window.Lang.use(newTitle)} | ${window.Lang.use(window.CONF["default"]["title"])}`;
-  }
+	static set(newTitle = ""){
+		if(!!newTitle === false) document.title = window.Lang.use(window.CONF["default"]["title"]);
+		else document.title = `${window.Lang.use(newTitle)} | ${window.Lang.use(window.CONF["default"]["title"])}`;
+	}
 }
 
 // Make Title Usable W/O Importing It
