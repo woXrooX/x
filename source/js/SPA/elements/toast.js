768c242474fbe47f31ff95a95ba7781cef089c6b
woXrooX <woXrooX@gmail.com>
2024-01-26 17:50:42 +0100

    DX impr + prep for the handleByAttributes method

--- source/js/SPA/elements/toast.js
+++ source/js/SPA/elements/toast.js
@@ -1,170 +1,137 @@
+// v0.1.1
+
 "use strict";
 
 export default class Toast extends HTMLElement{
-  static #selector = "body>toasts";
-  static #autoDismissTimer = 5000;
-  static #template = document.createElement("template");
-
-  static {
-    Toast.#template.innerHTML = `
-      <toast>
-        <labelcolor></labelcolor>
-        <main>
-          <icon></icon>
-          <type></type>
-          <content></content>
-        </main>
-        <dismiss></dismiss>
-      </toast>
-    `;
-  }
-
-  constructor(){
-    super();
-
-    // this.attachShadow({mode: 'open'});
-    this.shadow = this.attachShadow({mode: 'closed'});
-
-    Type: {
-      this.typeName = "warning";
-      let attributeType = this.typeName;
-      if(this.hasAttribute("type") === true) attributeType = this.getAttribute("type").toLowerCase();
-      if(["success", "info", "warning", "error"].includes(attributeType)) this.typeName = attributeType;
-      else this.textContent = this.getAttribute("type");
-    }
-
-    CSS: {
-        const style = document.createElement('style');
-
-        style.textContent = `
-        toast{
-          overflow: hidden;
-
-          background-color: var(--color-main-tint-1);
-          padding: var(--padding);
-          margin: 0px;
-          border-radius: var(--radius);
-
-          box-shadow: var(--shadow);
-
-          display: grid;
-          gap: 10px;
-          grid-template-columns:auto 2fr auto;
-          align-items: center;
-
-          animation: fadeIn var(--transition-velocity) ease;
-
-        }
-        @keyframes fadeIn{
-          0%{transform:translateY(-10px);}
-          100%{transform:translateY(0px);}
-        }
-
-        labelColor{
-          background-color: var(--color-${[this.typeName]});
-          height: 100%;
-          width: 5px;
-          border-radius: var(--radius);
-        }
-
-        toast > main{
-          display: grid;
-          grid-template-columns:auto 2fr;
-          grid-template-areas:
-            "icon type"
-            "content content";
-          align-items: center;
-        }
-
-        toast > main > icon{
-          font-size: 30px;
-          padding-right: 5px;
-          color: var(--color-${[this.typeName]});
-          grid-area: icon;
-        }
-
-        toast > main > type{
-          font-size: 1rem;
-          color: var(--color-text-primary);
-          text-transform: uppercase;
-          font-weight: bold;
-          grid-area: type;
-        }
-
-        toast > main > content{
-          color: var(--color-text-secondary);
-          font-size: 0.8rem;
-          grid-area:content;
-        }
-
-        toast > dismiss{
-          color: var(--color-text-primary);
-          font-size: 30px;
-          user-select: none;
-
-          width: 40px;
-          height: 40px;
-
-          border-radius: var(--radius);
-
-          display: grid;
-          place-items: center;
-
-          cursor: pointer;
-
-          transition: var(--transition-velocity) background-color;
-        }
-
-        toast > dismiss:hover{
-          background-color: var(--color-main-tint-2);
-        }
-        `;
-
-        this.shadow.appendChild(style);
-
-    }
-
-    ICONS: {
-		this.ICONS = {
-			"success": "&#10003;",
-			"info": "&#8505;",
-			"warning": "&#9888;",
-			"error": "&#10005;",
-			"close": "&#10005;"
-		}
+	static #selector = "body > toasts";
+	static #autoDismissTimer = 5000;
+	static #template = document.createElement("template");
+	static #ICONS = {
+		"success": "done_circle",
+		"info": "info_circle",
+		"warning": "warning_triangle",
+		"error": "error_hexagon"
 	}
 
-    // Clone And Append Template
-    this.shadow.appendChild(Toast.#template.content.cloneNode(true));
+	constructor(){
+		super();
 
-    // If typeName === TRUE Append Type Specific Icon Else Append "warning" Icon
-    this.shadow.querySelector("toast>main>icon").innerHTML = !!this.ICONS[this.typeName] ? this.ICONS[this.typeName] : this.ICONS["warning"];
+		this.shadow = this.attachShadow({mode: 'closed'});
 
-    // InnerHTML "typeName"
-    this.shadow.querySelector("toast>main>type").innerHTML = window.Lang.use(this.typeName);
+		Type: {
+			this.typeName = "error";
 
-    // InnerHTML "textContent"
-    this.shadow.querySelector("toast>main>content").innerHTML = window.Lang.use(this.textContent);
+			if(Object.keys(Toast.#ICONS).includes(this.getAttribute("type"))) this.typeName = this.getAttribute("type");
+			else this.textContent = `Toast.constructor(): Invalid type "${this.getAttribute('type')}"`;
+		}
 
-    // InnerHTML Close Button Icon
-    this.shadow.querySelector("toast>dismiss").innerHTML = this.ICONS["close"];
+		CSS: {
+			const style = document.createElement('style');
 
-    // Remove Toast On Click Dismiss
-    // dismiss.onclick = ()=> this.remove(); // Bug w/ N sec removal
-    this.shadow.querySelector("toast>dismiss").onclick = ()=> this.style.display = "none";
+			style.textContent = `
+				toast{
+					overflow: hidden;
 
-  }
+					background-color: var(--color-main-tint-1);
+					padding: var(--padding);
+					margin: 0px;
+					border-radius: var(--radius);
 
-  static new(type, content){
-    if(!!type === false || !!content === false) return;
+					box-shadow: var(--shadow);
 
-    document.querySelector(Toast.#selector).innerHTML += `<x-toast type="${type}">${content}</x-toast>`;
+					display: grid;
+					gap: 10px;
+					grid-template-columns:auto 2fr auto;
+					align-items: center;
 
-    // Auto Remove After N Seconds
-    setTimeout(()=>{document.querySelector(Toast.#selector).firstChild?.remove();}, Toast.#autoDismissTimer);
+					animation: fadeIn var(--transition-velocity) ease;
 
-  }
+					& > toast-label-color{
+						background-color: var(--color-${[this.typeName]});
+						height: 100%;
+						width: 5px;
+						border-radius: var(--radius);
+					}
+
+					& > main{
+						display: grid;
+						gap: calc(var(--gap) / 4);
+						grid-template-columns:auto 2fr;
+						grid-template-areas:
+							"icon type"
+							"content content";
+						align-items: center;
+
+						& > icon{
+							font-size: 1rem;
+							grid-area: icon;
+						}
+
+						& > type{
+							font-size: 1rem;
+							text-transform: uppercase;
+							font-weight: bold;
+							grid-area: type;
+						}
+
+						& > content{
+							color: var(--color-text-secondary);
+							font-size: 0.8rem;
+							grid-area:content;
+						}
+					}
+				}
+
+				@keyframes fadeIn{
+					0%{transform:translateY(-10px);}
+					100%{transform:translateY(0px);}
+				}
+			`;
+
+			this.shadow.appendChild(style);
+		}
+
+		this.shadow.innerHTML += `
+			<toast>
+				<toast-label-color></toast-label-color>
+				<main>
+					<icon>
+						<x-icon
+							name="${Toast.#ICONS[this.typeName]}"
+							color="var(--color-${this.typeName})"
+						></x-icon>
+					</icon>
+					<type>${window.Lang.use(this.typeName)}</type>
+					<content>${window.Lang.use(this.textContent)}</content>
+				</main>
+				<x-icon name="x" for="dismiss"></x-icon>
+			</toast>
+		`;
+
+		//// Remove Toast On Click Dismiss
+		// dismiss.onclick = ()=> this.remove(); // Bug w/ N sec removal
+		this.shadow.querySelector("toast > x-icon[for=dismiss]").onclick = ()=> this.style.display = "none";
+	}
+
+	static new(type, content){
+		if(!!type === false || !!content === false) return;
+
+		document.querySelector(Toast.#selector).innerHTML += `<x-toast type="${type}">${content}</x-toast>`;
+
+		// Auto Remove After N Seconds
+		setTimeout(()=>{document.querySelector(Toast.#selector).firstChild?.remove();}, Toast.#autoDismissTimer);
+	}
+
+	//// Handles Toast actions by the passed element's "x-toast" attributes
+	// x-toast -> enables
+	// x-toast="type:error" -> enables with specific only on type action
+	// x-toast-type="error|success" -> force type(s)
+	// x-toast-message -> force message
+	static handleByAttributes(element, response){
+
+	}
 }
+
 customElements.define('x-toast', Toast);
 
 // Make Toast Usable W/O Importing It
