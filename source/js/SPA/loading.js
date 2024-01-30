b5cc7e73cbcc3933314dc254278d2e8e7b313e20
woXrooX <woXrooX@gmail.com>
2024-01-30 11:10:24 +0100

    Clean up

--- source/js/SPA/loading.js
+++ source/js/SPA/loading.js
@@ -1,44 +1,32 @@
 "use strict";
 
 export default class Loading{
-  static selector = "body > loading";
-  static #elementLoading = null;
+	static selector = "body > loading";
+	static #elementLoading = null;
 
-  static {
-    Loading.#elementLoading = document.querySelector(Loading.selector);
+	static {
+		Loading.#elementLoading = document.querySelector(Loading.selector);
+	}
 
-  }
+	static start(){
+		// Check if body > loading exists
+		if(!!Loading.#elementLoading === false) return;
 
-  static start(){
-    // Check if body > loading exists
-    if(!!Loading.#elementLoading === false) return;
-    // console.log("start");
+		Loading.#elementLoading.style.zIndex = window.CSS.getValue("--z-loading");
+		Loading.#elementLoading.style.opacity = 1;
+	}
 
-    // Set z-index To loading
-    Loading.#elementLoading.style.zIndex = window.CSS.getValue("--z-loading");
+	static end(){
+		// Check if body > loading exists
+		if(!!Loading.#elementLoading === false) return;
 
-    // Change The Opacity To 1
-    Loading.#elementLoading.style.opacity = 1;
+		Loading.#elementLoading.style.opacity = 0;
 
-  }
-
-  static end(){
-    // Check if body > loading exists
-    if(!!Loading.#elementLoading === false) return;
-    // console.log("end");
-
-    // Change The Opacity To 0
-    Loading.#elementLoading.style.opacity = 0;
-
-    // Wait For The Duration Of --transition-velocity
-    setTimeout(()=>{
-      // Set z-index To minus
-      Loading.#elementLoading.style.zIndex = window.CSS.getValue("--z-minus");
-
-    }, parseInt(getComputedStyle(document.body).getPropertyValue('--transition-velocity')));
-
-  }
+		// Wait for the duration of --transition-velocity
+		setTimeout(()=>{
+			Loading.#elementLoading.style.zIndex = window.CSS.getValue("--z-minus");
+		}, parseInt(getComputedStyle(document.body).getPropertyValue('--transition-velocity')));
+	}
 };
 
-// Make Loading Usable W/O Importing It
 window.Loading = Loading;
