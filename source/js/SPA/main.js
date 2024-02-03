07f086888a7e712fa7623cfa901e396c7a97e3d4
woXrooX <woXrooX@gmail.com>
2024-02-03 23:36:02 +0100

    Method for gen situational content for devs

--- source/js/SPA/main.js
+++ source/js/SPA/main.js
@@ -12,6 +12,31 @@ export default class Main{
 		Main.element.classList.add("show");
 	}
 
+	static situationalContent(type = "error", contentTitle = "ERROR", content = "ERROR", documentTitle = null){
+		if(documentTitle === null) documentTitle = type.toUpperCase();
+
+		let icons = {
+			"success": "done_circle",
+			"info": "info_circle",
+			"warning": "warning_triangle",
+			"error": "error_hexagon"
+		}
+
+		Title.set(documentTitle);
+
+		return `
+			<container class="flex-y-center p-5">
+				<column class="flex-y-center surface-${type} p-8 gap-2">
+					<p class="d-flex flex-row flex-y-center gap-0-2 text-color-${type} text-size-2">
+						<x-svg name="${icons[type]}" class="text-size-2-5" color="var(--color-${type})"></x-svg>
+						${contentTitle}
+					</p>
+					<p class="text-size-1-1">${content}</p>
+				</column>
+			</container>
+		`;
+	}
+
 }
 
 // Make Main Usable W/O Importing It
