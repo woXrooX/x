047276665477ea1bc323d87a04a7dbf0a756cfa1
woXrooX <woXrooX@gmail.com>
2024-04-13 10:17:59 +0500

    Implemented instructions for x-modal attribute similar to XRequest instructions

--- source/js/SPA/elements/modal.js
+++ source/js/SPA/elements/modal.js
@@ -1,6 +1,8 @@
 "use strict";
 
 export default class Modal extends HTMLElement{
+	/////////////////////////// Static
+
 	static #selector = "body > modal";
 	static #element = null;
 	static elementMain = null;
@@ -18,13 +20,39 @@ export default class Modal extends HTMLElement{
 		Cover.onClickExecute(Modal.hide);
 	}
 
-	static async #execute_on_show(func_name){
-		if(!!func_name === false) return;
-		await Modal.#FUNC_POOL[func_name]();
-	}
-
+	/////////// APIs
 	static push_func(func){ Modal.#FUNC_POOL[func.name] = func; }
 
+	static hide(){
+		if(Modal.#shown === false) return;
+
+		Modal.#shown = false;
+
+		Modal.#element.classList.remove("show");
+
+		Modal.#element.ontransitionend = ()=>{
+			Modal.elementMain.replaceChildren();
+			Modal.#element.ontransitionend = null;
+		};
+
+		Cover.hide();
+	}
+
+	static handle_commands(commands, type){
+		if(commands === false) return;
+		if(type === false) return;
+
+		const instructions = Modal.#parse_commands(commands);
+
+		for(const instruction of instructions){
+			if(instruction["types"].includes("any") || instruction["types"].includes(type)){
+				Modal.#handle_command_action(instruction["action"]);
+				break;
+			}
+		}
+	}
+
+	/////////// Helpers
 	static #show(DOM, func_name = null){
 		if(Modal.#shown === true) return;
 
@@ -37,22 +65,47 @@ export default class Modal extends HTMLElement{
 		Cover.show();
 	}
 
-	static hide(){
-		if(Modal.#shown === false) return;
-
-		Modal.#shown = false;
-
-		Modal.#element.classList.remove("show");
-
-		Modal.#element.ontransitionend = ()=>{
-			Modal.elementMain.innerHTML = "";
-			Modal.#element.ontransitionend = null;
-		};
-
-		Cover.hide();
+	static async #execute_on_show(func_name){
+		if(!!func_name === false) return;
+		await Modal.#FUNC_POOL[func_name]();
 	}
 
-	/////////////// x-modal object
+	static #parse_commands(commands){
+		const instructions = [];
+
+		if(commands === false) return instructions;
+
+		commands = commands.split(' ');
+
+		for(const command of commands){
+			const parts = command.split(':');
+
+			// Invalid command
+			if(parts.length !== 3) continue;
+
+			instructions.push({
+				"types": parts[1].split('|'),
+				"action": parts[2]
+			});
+		}
+
+		return instructions;
+	}
+
+	static #handle_command_action(command){
+		switch(command){
+			case "hide":
+				Modal.hide();
+				break;
+
+			default:
+				Log.warning("Modal.handle_commands(): Invalid action");
+				break;
+		}
+	}
+
+	/////////////////////////// Object
+
 	#DOM = null;
 
 	constructor(){
