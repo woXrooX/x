e59ea238537591dffb968cb4ba927d9971313a69
woXrooX <woXrooX@gmail.com>
2024-04-29 14:35:45 +0500

    Fix: on email default title is not working

--- source/python/modules/SendGrid.py
+++ source/python/modules/SendGrid.py
@@ -42,11 +42,11 @@ if __name__ != "__main__":
 				<html>
 				<head>
 					<meta charset="utf-8">
-					<title>{Globals.CONF["default"]["title"]}</title>
+					<title>{Globals.PROJECT_LANG_DICT.get(Globals.CONF["default"]["title"], {}).get(Globals.CONF["default"]["language"]["fallback"], "x")}</title>
 				</head>
 				<body style="width:100%; padding:10px;">
 					<header style="text-aligment:center; color: white; padding:5px; background-color:hsl({Globals.CONF["default"]["color"]["brand"]["hue"]}deg, {Globals.CONF["default"]["color"]["brand"]["saturation"]}%, {Globals.CONF["default"]["color"]["brand"]["lightness"]}%); width:100%;">
-					{Globals.CONF["default"]["title"]}
+					{Globals.PROJECT_LANG_DICT.get(Globals.CONF["default"]["title"], {}).get(Globals.CONF["default"]["language"]["fallback"], "x")}
 					</header>
 					<main>{content}</main>
 					<footer></footer>
