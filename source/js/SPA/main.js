08b285f8f8b1b8b66b725c5152d03c51a891127f
woXrooX <woXrooX@gmail.com>
2024-05-07 16:07:29 +0500

    Centering the error text

--- source/js/SPA/main.js
+++ source/js/SPA/main.js
@@ -26,12 +26,12 @@ export default class Main{
 
 		return `
 			<container class="flex-y-center p-5">
-				<column class="flex-y-center surface-${type} p-8 gap-2">
+				<column class="flex-center surface-${type} p-8 gap-2">
 					<p class="d-flex flex-row flex-y-center gap-0-2 text-color-${type} text-size-2">
 						<x-svg name="${icons[type]}" class="text-size-2-5" color="var(--color-${type})"></x-svg>
 						${contentTitle}
 					</p>
-					<p class="text-size-1-1">${content}</p>
+					<p class="text-size-1-1 text-align-center">${content}</p>
 				</column>
 			</container>
 		`;
