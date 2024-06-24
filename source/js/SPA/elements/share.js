bb41e1894c730152627e11020ff942e1c6639be6
woXrooX <woXrooX@gmail.com>
2024-06-24 09:32:00 +0500

    Share mobile UI fix

--- source/js/SPA/elements/share.js
+++ source/js/SPA/elements/share.js
@@ -22,7 +22,7 @@ export default class Share extends HTMLElement{
 		this.innerHTML = `
 			<x-svg name="share" id="modal_share"></x-svg>
 			<x-modal trigger_selector="x-svg#modal_share">
-				<row class="p-1 gap-0-5">
+				<row class="flex-row flex-x-between p-10 gap-1 text-size-1-5">
 					<a href="https://twitter.com/intent/tweet?url=${shareData.url}&text=${shareData.text}" target="_blank">
 						<x-svg name="twitter_bird_original"></x-svg>
 					</a>
