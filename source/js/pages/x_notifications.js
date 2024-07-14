9d7370faa89c5abe3669202433909a56e1505cef
Muhammadrizo <madaminov.co@gmail.com>
2024-07-14 13:26:24 +0500

    some styling

--- source/js/pages/x_notifications.js
+++ source/js/pages/x_notifications.js
@@ -20,13 +20,15 @@ export async function after(){
 
 		let HTML = "";
 		let content = "";
+		let surface = "";
 		for(const notification of notifications){
 			!!notification['event'] ? content = `@${notification['sender']} ${Lang.use(notification['event'])}` : content = notification['content'];
+			!!notification['type'] ? surface = `surface-${notification['type']}` : surface = 'bg-2 bs-default';
 
 			HTML += `
-				<a href="/x/notification/${notification["id"]}" class="w-100 p-2 d-flex flex-column flex-y-end surface-${notification['type']}">
-					<p class="w-100">${content}</p>
-					<p class="text-size-0-6">${TTHR(notification["timestamp"])}</p>
+				<a href="/x/notification/${notification["id"]}" class="min-h-50px w-100 p-1 px-3 d-flex flex-row flex-x-between flex-y-center gap-0-5 ${surface}">
+					<p class="w-100 text-size-0-8">${content}</p>
+					<p class="w-auto text-size-0-6 text-color-secondary text-nowrap">${TTHR(notification["timestamp"])}</p>
 				</a>
 			`;
 		}
