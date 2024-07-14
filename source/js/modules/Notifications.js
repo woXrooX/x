3c7e3acf388eba4a1f744758082b6310fb53876a
woXrooX <woXrooX@gmail.com>
2024-07-14 17:17:35 +0500

    NCG

--- source/js/modules/Notifications.js
+++ source/js/modules/Notifications.js
@@ -1,11 +1,20 @@
 import { TTHR } from "/js/modules/datetime/datetime.js";
 
-// Notification content generator
-export function NCG(notification){
+// Notification card content generator
+export function notification_card_generator(notification){
 	return `
 		<a href="/x/notification/${notification["id"]}" class="min-h-50px w-100 p-1 px-3 d-flex flex-row flex-x-between gap-0-5 ${!!notification['type'] ? `surface-${notification['type']}` : 'bg-2 bs-default'}">
 			<p class="w-100 text-size-0-8">${notification["event"]} | ${notification["type"]} | ${notification["sender"]} | ${notification["content"]}</p>
 			<p class="w-auto text-size-0-6 text-color-secondary text-nowrap">${TTHR(notification["timestamp"])}</p>
 		</a>
+	`;
+}
+
+export function notification_content_generator(notification){
+	return `
+		<row class="min-h-50px w-100 p-1 px-3 flex-row flex-x-between gap-0-5">
+			<p class="w-100 text-size-0-8">${notification["event"]} | ${notification["type"]} | ${notification["sender"]} | ${notification["content"]}</p>
+			<p class="w-auto text-size-0-6 text-color-secondary text-nowrap">${TTHR(notification["timestamp"])}</p>
+		</row>
 	`;
 }
