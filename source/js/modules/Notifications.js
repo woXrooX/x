5ca924553d1d62a9409ff6f1f0a7b517d24c0a21
Muhammadrizo <madaminov.co@gmail.com>
2024-07-16 18:50:12 +0500

    grayscale added

--- source/js/modules/Notifications.js
+++ source/js/modules/Notifications.js
@@ -3,7 +3,14 @@ import { TTHR } from "/js/modules/datetime/datetime.js";
 // Notification card content generator
 export function notification_card_generator(notification){
 	return `
-		<a href="/x/notification/${notification["id"]}" class="min-h-50px w-100 p-1 px-3 d-flex flex-row flex-x-between gap-0-5 ${!!notification['type'] ? `surface-${notification['type']}` : 'bg-2 bs-default'}">
+		<a
+			href="/x/notification/${notification["id"]}"
+			class="
+				min-h-50px w-100 p-1 px-3 d-flex flex-row flex-x-between gap-0-5
+				${!!notification['type'] ? `surface-${notification['type']}` : 'bg-2 bs-default'}
+				${notification['seen'] == 1 ? "grayscale-95" : ''}
+			"
+		>
 			<p class="w-100 text-size-0-8">${notification["event"]} | ${notification["type"]} | ${notification["sender"]} | ${notification["content"]}</p>
 			<p class="w-auto text-size-0-6 text-color-secondary text-nowrap">${TTHR(notification["timestamp"])}</p>
 		</a>
