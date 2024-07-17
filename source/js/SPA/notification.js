a2cef158aa0448009a5297390b0272a87dc34259
woXrooX <woXrooX@gmail.com>
2024-07-17 19:14:44 +0500

    Class to work with notifs

---
+++ source/js/SPA/notification.js
@@ -0,0 +1,16 @@
+export default class Notification{
+	static unseen_count = 0;
+
+	static async update_unseen_count(){
+		if(!("user" in window.session)) return;
+
+		let data = await window.bridge({for: "get_unseen_count"}, "/x/notifications");
+		if("error" in data) return;
+		if(!("data" in data)) return;
+
+		Notification.unseen_count = data["data"];
+	}
+
+}
+
+window.x["Notification"] = Notification;
