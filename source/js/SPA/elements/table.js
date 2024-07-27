7fc82831ddc2a191ccba8b8919b4bd31052be8b7
woXrooX <woXrooX@gmail.com>
2024-07-27 22:46:37 +0500

    Style + add: missing svg + fix: multiple matches in search

--- source/js/SPA/elements/table.js
+++ source/js/SPA/elements/table.js
@@ -1,8 +1,5 @@
-// v0.1.1.0
-// Uses XUI
-
 export default class Table extends HTMLElement{
-	static sortModes = Object.freeze({ASC: 1, DESC: 2});
+	static #sort_modes = Object.freeze({ASC: 1, DESC: 2});
 
 	constructor(){
 		super();
@@ -12,36 +9,36 @@ export default class Table extends HTMLElement{
 
 		// Check if body values exists
 		if(!("body" in this.JSON)) return;
-		this.bodyValues = this.JSON["body"];
-		this.bodyValuesInChunks = [];
+		this.body_values = this.JSON["body"];
+		this.body_values_in_chunks = [];
 
 
 		// Sortable column IDs
-		this.sortableColumnIDs = [];
-		this.lastSortedColumnID = null;
-		this.lastSortMode = null;
+		this.sortable_column_ids = [];
+		this.last_sorted_column_id = null;
+		this.last_sort_mode = null;
 
-		this.pageSize = 10;
-		this.currentPage = 1;
+		this.page_size = 10;
+		this.current_page = 1;
 
 		// Encoded columns info holder
-		this.encodedColumns = [];
+		this.encoded_columns = [];
 
 
 		// Init table
-		this.#build();
+		this.#init();
 
-		this.#listenToTheSortClicks();
+		this.#listen_to_the_sort_clicks();
 	}
 
-	#build = ()=>{
+	#init = ()=>{
 		this.innerHTML = `
 			<main class="d-flex flex-column gap-0-5">
 
-				<row class="flex-y-center flex-x-between gap-0-5">
+				<row class="flex-row flex-y-center flex-x-between gap-0-5">
 					<column class="w-auto">
 						<select>
-							<option selected disabled>${this.pageSize}</option>
+							<option selected disabled>${this.page_size}</option>
 							<option value="10">10</option>
 							<option value="15">15</option>
 							<option value="20">20</option>
@@ -56,8 +53,8 @@ export default class Table extends HTMLElement{
 
 				<div for="table"></div>
 
-				<div for="paginationContainer" class="d-flex flex-row flex-x-between">
-					<div for="showingCounter" class="d-flex flex-row flex-x-start flex-y-center text-size-0-8"></div>
+				<div for="pagination_container" class="d-flex flex-row flex-x-between">
+					<div for="showing_counter" class="d-flex flex-row flex-x-start flex-y-center text-size-0-8"></div>
 					<div for="pagination" class="d-flex flex-row flex-x-end gap-0-2"></div>
 				</div>
 
@@ -65,17 +62,16 @@ export default class Table extends HTMLElement{
 		`;
 
 
-		this.#listenToPageSizeSelect();
+		this.#listen_to_page_size_select();
 
-		this.#listenToSearchTyping();
+		this.#listen_to_search_typing();
 
-		this.#buildTable();
+		this.#build_table();
 
-		this.#buildPageButtons();
+		this.#build_page_buttons();
 	}
 
-	///////////// Table
-	#buildTable = ()=>{
+	#build_table = ()=>{
 		this.querySelector("main > div[for=table]").innerHTML = `
 			<table class="${this.getAttribute("class") || ""}">
 				<thead><tr></tr></thead>
@@ -84,12 +80,11 @@ export default class Table extends HTMLElement{
 			</table>
 		`;
 
-		this.#buildHead();
-		this.#buildBody();
-		this.#buildFoot();
+		this.#build_head();
+		this.#build_body();
+		this.#build_foot();
 	}
-
-	#buildHead = ()=>{
+	#build_head = ()=>{
 		if(!("head" in this.JSON)) return;
 		let HTML = "";
 
@@ -99,14 +94,14 @@ export default class Table extends HTMLElement{
 			// Check if sortable
 			if("sortable" in this.JSON["head"][index] && this.JSON["head"][index]["sortable"] === true){
 				// Save sortable column ID
-				this.sortableColumnIDs.push(index);
+				this.sortable_column_ids.push(index);
 
 				// Create sort icon
 				HTML += `
 					<th>
 						<row class="cursor-pointer gap-0-5 flex-y-center">
 							${this.JSON["head"][index]["title"]}
-							<x-svg color="currentcolor" name="sort_ASC"></x-svg>
+							<x-svg name="sort_ASC" toggle="sort_DESC"></x-svg>
 						</row>
 					</th>
 				`;
@@ -116,30 +111,29 @@ export default class Table extends HTMLElement{
 			else HTML += `<th>${this.JSON["head"][index]["title"]}</th>`;
 
 			// Check if this column is encoded
-			if("encoded" in this.JSON["head"][index] && this.JSON["head"][index]["encoded"] === true) this.encodedColumns.push(true);
-			else this.encodedColumns.push(false);
+			if("encoded" in this.JSON["head"][index] && this.JSON["head"][index]["encoded"] === true) this.encoded_columns.push(true);
+			else this.encoded_columns.push(false);
 
 
 		}
 
 		this.querySelector("table > thead > tr").innerHTML = HTML;
 	}
-
-	#buildBody = ()=>{
+	#build_body = ()=>{
 		let HTML = "";
 
-		this.#divideDataIntoChunks();
+		this.#divide_data_into_chunks();
 
-		if(this.bodyValuesInChunks.length === 0){
+		if(this.body_values_in_chunks.length === 0){
 			this.querySelector("table > tbody").innerHTML = `<tr><td>${window.Lang.use("no_data")}</td></tr>`;
 			return;
 		}
 
-		for(const row of this.bodyValuesInChunks[this.currentPage-1]){
+		for(const row of this.body_values_in_chunks[this.current_page-1]){
 			HTML += "<tr>";
 
 			for(let index = 0; index < row.length; index++)
-				if(this.encodedColumns[index] === true) HTML += `<td>${decodeURIComponent(row[index])}</td>`;
+				if(this.encoded_columns[index] === true) HTML += `<td>${decodeURIComponent(row[index])}</td>`;
 				else HTML += `<td>${row[index]}</td>`;
 
 			HTML += "</tr>";
@@ -147,8 +141,7 @@ export default class Table extends HTMLElement{
 
 		this.querySelector("table > tbody").innerHTML = HTML;
 	}
-
-	#buildFoot = ()=>{
+	#build_foot = ()=>{
 		if(!("foot" in this.JSON)) return;
 		let HTML = "";
 		for(const cell of this.JSON["foot"]) HTML += `<td>${cell}</td>`;
@@ -157,131 +150,140 @@ export default class Table extends HTMLElement{
 	}
 
 	////////// Sort
-	// Sort click listeners
-	#listenToTheSortClicks = ()=>{
-		for(const id of this.sortableColumnIDs){
-			this.querySelector(`table > thead > tr > th:nth-child(${id+1})`).onclick = ()=>{
-				// Update last sorted column ID with the current clicked column ID
-				this.lastSortedColumnID = id;
+	#listen_to_the_sort_clicks = ()=>{
+		for(const id of this.sortable_column_ids){
+			const th_element = this.querySelector(`table > thead > tr > th:nth-child(${id+1})`);
 
-				switch(this.lastSortMode){
-				case Table.sortModes.ASC:
-					this.#sortDESC();
-					this.lastSortMode = Table.sortModes.DESC;
+			th_element.onclick = ()=>{
+				// Update last sorted column ID with the current clicked column ID
+				this.last_sorted_column_id = id;
+
+				th_element.querySelector("row > x-svg").forceToggle();
+
+				switch(this.last_sort_mode){
+				case Table.#sort_modes.ASC:
+					this.#sort_DESC();
+					this.last_sort_mode = Table.#sort_modes.DESC;
 					break;
 
-				case Table.sortModes.DESC:
-					this.#sortASC();
-					this.lastSortMode = Table.sortModes.ASC;
+				case Table.#sort_modes.DESC:
+					this.#sort_ASC();
+					this.last_sort_mode = Table.#sort_modes.ASC;
 					break;
 
 				default:
-					this.#sortASC();
-					this.lastSortMode = Table.sortModes.ASC;
+					this.#sort_ASC();
+					this.last_sort_mode = Table.#sort_modes.ASC;
 				}
 
 				// Update table view
-				this.#buildBody();
+				this.#build_body();
 			};
 		}
 	}
 
 	// Sort algo ASC
-	#sortASC = ()=>{
-		this.bodyValues.sort((a, b)=>{
+	#sort_ASC = ()=>{
+		this.body_values.sort((a, b)=>{
 			// Numerical comparison
-			if(!isNaN(a[this.lastSortedColumnID]) && !isNaN(b[this.lastSortedColumnID]))
-			return a[this.lastSortedColumnID] - b[this.lastSortedColumnID];
+			if(!isNaN(a[this.last_sorted_column_id]) && !isNaN(b[this.last_sorted_column_id]))
+			return a[this.last_sorted_column_id] - b[this.last_sorted_column_id];
 
 			// String comparison
-			else return a[this.lastSortedColumnID].localeCompare(b[this.lastSortedColumnID]);
+			else return a[this.last_sorted_column_id].localeCompare(b[this.last_sorted_column_id]);
 		});
 	}
 
   	// Sort algo DESC
-	#sortDESC = ()=>{
-		this.bodyValues.sort((a, b)=>{
+	#sort_DESC = ()=>{
+		this.body_values.sort((a, b)=>{
 			// Numerical comparison
-			if(!isNaN(a[this.lastSortedColumnID]) && !isNaN(b[this.lastSortedColumnID]))
-				return b[this.lastSortedColumnID] - a[this.lastSortedColumnID];
+			if(!isNaN(a[this.last_sorted_column_id]) && !isNaN(b[this.last_sorted_column_id]))
+				return b[this.last_sorted_column_id] - a[this.last_sorted_column_id];
 
 			// String comparison
-			else return b[this.lastSortedColumnID].localeCompare(a[this.lastSortedColumnID]);
+			else return b[this.last_sorted_column_id].localeCompare(a[this.last_sorted_column_id]);
 		});
 	}
 
-	#sortByValue = (value)=>{
-		this.bodyValues = [];
+	#sort_by_value = (value)=>{
+		this.body_values = [];
 
 		const VALUE_LOWER_CASE = value.toLowerCase();
 
-		for(const row of this.JSON["body"])
-			for(const cell of row)
-				if(typeof cell === "string" && cell.toLowerCase().includes(VALUE_LOWER_CASE)) this.bodyValues.push(row);
+		loop_row: for(const row of this.JSON["body"])
+			loop_cell: for(const cell of row)
+				if(typeof cell === "string" && cell.toLowerCase().includes(VALUE_LOWER_CASE)){
+					this.body_values.push(row);
+					break loop_cell;
+				}else if(String(cell).toLowerCase().includes(VALUE_LOWER_CASE)){
+					this.body_values.push(row);
+					break loop_cell;
+				}
 
-				else if(String(cell).toLowerCase().includes(VALUE_LOWER_CASE)) this.bodyValues.push(row);
+		console.log(this.body_values);
 
-		if(this.bodyValues.length === 0) this.bodyValues = [[window.Lang.use("no_matches")]];
+		if(this.body_values.length === 0) this.body_values = [[window.Lang.use("no_matches")]];
 	}
 
 	// Divide data to chunks aka pages
-	#divideDataIntoChunks = ()=>{
+	#divide_data_into_chunks = ()=>{
 		// Empty the chunks
-		this.bodyValuesInChunks = [];
+		this.body_values_in_chunks = [];
 
-		for(let i = 0; i < this.bodyValues.length; i += this.pageSize)
-			this.bodyValuesInChunks.push(this.bodyValues.slice(i, i + this.pageSize));
+		for(let i = 0; i < this.body_values.length; i += this.page_size)
+			this.body_values_in_chunks.push(this.body_values.slice(i, i + this.page_size));
 	}
 
 	///////////// Search input
-	#listenToSearchTyping = ()=>{
+	#listen_to_search_typing = ()=>{
 		this.querySelector("main > row > column > input").oninput = ()=>{
 			if(event.target.value == ""){
-				this.bodyValues = this.JSON["body"];
-				this.#buildBody();
+				this.body_values = this.JSON["body"];
+				this.#build_body();
 				return;
 			}
-			this.#sortByValue(event.target.value);
-			this.#buildBody();
+			this.#sort_by_value(event.target.value);
+			this.#build_body();
 		}
 	}
 
 	///////////// Pagination
-	#listenToPageSizeSelect = ()=>{
+	#listen_to_page_size_select = ()=>{
 		this.querySelector("main > row > column > select").onchange = ()=>{
 			const selectedPageSize = event.target.value;
 
 			// If not a number then show all
-			if(isNaN(selectedPageSize)) this.pageSize = this.JSON["body"].length;
+			if(isNaN(selectedPageSize)) this.page_size = this.JSON["body"].length;
 
 			// Else set page size to the selected
-			else this.pageSize = parseInt(selectedPageSize);
+			else this.page_size = parseInt(selectedPageSize);
 
-			// Reset "currentPage" to 1
-			this.currentPage = 1;
+			// Reset "current_page" to 1
+			this.current_page = 1;
 
 			// Update and build the body
-			this.#buildBody();
+			this.#build_body();
 
 			// Update and build the page buttons
-			this.#buildPageButtons();
+			this.#build_page_buttons();
 		}
 	}
 
-	#buildShowingCounter = ()=>{
-		this.querySelector("main > div[for=paginationContainer]:last-child > div[for=showingCounter]").innerHTML = `
-			Page ${this.currentPage} of ${this.bodyValuesInChunks.length}
+	#build_showing_counter = ()=>{
+		this.querySelector("main > div[for=pagination_container]:last-child > div[for=showing_counter]").innerHTML = `
+			Page ${this.current_page} of ${this.body_values_in_chunks.length}
 		`;
 	}
 
-	#buildPageButtons = ()=>{
-		this.#buildShowingCounter();
+	#build_page_buttons = ()=>{
+		this.#build_showing_counter();
 
 		let buttonsHTML = "";
 
-		for(let i = 1; i <= this.bodyValuesInChunks.length; i++) buttonsHTML += `<button class="btn btn-primary btn-s d-none" name="${i}">${i}</button>`;
+		for(let i = 1; i <= this.body_values_in_chunks.length; i++) buttonsHTML += `<button class="btn btn-primary btn-s d-none" name="${i}">${i}</button>`;
 
-		this.querySelector("main > div[for=paginationContainer]:last-child > div[for=pagination]").innerHTML = `
+		this.querySelector("main > div[for=pagination_container]:last-child > div[for=pagination]").innerHTML = `
 			<button class="btn btn-primary btn-s text-transform-uppercase" name="first">${window.Lang.use("first")}</button>
 			<button class="btn btn-primary btn-s" name="previous"><x-svg name="arrow_back" color="white"></x-svg></button>
 			<section class="d-flex flex-row gap-0-2">${buttonsHTML}</section>
@@ -289,24 +291,24 @@ export default class Table extends HTMLElement{
 			<button class="btn btn-primary btn-s text-transform-uppercase" name="last">${window.Lang.use("last")}</button>
 		`;
 
-		this.firstButton = this.querySelector(`main > div[for=paginationContainer]:last-child > div[for="pagination"] > button[name=first]`);
-		this.previousButton = this.querySelector(`main > div[for=paginationContainer]:last-child > div[for="pagination"] > button[name=previous]`);
-		this.nextButton = this.querySelector(`main > div[for=paginationContainer]:last-child > div[for=pagination] > button[name=next]`);
-		this.lastButton = this.querySelector(`main > div[for=paginationContainer]:last-child > div[for="pagination"] > button[name=last]`);
+		this.firstButton = this.querySelector(`main > div[for=pagination_container]:last-child > div[for="pagination"] > button[name=first]`);
+		this.previousButton = this.querySelector(`main > div[for=pagination_container]:last-child > div[for="pagination"] > button[name=previous]`);
+		this.nextButton = this.querySelector(`main > div[for=pagination_container]:last-child > div[for=pagination] > button[name=next]`);
+		this.lastButton = this.querySelector(`main > div[for=pagination_container]:last-child > div[for="pagination"] > button[name=last]`);
 
-		this.#updateButtons(this.currentPage);
-		this.#listenToPageButtonsClicks();
+		this.#update_buttons(this.current_page);
+		this.#listen_to_page_buttons_clicks();
 	}
 
-	#hideButtons = ()=>{
-		const buttons = this.querySelectorAll("main > div[for=paginationContainer]:last-child > div[for=pagination] > section > button");
+	#hide_buttons = ()=>{
+		const buttons = this.querySelectorAll("main > div[for=pagination_container]:last-child > div[for=pagination] > section > button");
 
 		for(const button of buttons) button.classList.add("d-none");
 	}
 
-	#updateButtons = (id)=>{
+	#update_buttons = (id)=>{
 		// First hide buttons
-		this.#hideButtons();
+		this.#hide_buttons();
 
 		//// Enable/Disable main buttons
 		// Enable/Disable "first" button
@@ -318,18 +320,18 @@ export default class Table extends HTMLElement{
 		else this.previousButton.disabled = true;
 
 		// Enable/Disable "next" button
-		if(id == this.bodyValuesInChunks.length) this.nextButton.disabled = true;
+		if(id == this.body_values_in_chunks.length) this.nextButton.disabled = true;
 		else this.nextButton.disabled = false;
 
 		// Enable/Disable "last" button
-		if(id == this.bodyValuesInChunks.length) this.lastButton.disabled = true;
+		if(id == this.body_values_in_chunks.length) this.lastButton.disabled = true;
 		else this.lastButton.disabled = false;
 
 
 		const buttons = [
-			this.querySelector(`main > div[for=paginationContainer]:last-child > div[for=pagination] > section > button:nth-child(${id-1})`),
-			this.querySelector(`main > div[for=paginationContainer]:last-child > div[for=pagination] > section > button:nth-child(${id})`),
-			this.querySelector(`main > div[for=paginationContainer]:last-child > div[for=pagination] > section > button:nth-child(${id+1})`)
+			this.querySelector(`main > div[for=pagination_container]:last-child > div[for=pagination] > section > button:nth-child(${id-1})`),
+			this.querySelector(`main > div[for=pagination_container]:last-child > div[for=pagination] > section > button:nth-child(${id})`),
+			this.querySelector(`main > div[for=pagination_container]:last-child > div[for=pagination] > section > button:nth-child(${id+1})`)
 		];
 
 		for(const button of buttons) button?.classList.remove("d-none", "disabled", "text-decoration-underline");
@@ -339,33 +341,31 @@ export default class Table extends HTMLElement{
 		buttons[1]?.classList.add("text-decoration-underline");
 
 		// Update body after current page changes
-		this.#buildBody();
+		this.#build_body();
 
 		// Update showing counter
-		this.#buildShowingCounter();
+		this.#build_showing_counter();
 	}
 
-	#listenToPageButtonsClicks = ()=>{
+	#listen_to_page_buttons_clicks = ()=>{
 		// first
-		this.firstButton.onclick = ()=> this.#updateButtons((this.currentPage = 1));
+		this.firstButton.onclick = ()=> this.#update_buttons((this.current_page = 1));
 
 		// previous
-		this.previousButton.onclick = ()=> this.#updateButtons(--this.currentPage);
+		this.previousButton.onclick = ()=> this.#update_buttons(--this.current_page);
 
 		// next
-		this.nextButton.onclick = ()=> this.#updateButtons(++this.currentPage);
+		this.nextButton.onclick = ()=> this.#update_buttons(++this.current_page);
 
 		// last
-		this.lastButton.onclick = ()=> this.#updateButtons((this.currentPage = this.bodyValuesInChunks.length));
+		this.lastButton.onclick = ()=> this.#update_buttons((this.current_page = this.body_values_in_chunks.length));
 
-		// 1 to this.bodyValuesInChunks.length
-		const buttons = this.querySelectorAll("main > div[for=paginationContainer]:last-child > div[for=pagination] > section > button");
+		// 1 to this.body_values_in_chunks.length
+		const buttons = this.querySelectorAll("main > div[for=pagination_container]:last-child > div[for=pagination] > section > button");
 
-		for(const button of buttons) button.onclick = ()=> this.#updateButtons((this.currentPage = parseInt(button.name)));
+		for(const button of buttons) button.onclick = ()=> this.#update_buttons((this.current_page = parseInt(button.name)));
 	}
 };
 
 window.customElements.define('x-table', Table);
-
-// Make Table Usable W/O Importing It
-window.Table = Table;
+window.x["Table"] = Table;
