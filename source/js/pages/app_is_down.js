602b71901bc65c8aceb6d95efa637a7c220f6795
woXrooX <woXrooX@gmail.com>
2024-07-27 12:15:29 +0500

    UI impr

--- source/js/pages/app_is_down.js
+++ source/js/pages/app_is_down.js
@@ -3,10 +3,10 @@ export const TITLE = Lang.use("app_is_down");
 export default function main(){
 	return `
 		<container class="p-5">
-			<column class="w-50 surface-v1 p-5">
+			<column class="w-50 surface-v1 p-5 gap-1">
 				<x-svg name="${window.CONF["tools"]["app_is_down"]["icon"]}"></x-svg>
-				<h1>${window.CONF["tools"]["app_is_down"]["title"]}</h1>
-				<h3 class="text-color-secondary">${window.CONF["tools"]["app_is_down"]["explanation"]}</h3>
+				<p class="text-align-center text-size-2">${window.CONF["tools"]["app_is_down"]["title"]}</p>
+				<p class="text-align-center text-color-secondary">${window.CONF["tools"]["app_is_down"]["explanation"]}</p>
 			</column>
 		</container>
 	`;
