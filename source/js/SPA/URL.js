a121ff18716213a712208a39ef0245f26ae34b36
woXrooX <woXrooX@gmail.com>
2024-11-06 11:07:30 +0500

    Style: Clean up

--- source/js/SPA/URL.js
+++ source/js/SPA/URL.js
@@ -12,28 +12,15 @@ export default class URL{
 	};
 
 	static {
-		URL.current_location.href = window.location.href;
-		URL.current_location.hash = window.location.hash;
-		URL.current_location.pathname = window.location.pathname;
+		URL.#update_current_URL();
 
 		URL.previous_location = structuredClone(URL.current_location);
 	}
 
 	static handle_change(){
-		Log.error("URL.handle_change()");
+		// Log.info("URL.handle_change()");
 
-		URL.current_location.href = window.location.href;
-		URL.current_location.hash = window.location.hash;
-		URL.current_location.pathname = window.location.pathname;
-
-		console.log("Prev", URL.previous_location);
-		console.log("Curr", URL.current_location);
-
-
-		// if(
-		// 	window.location.pathname === URL.previous_location.pathname &&
-		// 	window.location.hash != URL.previous_location.hash
-		// ) return;
+		URL.#update_current_URL();
 
 		if(window.location.pathname != URL.previous_location.pathname){
 			Router.handle();
@@ -41,6 +28,14 @@ export default class URL{
 		}
 
 		if(window.location.href !== URL.previous_location.href) URL.previous_location = structuredClone(URL.current_location);
+	}
+
+	static #update_current_URL(){
+		// Log.info("URL.#update_current_URL()");
+
+		URL.current_location.href = window.location.href;
+		URL.current_location.hash = window.location.hash;
+		URL.current_location.pathname = window.location.pathname;
 	}
 }
 
