00fed401205f9440e90b39fcc05a1aff98324549
woXrooX <woXrooX@gmail.com>
2024-11-24 18:27:55 +0300

    Conf brand color is obsolete

--- source/python/modules/SendGrid.py
+++ source/python/modules/SendGrid.py
@@ -45,7 +45,7 @@ if __name__ != "__main__":
 						<title>{Globals.PROJECT_LANG_DICT.get(Globals.CONF["default"]["title"], {}).get(Globals.CONF["default"]["language"]["fallback"], "x")}</title>
 					</head>
 					<body style="width:100%; padding:10px;">
-						<header style="text-aligment:center; color: white; padding:5px; background-color:hsl({Globals.CONF["default"]["color"]["brand"]["hue"]}deg, {Globals.CONF["default"]["color"]["brand"]["saturation"]}%, {Globals.CONF["default"]["color"]["brand"]["lightness"]}%); width:100%;">
+						<header style="text-aligment:center; color: white; padding:5px; width:100%;">
 							{Globals.PROJECT_LANG_DICT.get(Globals.CONF["default"]["title"], {}).get(Globals.CONF["default"]["language"]["fallback"], "x")}
 						</header>
 						<main>{content}</main>
