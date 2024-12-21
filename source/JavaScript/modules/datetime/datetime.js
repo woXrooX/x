1d6eb7527a863246bf997aad878eb9bc27e37dc5
woXrooX <woXrooX@gmail.com>
2024-12-21 13:41:54 +0300

    New helper added

--- source/JavaScript/modules/datetime/datetime.js
+++ source/JavaScript/modules/datetime/datetime.js
@@ -156,3 +156,17 @@ export function milliseconds_to_human_readable_v1(milliseconds) {
 
 	return formatted_time;
 }
+
+// Format DD.MM.YYYY HH:MM:MS
+function milliseconds_to_human_readable_v2(MS){
+	const date = new Date(MS);
+
+	const day = date.getDate().toString().padStart(2,'0');
+	const month = (date.getMonth() + 1).toString().padStart(2,'0');
+	const year = date.getFullYear();
+	const hours = date.getHours().toString().padStart(2,'0');
+	const minutes = date.getMinutes().toString().padStart(2,'0');
+	const milliseconds = date.getMilliseconds().toString().padStart(4,'0');
+
+	return `${day}.${month}.${year} ${hours}:${minutes}:${milliseconds}`;
+}
