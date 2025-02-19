86665f82278c2e6373a9abfef8c89b082c57a543
izzatillo <yzokenm@gmail.com>
2025-02-19 17:48:08 +0500

    refactor

--- source/JavaScript/pages/x_user.js
+++ source/JavaScript/pages/x_user.js
@@ -1,43 +1,14 @@
 export const TITLE = window.Lang.use("user");
 
-Modal.push_func(async function modal_user_roles(){
-	const roles_element = Modal.element_main.querySelector("column#roles");
-	
-	let user_roles = await window.bridge({"for": "get_user_roles"})
-	if("data" in user_roles) user_roles = user_roles["data"];
-	else return `<p class="width-100 text-size-0-8 surface-info padding-1">No user roles</p>`;
-
-	console.log(user_roles);
-	
-	let HTML = '';
-	for (const user_role of user_roles) {
-		HTML += `
-			<label class="d-flex flex-row gap-0-5">
-				<input type="checkbox" name="roles" value="${user_role['name']}" ${user_role["assigned"] === 1 ? "checked" : ''} >
-				<p for="checkbox">${user_role['name']}</p>
-			</label>
-		`;
-		
-	}
-	roles_element.innerHTML = `
-		<form for="assign_roles">
-			${HTML}
-			<label>
-				<input class="btn btn-primary" type='submit' name='save' value="save">
-				<p for='assign_roles'></p>
-			</label>
-		</form>
-	`
-})
-
-export default function main(){
+export default async function main(){
 	return `
 		<container class="padding-5 gap-1">
-			${actions_HTML()}
+			${await actions_HTML()}
 		</container>
 	`;
 
-	function actions_HTML(){
+	async function actions_HTML(){
+		
 		return `
 			<row class="padding-1 surface-v1 gap-0-5 flex-row flex-x-start">
 				<x-svg
@@ -52,8 +23,8 @@ export default function main(){
 				></x-svg>
 
 				<x-svg name="gear" id="modal_user_roles" class="btn btn-info"></x-svg>
-				<x-modal trigger_selector="x-svg#modal_user_roles" modal_func="modal_user_roles">
-					<column id="roles" class="padding-2 gap-1"></column>
+				<x-modal trigger_selector="x-svg#modal_user_roles">
+					${await roles_HTML()}
 				</x-modal>
 
 				<x-svg
@@ -68,6 +39,32 @@ export default function main(){
 				></x-svg>
 			</row>
 		`;
+
+		async function roles_HTML(){
+			let roles = await window.bridge({"for": "get_user_roles"})
+			if("data" in roles) roles = roles["data"];
+			else return `<p class="width-100 text-size-0-8 surface-info padding-1">No user roles</p>`;
+
+			let HTML = '';
+			for (const role of roles) {
+				HTML += `
+					<label class="display-flex flex-row flex-y-center gap-0-3">
+						<input type="checkbox" name="roles" value="${role['name']}" ${role["assigned"] === 1 ? "checked" : ''} >
+						<p for="checkbox">${role['name']}</p>
+					</label>
+				`;
+			}
+			
+			return`
+				<column id="roles" class="padding-2 gap-1">
+					<form for="assign_roles" x-modal="on:success:hide" x-toast="on:any:message">
+						${HTML}
+						<input class="btn btn-primary" type='submit' name='save' value="save">
+						<p for='assign_roles'></p>
+					</form>
+				</column>
+			`;
+		}
 	}
 }
 
