14b79cac1015f18bc6cd8a6a77bf55da88414c9f
woXrooX <woXrooX@gmail.com>
2025-03-16 00:21:54 +0500

    Clean up

--- databases/MySQL/x.sql
+++ databases/MySQL/x.sql
@@ -8,13 +8,13 @@ COLLATE utf8mb4_0900_ai_ci;
 USE [NAME];
 
 -- Set the session level
-SET NAMES utf8mb4;
-SET character_set_client = utf8mb4;
-SET character_set_connection = utf8mb4;
-SET character_set_database = utf8mb4;
-SET character_set_server = utf8mb4;
-SET character_set_results = utf8mb4;
-SET collation_connection = utf8mb4_0900_ai_ci;
+-- SET NAMES utf8mb4;
+-- SET character_set_client = utf8mb4;
+-- SET character_set_connection = utf8mb4;
+-- SET character_set_database = utf8mb4;
+-- SET character_set_server = utf8mb4;
+-- SET character_set_results = utf8mb4;
+-- SET collation_connection = utf8mb4_0900_ai_ci;
 
 -- If you need case-sensitive comparisons (then use utf8mb4_0900_as_cs)
 
