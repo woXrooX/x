05e6c4bb7bc524c1e987368c681ffd4ad5670f51
woXrooX <woXrooX@gmail.com>
2025-04-09 22:08:31 +0500

    Build: SPA/Title.js is obsolete

--- source/JavaScript/SPA/Title.js
+++
@@ -1,8 +0,0 @@
-export default class Title{
-	static set(newTitle = ""){
-		if(!!newTitle === false) document.title = window.Lang.use(window.CONF["default"]["title"]);
-		else document.title = `${window.Lang.use(newTitle)} | ${window.Lang.use(window.CONF["default"]["title"])}`;
-	}
-}
-
-window.Title = Title;
