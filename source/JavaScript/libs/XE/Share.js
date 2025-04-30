1c2b225ceb7fef321e1b8c53d29d7b8311c11bf0
woXrooX <woXrooX@gmail.com>
2025-04-30 06:07:35 +0500

    x share clean up

--- source/JavaScript/libs/XE/Share.js
+++ source/JavaScript/libs/XE/Share.js
@@ -2,46 +2,39 @@ export default class Share extends HTMLElement{
 	constructor(){
 		super();
 
-		this.textToShare = null;
-		if(!!this.getAttribute("selector") === true) this.textToShare = this.getAttribute("selector");
-		else if(!!this.getAttribute("text") === true) this.textToShare = this.getAttribute("text");
+		const data = {
+			title: encodeURIComponent(document.title),
+			content: encodeURIComponent(document.querySelector('meta[name="description"]')?.getAttribute('content') || ''),
 
-		const shareData = {
-			title: encodeURIComponent(this.getAttribute("title") || document.title),
-			text: encodeURIComponent(this.textToShare || window.Lang.use("share_default_text")),
-
-			// If Share URL Is Not HTTPS It Won't Work
-			// On Facebook
-			// On Linkedin
-			url: encodeURIComponent(this.getAttribute("url") || window.location.href)
+			// If Share URL is not HTTPS it won't work on: Facebook, Linkedin
+			URL: encodeURIComponent(window.location.href)
 		};
 
-
 		this.innerHTML = `
-			<x-svg name="share" id="modal_share"></x-svg>
-			<x-modal trigger_selector="x-svg#modal_share">
-				<row class="flex-row flex-x-between padding-10 gap-1 text-size-1-5">
-					<a href="https://twitter.com/intent/tweet?url=${shareData.url}&text=${shareData.text}" target="_blank">
+			<x-svg name="share" id="modal_x_share"></x-svg>
+			<x-modal trigger_selector="x-svg#modal_x_share">
+				<row class="flex-row flex-x-between padding-2 gap-1 text-size-2">
+					<a href="https://twitter.com/intent/tweet?url=${data.URL}&text=${data.content}" target="_blank">
 						<x-svg name="twitter_bird_original"></x-svg>
 					</a>
 
-					<a href="https://www.linkedin.com/sharing/share-offsite/?url=${shareData.url}&title=${shareData.title}&summary=${shareData.text}&source=${shareData.url}" target="_blank">
+					<a href="https://www.linkedin.com/sharing/share-offsite/?url=${data.URL}&title=${data.title}&summary=${data.content}&source=${data.URL}" target="_blank">
 						<x-svg name="linkedin_original"></x-svg>
 					</a>
 
-					<a href="https://www.facebook.com/sharer.php?u=${shareData.url}&quote=${shareData.text}" target="_blank">
+					<a href="https://www.facebook.com/sharer.php?u=${data.URL}&quote=${data.content}" target="_blank">
 						<x-svg name="facebook_original"></x-svg>
 					</a>
 
-					<a href="https://www.instagram.com/share?url=${shareData.url}&caption=${shareData.text}" target="_blank">
+					<a href="https://www.instagram.com/share?url=${data.URL}&caption=${data.content}" target="_blank">
 						<x-svg name="instagram_original"></x-svg>
 					</a>
 
-					<a href="https://t.me/share/url?url=${shareData.url}&text=${shareData.text}" target="_blank">
+					<a href="https://t.me/share/url?url=${data.URL}&text=${data.content}" target="_blank">
 						<x-svg name="telegram_original"></x-svg>
 					</a>
 
-					<a href="https://www.reddit.com/submit?selftext=${shareData.text}&title=${shareData.title}&url=${shareData.url}" target="_blank">
+					<a href="https://www.reddit.com/submit?selftext=${data.content}&title=${data.title}&url=${data.URL}" target="_blank">
 						<x-svg name="reddit_original"></x-svg>
 					</a>
 				</row>
@@ -49,22 +42,13 @@ export default class Share extends HTMLElement{
 		`;
 
 		this.querySelector("x-modal").onclick = async ()=>{
-
-			// Check If navigator.share
 			if(!!navigator.share === true)
-				try{
-					await navigator.share(shareData);
-					window.Log.success("MDN shared successfully");
-				}catch(err){
-					window.Log.error(`Error: ${err}`);
-				}
+				try{ await navigator.share(data); }
+				catch(err){ window.Log.error(`x-share error: ${err}`); }
 
-			else window.Log.warning("No Native Support For 'navigator.share' On Your Device!");
+			else window.Log.warning("No native support for 'navigator.share' on this device!");
 		};
 	}
 };
 
 window.customElements.define('x-share', Share);
-
-// Make Share Usable W/O Importing It
-window.Share = Share;
