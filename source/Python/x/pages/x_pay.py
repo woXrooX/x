3275f74f7e1bd21574ff1353d33245a4d50a5037
woXrooX <woXrooX@gmail.com>
2025-05-05 10:08:32 +0500

    Prep

--- source/Python/x/pages/x_pay.py
+++ source/Python/x/pages/x_pay.py
@@ -1,20 +1,41 @@
+from main import session
+
 from Python.x.modules.Page import Page
 from Python.x.modules.Payment_System.Payment import Payment
+from Python.x.modules.response import response
+from Python.x.modules.Globals import Globals
 
 @Page.build()
 def x_pay(request):
-	if request.method == "POST":
-		if request.content_type == "application/json":
-			if request.get_json()["for"] == "get_publishable_key": return response(type="success", message="success", data=Globals.CONF["Stripe"]["publishable_key"])
+	if request.method == "POST" and request.content_type == "application/json":
+		data = request.get_json()
 
-			if request.get_json()["for"] == "create_payment_intent":
+		match data.get("for"):
+			case "get_publishable_key": return response(type="success", message="success", data=Globals.CONF["Stripe"]["publishable_key"])
+
+			case "create_payment_intent":
 				return Payment.create_payment_intent(
-					amount=1234,
-					currency="gbp",
-					automatic_payment_methods={'enabled': True},
-					metadata={
-						"key_1": "value_1",
-						"key_2": "value_2"
-					},
-					receipt_email="sample@site.com"
+					amount=1000,
+					currency="usd",
+					automatic_payment_methods={'enabled': True}
 				)
+
+			case "create_subscription":
+				metadata = data.get("metadata", {})
+				metadata["user_id"] = session["user"]["id"]
+
+				return Payment.create_subscription(
+					customer_id=data.get("customer_id"),
+					price_id=data.get("price_id"),
+					payment_method_id=data.get("payment_method_id"),
+					metadata=metadata,
+					trial_days=data.get("trial_days")
+				)
+
+			case "attach_payment_method":
+				return Payment.attach_payment_method_to_customer(
+					payment_method_id=data.get("payment_method_id"),
+					customer_id=data.get("customer_id")
+				)
+
+			case _: return response(type="error", message="invalid_operation")
