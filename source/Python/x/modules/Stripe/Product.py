8caf1c653b1c37b1af72d5d74565413f4f6404e3
woXrooX <woXrooX@gmail.com>
2025-05-06 15:25:45 +0500

    Sample products

---
+++ source/Python/x/modules/Stripe/Product.py
@@ -0,0 +1,17 @@
+if __name__ != "__main__":
+	from Python.x.modules.Logger import Log
+
+	class Product:
+		one_time_products = {
+			"sample_one": {
+				"amount": 1000,
+				"currency": "usd"
+			}
+		}
+
+		subscription_products = {
+			"sample_one": {
+				"amount": 1000,
+				"currency": "usd"
+			}
+		}
