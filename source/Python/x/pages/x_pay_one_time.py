4a6c7e7e8dea940142515eebc1276ea5e77df1a5
woXrooX <woXrooX@gmail.com>
2025-05-06 21:59:47 +0500

    Instructions added

--- source/Python/x/pages/x_pay_one_time.py
+++ source/Python/x/pages/x_pay_one_time.py
@@ -1,3 +1,7 @@
+# Final event to handle for one time payment
+# Fires the moment the funds are captured. It’s emitted for all PaymentIntent‑based charges, including ones created for Checkout or Subscriptions.
+# payment_intent.succeeded
+
 from main import session
 
 from Python.x.modules.Page import Page
@@ -16,8 +20,8 @@ def x_pay_one_time(request):
 
 			if request.get_json()["for"] == "get_publishable_key": return response(type="success", message="success", data=Globals.CONF["Stripe"]["publishable_key"])
 
-			if request.get_json()["for"] == "create_payment_intent":
-				return Payment.create_payment_intent(
+			if request.get_json()["for"] == "create_intent":
+				return Payment.create_intent(
 					amount=Product.one_time_products["sample_one"]["amount"],
 					currency=Product.one_time_products["sample_one"]["currency"],
 					automatic_payment_methods={'enabled': True}
--- source/Python/x/pages/x_pay_subscription.py
+++ source/Python/x/pages/x_pay_subscription.py
@@ -1,3 +1,8 @@
+# Events to look at:
+# invoice.payment_succeeded -> When subscription paid
+# invoice.payment_failed -> Payment failed, notify the user
+# customer.subscription.deleted -> Subscription cancelled
+
 from main import session
 
 from Python.x.modules.Page import Page
@@ -7,18 +12,16 @@ from Python.x.modules.Stripe.Payment import Payment
 from Python.x.modules.Stripe.Product import Product
 
 @Page.build()
-def x_pay_subscription(request):
+def x_pay_subscription(request, subscription_name):
+	if subscription_name not in Product.subscription_products: return response(type="error", message="invalid_request")
+
 	if request.method == "POST":
 		if request.content_type == "application/json":
 			if request.get_json()["for"] == "get_product":
 				# Your logic goes here
-				return response(type="success", message="success", data=Product.subscription_products["sample_one"])
+				return response(type="success", message="success", data=Product.subscription_products[subscription_name])
 
 			if request.get_json()["for"] == "get_publishable_key": return response(type="success", message="success", data=Globals.CONF["Stripe"]["publishable_key"])
 
-			if request.get_json()["for"] == "create_payment_intent":
-				return Payment.create_payment_intent(
-					amount=Product.subscription_products["sample_one"]["amount"],
-					currency=Product.subscription_products["sample_one"]["currency"],
-					automatic_payment_methods={'enabled': True}
-				)
+			if request.get_json()["for"] == "create_subscription":
+				return Payment.create_subscription(subscription_name)
