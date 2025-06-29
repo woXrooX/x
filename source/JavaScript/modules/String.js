673dbcd8a659ee98c09a63f77d56f55b590410d1
woXrooX <woXrooX@gmail.com>
2025-06-29 10:57:14 +0500

    New string x method for capping the first char

--- source/JavaScript/modules/String.js
+++ source/JavaScript/modules/String.js
@@ -1,6 +1,7 @@
 export default class x_String{
 	static {
-		String.prototype.x_format = function(values){ return x_String.format(this, values); };
+		String.prototype.x_format = function(values) { return x_String.format(this, values); };
+		String.prototype.x_capitalize_first = function() { return x_String.capitalize_first(this); };
 	}
 
 	/////////// APIs
@@ -10,6 +11,11 @@ export default class x_String{
 	static format(template, values){
 		return template.replace(/{(\w+)}/g, replacer);
 		function replacer(match, key){ return values[key]; }
+	}
+
+	static capitalize_first(str) {
+		if (!str) return '';
+		return str[0].toUpperCase() + str.slice(1);
 	}
 }
 
