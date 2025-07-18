04d8ab607d6efda52ea18ee2c5064e3e06ae4425
rizoxon <madaminov.co@gmail.com>
2025-07-18 18:48:54 +0500

    Notification event togglers

--- source/Python/x/modules/Notifications.py
+++ source/Python/x/modules/Notifications.py
@@ -35,7 +35,9 @@ if __name__ != "__main__":
 						users.id,
 						users.eMail,
 						users.phone_number,
-						CASE WHEN disabled_notification_events.id IS NOT NULL THEN 1 ELSE 0 END AS event_is_disabled
+						disabled_notification_events.via_in_app,
+						disabled_notification_events.via_eMail,
+						disabled_notification_events.via_SMS
 					FROM users
 					LEFT JOIN disabled_notification_events ON
 						disabled_notification_events.event = %s AND
@@ -50,23 +52,20 @@ if __name__ != "__main__":
 			)
 			if recipient is False or recipient is None: return False
 
-			# Successfully exit if event is disabled
-			if recipient["event_is_disabled"] is 1: return True
-
 			is_successfully = True
 
 			if via_in_app is True:
-				if Notifications.new_in_app(recipient["id"], sender, content_TEXT, content_JSON, event_name, type_name) is False:
+				if Notifications.new_in_app(recipient, sender, content_TEXT, content_JSON, event_name, type_name) is False:
 					Log.warning("Notifications.new() -> Could not create: new_in_app()")
 					is_successfully = False
 
 			if via_eMail is True:
-				if Notifications.new_eMail(recipient["eMail"], sender, content_TEXT, content_JSON, event_name) is False:
+				if Notifications.new_eMail(recipient, sender, content_TEXT, content_JSON, event_name) is False:
 					Log.warning("Notifications.new() -> Could not send: new_eMail()")
 					is_successfully = False
 
 			if via_SMS is True:
-				if Notifications.new_SMS(recipient["phone_number"], sender, content_TEXT, content_JSON, event_name) is False:
+				if Notifications.new_SMS(recipient, sender, content_TEXT, content_JSON, event_name) is False:
 					Log.warning("Notifications.new() -> Could not send: new_SMS()")
 					is_successfully = False
 
@@ -85,11 +84,13 @@ if __name__ != "__main__":
 			event_name = None,
 			type_name = None
 		):
+			if "via_in_app" in recipient and recipient["via_in_app"] == 1: return True
+
 			data = MySQL.execute(
 				sql="INSERT INTO notifications (sender, recipient, content_TEXT, content_JSON, event, type) VALUES (%s, %s, %s, %s, %s, %s);",
 				params=[
 					sender,
-					recipient,
+					recipient["id"],
 					content_TEXT,
 					json.dumps(content_JSON, default=str) if isinstance(content_JSON, dict) else None,
 					Globals.NOTIFICATION_EVENTS.get(event_name, {}).get("id", None),
@@ -108,7 +109,9 @@ if __name__ != "__main__":
 			content_JSON = None,
 			event_name = None
 		):
-			if recipient is None: return False
+			if "via_eMail" in recipient and recipient["via_eMail"] == 1: return True
+
+			if recipient["eMail"] is None: return False
 
 			eMail_subject_LANGUAGE_DICTIONARY_key = f"{event_name}_eMail_subject"
 			eMail_content_LANGUAGE_DICTIONARY_key = f"{event_name}_eMail_content"
@@ -119,17 +122,17 @@ if __name__ != "__main__":
 
 			content_JSON = content_JSON if isinstance(content_JSON, dict) else {}
 
-			try: subject = Globals.LANGUAGE_DICTIONARY[eMail_subject_LANGUAGE_DICTIONARY_key]["en"].format(sender=sender, recipient=recipient, content_TEXT=content_TEXT, **content_JSON)
+			try: subject = Globals.LANGUAGE_DICTIONARY[eMail_subject_LANGUAGE_DICTIONARY_key]["en"].format(sender=sender, recipient=recipient["eMail"], content_TEXT=content_TEXT, **content_JSON)
 			except Exception as e:
 				Log.error(f"Notifications.new_eMail()->subject: {e}")
 				return False
 
-			try: content = Globals.LANGUAGE_DICTIONARY[eMail_content_LANGUAGE_DICTIONARY_key]["en"].format(sender=sender, recipient=recipient, content_TEXT=content_TEXT, **content_JSON)
+			try: content = Globals.LANGUAGE_DICTIONARY[eMail_content_LANGUAGE_DICTIONARY_key]["en"].format(sender=sender, recipient=recipient["eMail"], content_TEXT=content_TEXT, **content_JSON)
 			except Exception as e:
 				Log.error(f"Notifications.new_eMail()->content: {e}")
 				return False
 
-			return SendGrid.send("noreply", recipient, content, subject)
+			return SendGrid.send("noreply", recipient["eMail"], content, subject)
 
 		@staticmethod
 		def new_SMS(
@@ -139,7 +142,9 @@ if __name__ != "__main__":
 			content_JSON = None,
 			event_name = None
 		):
-			if recipient is None: return
+			if "via_SMS" in recipient and recipient["via_SMS"] == 1: return True
+
+			if recipient["phone_number"] is None: return
 
 			SMS_body_LANGUAGE_DICTIONARY_key = f"{event_name}_SMS_body"
 
@@ -147,9 +152,9 @@ if __name__ != "__main__":
 
 			content_JSON = content_JSON if isinstance(content_JSON, dict) else {}
 
-			try: content = Globals.LANGUAGE_DICTIONARY[SMS_body_LANGUAGE_DICTIONARY_key]["en"].format(recipient=recipient, sender=sender, content_TEXT=content_TEXT, **content_JSON)
+			try: content = Globals.LANGUAGE_DICTIONARY[SMS_body_LANGUAGE_DICTIONARY_key]["en"].format(recipient=recipient["phone_number"], sender=sender, content_TEXT=content_TEXT, **content_JSON)
 			except Exception as e:
 				Log.error(f"Notifications.new_SMS()->content: {e}")
 				return False
 
-			Twilio.send_sms(content, Globals.CONF["Twilio"]["alphanumeric_sender_id"], recipient)
+			Twilio.send_sms(content, Globals.CONF["Twilio"]["alphanumeric_sender_id"], recipient["phone_number"])
