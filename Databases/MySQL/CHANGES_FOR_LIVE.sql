2725d69fbe975e72385de6e5cbcb8675fab74857
rizoxon <madaminov.co@gmail.com>
2025-07-21 16:02:46 +0500

    missing piece

--- Databases/MySQL/CHANGES_FOR_LIVE.sql
+++ Databases/MySQL/CHANGES_FOR_LIVE.sql
@@ -2,3 +2,8 @@
 \W
 
 USE [NAME];
+
+ALTER TABLE disabled_notification_events
+	ADD COLUMN `via_in_app` BIT(1) NOT NULL DEFAULT b'0' AFTER `event`,
+	ADD COLUMN `via_eMail` BIT(1) NOT NULL DEFAULT b'0' AFTER `via_in_app`,
+	ADD COLUMN `via_SMS` BIT(1) NOT NULL DEFAULT b'0' AFTER `via_eMail`;
