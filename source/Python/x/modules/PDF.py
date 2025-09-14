9177f8a844a6879d337857d15682b160b18762ea
woXrooX <woXrooX@gmail.com>
2025-09-14 11:30:29 +0500

    Changes

--- source/Python/x/modules/PDF.py
+++ source/Python/x/modules/PDF.py
@@ -3,17 +3,19 @@
 
 if __name__ != "__main__":
 	from weasyprint import HTML
+
 	from Python.x.modules.Globals import Globals
 
 	class PDF:
 		@staticmethod
-		def generate(content, path_to_save, file_name_to_save, base_url=Globals.PROJECT_PATH):
-			# Check For Invalid Arguments
-			if not content or not path_to_save or not file_name_to_save: return False
+		def generate(content, file_path_and_name_and_extension):
+			if not content or not file_path_and_name_and_extension: return False
 
-			file_path_and_name = f"{path_to_save}/{file_name_to_save}.pdf"
+			try: HTML(
+				string=content,
+				base_url=Globals.PROJECT_PATH
+			).write_pdf(f"{Globals.PROJECT_PATH}{file_path_and_name_and_extension}")
 
-			try: HTML(string=content, base_url=base_url).write_pdf(file_path_and_name)
 			except: return False
 
 			return True
