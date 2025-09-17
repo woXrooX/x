03532416a2881ad3e280e87d704210a8df1ed0fc
woXrooX <woXrooX@gmail.com>
2025-09-17 10:14:34 +0500

    x layers prep for funcs

--- source/JavaScript/libs/XE/Layers/Layer.js
+++ source/JavaScript/libs/XE/Layers/Layer.js
@@ -1,4 +1,41 @@
 export default class Layer extends HTMLElement {
+	/////////////////////////// Static
+
+	/////////// APIs
+
+	static build_DOM_HTML(id, content){
+		return `
+			<container id="layer_${id}" class="adding">
+				<cover></cover>
+				<layer class="surface-v1 overflow-hidden">
+					<x-svg
+						class="
+							btn	btn-primary btn-s
+
+							position-fixed
+							top-5px
+							right-5px
+						"
+						for="layer_remove"
+						name="x"
+						color="ffffff"
+					></x-svg>
+					<main
+						class="
+							overflow-y-scroll
+							width-100
+							height-100
+							padding-top-2rem
+						"
+					>${content}</main>
+				</layer>
+			</container>
+		`;
+	}
+
+
+	/////////////////////////// Object
+
 	#DOM = null;
 
 	constructor() {
@@ -12,7 +49,7 @@ export default class Layer extends HTMLElement {
 
 	#handle_trigger_click = ()=> {
 		const trigger_element = document.querySelector(this.getAttribute("trigger_selector"));
-		if(!!trigger_element === false) return;
+		if (!!trigger_element === false) return;
 
 		trigger_element.onclick = ()=> window.x.Layers.add(
 			this.#DOM,
