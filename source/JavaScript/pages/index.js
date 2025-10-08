d598e01b62c6fcdc3b92d6f8261c91c786ff3584
woXrooX <woXrooX@gmail.com>
2025-10-08 10:35:38 +0500

    Def pages content improved

--- source/JavaScript/pages/index.js
+++ source/JavaScript/pages/index.js
@@ -3,12 +3,11 @@ export function before() { window.x.Head.set_title("index"); }
 export default async function main(){
 	return `
 		<container class="max-width-1200px padding-5 gap-1">
-			<column class="surface-v1 padding-2 gap-1">
-				<p>Welcome to x</p>
-				<p>This is the default "/" page</p>
+			<column class="surface-v1 padding-5 gap-1">
+				<p class="text-size-2">Welcome to x!</p>
+				<p>New here? Start with the <a href="https://www.woxroox.com/developer/tools/x/documentation/quick_start" target="_blank">Quick Start</a>.</p>
+				<p class="text-color-secondary text-size-0-9">You're on the "/" page.</p>
 			</column>
 		</container>
 	`;
 }
-
-
