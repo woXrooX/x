f16147ec9c5777da1ccce755dcd48f0e4df894ea
woXrooX <woXrooX@gmail.com>
2025-12-03 09:59:39 +0500

    Impr + name fix

--- source/Python/x/modules/time/time.py
+++ source/Python/x/modules/time/time.py
@@ -2,13 +2,10 @@ if __name__ != "__main__":
 	from datetime import datetime, date
 
 	# Returns "datetime" object
-	def validate_timestamp(
+	# Validates & formats the timestamp string
+	def string_to_datetime(
 		timestamp: str,
 		format_string = "%Y-%m-%d %H:%M:%S.%f"
 	):
-		validated_time = False
-
-		try: validated_time = datetime.strptime(timestamp, format_string).date()
+		try: return datetime.strptime(timestamp, format_string)
 		except: return False
-
-		return validated_time
