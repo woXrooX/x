052dc2f25b6cb1363e99c406c310775139147a23
woXrooX <woXrooX@gmail.com>
2026-04-21 12:38:16 +0500

    on update function added

--- Databases/PostgreSQL/Functions.sql
+++ Databases/PostgreSQL/Functions.sql
@@ -1,1 +1,14 @@
 \! echo "============================== Functions.sql ==============================";
+
+
+-- ON UPDATE CURRENT_TIMESTAMP
+CREATE OR REPLACE FUNCTION on_update_set_current_timestamp()
+RETURNS TRIGGER AS $$
+	BEGIN
+		IF NEW IS DISTINCT FROM OLD THEN
+			NEW.metadata_last_updated_at = CURRENT_TIMESTAMP;
+		END IF;
+
+		RETURN NEW;
+	END;
+$$ LANGUAGE plpgsql;
