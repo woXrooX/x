fdb1d99f72777b959fbbb150959f7334ae7ef3bf
woXrooX <woXrooX@gmail.com>
2026-07-02 11:37:12 +0500

    Using built  in

--- source/JavaScript/libs/XE/Modal.js
+++ source/JavaScript/libs/XE/Modal.js
@@ -97,7 +97,7 @@ export default class Modal extends HTMLElement{
 	static async #execute_on_show(func_name, modal_data = null){
 		if(!!func_name === false) return;
 
-		if (!(func_name in Modal.#FUNC_POOL)) return console.error(`Modal.#execute_on_show(): Invalid func_name: ${func_name}`);
+		if (!(func_name in Modal.#FUNC_POOL)) return Log.error(`Modal.#execute_on_show(): Invalid func_name: ${func_name}`);
 
 		await Modal.#FUNC_POOL[func_name](modal_data);
 	}
