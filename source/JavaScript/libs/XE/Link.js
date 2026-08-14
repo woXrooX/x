04a7fcd6760aa24774ecc315398d86b009e4debe
woXrooX <woXrooX@gmail.com>
2026-08-14 10:36:50 +0500

    When history cannot go to, dont show the link button

--- source/JavaScript/libs/XE/Link.js
+++ source/JavaScript/libs/XE/Link.js
@@ -1,31 +1,40 @@
 export default class Link extends HTMLElement{
-	#href = null;
+	#parts = null;
 
-	constructor(){ super(); }
+	constructor() { super(); }
 
-	connectedCallback(){ this.addEventListener("click", this.#event_handler_click); }
-
-	disconnectedCallback(){ this.removeEventListener("click", this.#event_handler_click); }
-
-	#event_handler_click(event){
-		event.stopPropagation();
-		this.#handle_go();
-	}
-
-	#handle_go(){
+	connectedCallback() {
 		if(this.hasAttribute("go") !== true) return;
 
-		const parts = this.getAttribute("go").split(':', 2);
+		this.#parse_commands();
+		this.#validate_can_go_to_history();
 
-		switch (parts[0]) {
+		this.addEventListener("click", this.#event_handler_click);
+	}
+
+	disconnectedCallback() { this.removeEventListener("click", this.#event_handler_click); }
+
+	#event_handler_click(event) {
+		event.stopPropagation();
+
+		switch (this.#parts[0]) {
 			case "URL":
-				window.Hyperlink.locate(parts[1]);
+				Hyperlink.locate(this.#parts[1]);
 				break;
 
 			case "history":
-				const res = Hyperlink.go_to_history(parts[1]);
+				const res = Hyperlink.go_to_history(this.#parts[1]);
 				break;
 		}
+	}
+
+	#parse_commands() {
+		this.#parts = this.getAttribute("go").split(':', 2);
+	}
+
+	#validate_can_go_to_history() {
+		if (this.#parts[0] != "history") return;
+		if (Hyperlink.can_go_to_history(this.#parts[1]) === false) this.classList.add("display-none");
 	}
 };
 
